6f54477b562db131d9dbf52ca3e06b3de410654e
agdaq (ALPHA-g prototype DAQ <agdaq@alphagdaq.triumf.ca>
2017-01-27 18:29:33 -0800

    KO - implement timeout and restart

--- src/feyair.cxx
+++ src/feyair.cxx
@@ -8,93 +8,134 @@
 
 #include "KOsocket.h"
 
+bool Wait(KOsocket*s, int wait_sec, const char* explain)
+{
+   while (1) {
+      int a = s->available();
+      //printf("Wait %d sec, available %d\n", wait_sec, a);
+      if (a > 0)
+         return true;
+      if (wait_sec <= 0) {
+         printf("Timeout waiting for %s\n", explain);
+         return false;
+      }
+      wait_sec--;
+      sleep(1);
+   }
+   // not reached
+}
+
 int main(int argc, char* argv[])
 {
    setbuf(stdout, NULL);
    setbuf(stderr, NULL);
 
-   KOsocket* s = new KOsocket("tpc02", 40);
+   const char* name = argv[1];
 
-   char out[256];
-
-   sprintf(out, "yair%d.txt", (int)time(NULL));
-
-   FILE *fout = fopen(out, "w");
-   assert(fout);
-      
-   printf("Writing to %s\n", out);
-
-   setbuf(fout, NULL);
-
-   int count = 0;
-
-   while (1) {
-      time_t t1 = time(NULL);
-
-      const char *cmd1 = "uart_regfile_status_read 7 8 3\n";
-      s->write(cmd1, strlen(cmd1));
-
-      char reply[1024];
-      int rd = s->read(reply, sizeof(reply));
-      int v = atoi(reply);
-      int have = v & (1<<19);
-      if (rd > 0)
-         reply[rd] = 0;
-      printf("rd %d, value %d, 0x%x, have %d, reply [%s]\n", rd, v, v, have, reply);
-
-      time_t t2 = time(NULL);
-
-      if (!have) {
-         sleep(1);
-         continue;
-      }
-
-      const char *cmd2 = "simult_capture_of_hw_trig_uart_nios_dacs 7 3 0 0\n";
-      s->write(cmd2, strlen(cmd2));
-
-      int total = 0;
-
-      rd = s->read(reply, sizeof(reply));
-      printf("rd %d, reply [%s]\n", rd, reply);
-
-      if (rd <= 0)
-         break;
-
-      total += rd;
-
-      reply[rd] = 0;
-
-      time_t t3 = time(NULL);
-
-      fprintf(fout, "%d %d %d %d %s", count, (int)t1, (int)t2, (int)t3, reply);
-
-      while (rd > 0) {
-         rd = s->read(reply, sizeof(reply));
-         printf("%d.", rd);
-         if (rd <= 0)
-            break;
-         total += rd;
-         reply[rd] = 0;
-         fprintf(fout, "%s", reply);
-         if (rd < 750)
-            break;
-      }
-      printf("done\n");
-
-      time_t te = time(NULL);
-
-      printf("event %d, got %d bytes, times %d %d %d\n", count, total, (int)(t2-t1), (int)(t3-t2), (int)(te-t1));
-
-      fprintf(fout, " total %d elapsed %d %d %d\n", total, (int)(t2-t1), (int)(t3-t2), (int)(te-t1));
-      count++;
+   if (strcmp(name, "tpc01")==0) {
+      // good
+   } else if (strcmp(name, "tpc02")==0) {
+      // good
+   } else {
+      printf("Only tpc01 and tpc02 permitted. Bye.\n");
+      return 1;
    }
 
-   fclose(fout);
-   fout = NULL;
+   while (1) {
+      KOsocket* s = new KOsocket(name, 40);
 
-   s->shutdown();
-   delete s;
-   s = NULL;
+      char out[256];
+      
+      sprintf(out, "%s.%d.txt", name, (int)time(NULL));
+      
+      FILE *fout = fopen(out, "w");
+      assert(fout);
+      
+      printf("Writing to %s\n", out);
+      
+      setbuf(fout, NULL);
+      
+      int count = 0;
+      
+      while (1) {
+         time_t t1 = time(NULL);
+         
+         const char *cmd1 = "uart_regfile_status_read 7 8 3\n";
+         s->write(cmd1, strlen(cmd1));
+
+         if (!Wait(s, 10, "reply to uart_regfile_status_read"))
+            break;
+         
+         char reply[102400];
+
+         int rd = s->read(reply, sizeof(reply));
+         int v = atoi(reply);
+         int have = v & (1<<19);
+         if (rd > 0)
+            reply[rd] = 0;
+
+         printf("rd %d, value %d, 0x%x, have %d, reply [%s]\n", rd, v, v, have, reply);
+         
+         time_t t2 = time(NULL);
+         
+         if (!have) {
+            sleep(1);
+            continue;
+         }
+         
+         const char *cmd2 = "simult_capture_of_hw_trig_uart_nios_dacs 7 3 0 0\n";
+         s->write(cmd2, strlen(cmd2));
+         
+         int total = 0;
+         
+         if (!Wait(s, 40, "reply to simult_capture_of_hw_trig_uart_nios_dacs"))
+            break;
+         
+         rd = s->read(reply, sizeof(reply));
+         printf("rd %d, reply [%s]\n", rd, reply);
+         
+         if (rd <= 0)
+            break;
+         
+         total += rd;
+         
+         reply[rd] = 0;
+         
+         time_t t3 = time(NULL);
+         
+         fprintf(fout, "%d %d %d %d %s", count, (int)t1, (int)t2, (int)t3, reply);
+         
+         while (rd > 0) {
+            if (!Wait(s, 3, "data read"))
+               break;
+
+            rd = s->read(reply, sizeof(reply));
+            printf("%d.", rd);
+            if (rd <= 0)
+               break;
+            total += rd;
+            reply[rd] = 0;
+            fprintf(fout, "%s", reply);
+            //if (rd < 750)
+            //break;
+         }
+         printf("done\n");
+         
+         time_t te = time(NULL);
+         
+         printf("event %d, got %d bytes, times %d %d %d\n", count, total, (int)(t2-t1), (int)(t3-t2), (int)(te-t1));
+         
+         fprintf(fout, " total %d elapsed %d %d %d\n", total, (int)(t2-t1), (int)(t3-t2), (int)(te-t1));
+         count++;
+      }
+      
+      fclose(fout);
+      fout = NULL;
+      
+      s->shutdown();
+      delete s;
+      s = NULL;
+   }
 
    return 0;
 }
