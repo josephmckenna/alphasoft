54a850e7db2c4a2f580d028bb381613e8f0cc1cd
agdaq (ALPHA-g prototype DAQ <agdaq@alphagdaq.triumf.ca>
2017-02-01 17:01:06 -0800

    KO - generate midas events instead of writing to ascii file

--- src/feyair.cxx
+++ src/feyair.cxx
@@ -60,14 +60,14 @@ int main(int argc, char* argv[])
 
    TMFeCommon *eqc = new TMFeCommon();
    eqc->EventID = 2;
-   eqc->FrontendName = "feyair";
+   eqc->FrontendName = std::string("feyair_") + name;
    
    TMFeEquipment* eq = new TMFeEquipment(C(std::string("FEAM_") + name));
    eq->Init(eqc);
 
    mfe->RegisterEquipment(eq);
 
-   if (1) { // test events
+   if (0) { // test events
       while (1) {
          char buf[25600];
          eq->ComposeEvent(buf, sizeof(buf));
@@ -94,18 +94,22 @@ int main(int argc, char* argv[])
       
       sprintf(out, "%s.%d.txt", name, (int)time(NULL));
       
-      FILE *fout = fopen(out, "w");
-      assert(fout);
+      FILE *fout = NULL;
+
+      if (0) {
+         fout = fopen(out, "w");
+         assert(fout);
+
+         printf("Writing to %s\n", out);
       
-      printf("Writing to %s\n", out);
-      
-      setbuf(fout, NULL);
+         setbuf(fout, NULL);
+      }
       
       int count = 0;
       
       if (1) {
          char cmdx[256];
-         sprintf(cmdx, "uart_regfile_ctrl_write 0 a %x 0\n", 100+7000/20); // 20ns step
+         sprintf(cmdx, "uart_regfile_ctrl_write 0 a %x 0\n", 7000/20); // 20ns step
          s->write(cmdx, strlen(cmdx));
 
          sleep(1);
@@ -175,9 +179,13 @@ int main(int argc, char* argv[])
          reply[rd] = 0;
          
          time_t t3 = time(NULL);
-         
-         fprintf(fout, "%d %d %d %d %s", count, (int)t1, (int)t2, (int)t3, reply);
-         
+
+         if (fout) {
+            fprintf(fout, "%d %d %d %d %s", count, (int)t1, (int)t2, (int)t3, reply);
+         }
+
+         std::string rrr = reply;
+
          while (rd > 0) {
             if (!Wait(s, 3, "data read"))
                break;
@@ -188,7 +196,13 @@ int main(int argc, char* argv[])
                break;
             total += rd;
             reply[rd] = 0;
-            fprintf(fout, "%s", reply);
+
+            if (fout) {
+               fprintf(fout, "%s", reply);
+            }
+
+            rrr += reply;
+
             //if (rd < 750)
             //break;
          }
@@ -197,13 +211,50 @@ int main(int argc, char* argv[])
          time_t te = time(NULL);
          
          printf("event %d, got %d bytes, times %d %d %d\n", count, total, (int)(t2-t1), (int)(t3-t2), (int)(te-t1));
-         
-         fprintf(fout, " total %d elapsed %d %d %d\n", total, (int)(t2-t1), (int)(t3-t2), (int)(te-t1));
+
+         if (fout) {
+            fprintf(fout, " total %d elapsed %d %d %d\n", total, (int)(t2-t1), (int)(t3-t2), (int)(te-t1));
+         }
+
          count++;
+
+         char buf[2560000];
+         eq->ComposeEvent(buf, sizeof(buf));
+         eq->BkInit(buf, sizeof(buf));
+         unsigned short* xptr = (unsigned short*)eq->BkOpen(buf, bank, TID_WORD);
+         unsigned short* ptr = xptr;
+
+         char*s = (char*)rrr.c_str();
+         for (int i=0; i<1000000; i++) {
+            while (*s == '+')
+               s++;
+            if (*s == 0)
+               break;
+            if (*s == '\n')
+               break;
+            if (*s == '\r')
+               break;
+            int v = strtoul(s, &s, 10);
+            *ptr++ = v;
+         }
+         eq->BkClose(buf, ptr);
+
+         printf("found %d entries\n", (int)(ptr-xptr));
+
+         if (0) {
+            for (int i=0; i<30; i++) {
+               printf("buf[%d]: 0x%08x\n", i, ((int*)buf)[i]);
+            }
+         }
+
+         eq->SendEvent(buf);
+         eq->WriteStatistics();
       }
-      
-      fclose(fout);
-      fout = NULL;
+
+      if (fout) {
+         fclose(fout);
+         fout = NULL;
+      }
       
       s->shutdown();
       delete s;
