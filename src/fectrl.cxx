b78b6ac4a6e20ddf1f6063e411c7116ab1013fdb
agdaq (ALPHA-g prototype DAQ <agdaq@alphagdaq.triumf.ca>
2017-07-05 13:27:00 -0700

    KO - read all parameters from mod8

--- src/fectrl.cxx
+++ src/fectrl.cxx
@@ -115,6 +115,8 @@ public:
    TMFeEquipment* eq = NULL;
    KOtcpConnection* s = NULL;
 
+   std::string fOdbName;
+
 #if 0
    static std::vector<std::string> split(const std::string& s)
    {
@@ -145,7 +147,7 @@ public:
    }
 #endif
 
-   void WR(const char* name, const char* v)
+   void WR(const char* mid, const char* vid, const char* v)
    {
       if (mfe->fShutdown)
          return;
@@ -154,12 +156,66 @@ public:
       path += "/Equipment/";
       path += eq->fName;
       path += "/Readback/";
-      path += name;
-      //printf("Write ODB %s Readback %s: %s\n", C(path), name, v);
+      path += fOdbName;
+      path += "/";
+      path += mid;
+      path += "/";
+      path += vid;
+      //printf("Write ODB %s : %s\n", C(path), v);
       int status = db_set_value(mfe->fDB, 0, C(path), v, strlen(v)+1, 1, TID_STRING);
       if (status != DB_SUCCESS) {
          printf("WR: db_set_value status %d\n", status);
       }
+   }
+         
+   void WRI(const char* mid, const char* vid, const std::vector<int>& v)
+   {
+      if (mfe->fShutdown)
+         return;
+      
+      std::string path;
+      path += "/Equipment/";
+      path += eq->fName;
+      path += "/Readback/";
+      path += fOdbName;
+      path += "/";
+      path += mid;
+      path += "/";
+      path += vid;
+      //printf("Write ODB %s : %s\n", C(path), v);
+      int status = db_set_value(mfe->fDB, 0, C(path), &v[0], sizeof(int)*v.size(), v.size(), TID_INT);
+      if (status != DB_SUCCESS) {
+         printf("WR: db_set_value status %d\n", status);
+      }
+   }
+         
+   void WRB(const char* mid, const char* vid, const std::vector<bool>& v)
+   {
+      if (mfe->fShutdown)
+         return;
+      
+      std::string path;
+      path += "/Equipment/";
+      path += eq->fName;
+      path += "/Readback/";
+      path += fOdbName;
+      path += "/";
+      path += mid;
+      path += "/";
+      path += vid;
+      //printf("Write ODB %s : %s\n", C(path), v);
+
+      BOOL *bb = new BOOL[v.size()];
+      for (unsigned i=0; i<v.size(); i++) {
+         bb[i] = v[i];
+      }
+
+      int status = db_set_value(mfe->fDB, 0, C(path), bb, sizeof(BOOL)*v.size(), v.size(), TID_BOOL);
+      if (status != DB_SUCCESS) {
+         printf("WR: db_set_value status %d\n", status);
+      }
+
+      delete bb;
    }
          
    void WVD(const char* name, const std::vector<double> &v)
@@ -179,11 +235,159 @@ public:
       }
    }
 
+   std::vector<int> JsonToIntArray(const MJsonNode* n)
+   {
+      std::vector<int> vi;
+      const MJsonNodeVector *a = n->GetArray();
+      if (a) {
+         for (unsigned i=0; i<a->size(); i++) {
+            const MJsonNode* ae = a->at(i);
+            if (ae) {
+               vi.push_back(ae->GetInt());
+            }
+         }
+      }
+      return vi;
+   }
+
+   std::vector<bool> JsonToBoolArray(const MJsonNode* n)
+   {
+      std::vector<bool> vb;
+      const MJsonNodeVector *a = n->GetArray();
+      if (a) {
+         for (unsigned i=0; i<a->size(); i++) {
+            const MJsonNode* ae = a->at(i);
+            if (ae) {
+               vb.push_back(ae->GetBool());
+            }
+         }
+      }
+      return vb;
+   }
+
+   std::vector<std::string> GetModules()
+   {
+      std::vector<std::string> modules;
+
+      std::vector<std::string> headers;
+      std::vector<std::string> reply_headers;
+      std::string reply_body;
+
+      KOtcpError e = s->HttpGet(headers, "/read_node?includeMods=y", &reply_headers, &reply_body);
+
+      if (e.error) {
+         eq->SetStatus("http error", "red");
+         mfe->Msg(MERROR, "GetModules", "HttpGet() error %s", e.message.c_str());
+         return modules;
+      }
+
+      MJsonNode* jtree = MJsonNode::Parse(reply_body.c_str());
+      //jtree->Dump();
+
+      const MJsonNode* m = jtree->FindObjectNode("module");
+      if (m) {
+         const MJsonNodeVector *ma = m->GetArray();
+         if (ma) {
+            for (unsigned i=0; i<ma->size(); i++) {
+               const MJsonNode* mae = ma->at(i);
+               if (mae) {
+                  //mae->Dump();
+                  const MJsonNode* maek = mae->FindObjectNode("key");
+                  const MJsonNode* maen = mae->FindObjectNode("name");
+                  if (maek && maen) {
+                     printf("module [%s] %s\n", maek->GetString().c_str(), maen->GetString().c_str());
+                     modules.push_back(maek->GetString());
+                  }
+               }
+            }
+         }
+      }
+
+      delete jtree;
+
+      return modules;
+   }
+
+   std::vector<std::string> GetVariables(const std::string& mid)
+   {
+      std::vector<std::string> variables;
+
+      std::vector<std::string> headers;
+      std::vector<std::string> reply_headers;
+      std::string reply_body;
+
+      std::string url;
+      url += "/read_module?includeVars=y&mid=";
+      url += mid.c_str();
+      url += "&includeData=y";
+
+      KOtcpError e = s->HttpGet(headers, url.c_str(), &reply_headers, &reply_body);
+
+      if (e.error) {
+         eq->SetStatus("http error", "red");
+         mfe->Msg(MERROR, "GetModules", "HttpGet() error %s", e.message.c_str());
+         return variables;
+      }
+
+      MJsonNode* jtree = MJsonNode::Parse(reply_body.c_str());
+      //jtree->Dump();
+
+      const MJsonNode* v = jtree->FindObjectNode("var");
+      if (v) {
+         //v->Dump();
+         const MJsonNodeVector *va = v->GetArray();
+         if (va) {
+            for (unsigned i=0; i<va->size(); i++) {
+               const MJsonNode* vae = va->at(i);
+               if (vae) {
+                  //vae->Dump();
+                  const MJsonNode* vaek = vae->FindObjectNode("key");
+                  const MJsonNode* vaet = vae->FindObjectNode("type");
+                  const MJsonNode* vaed = vae->FindObjectNode("d");
+                  if (vaek && vaet && vaed) {
+                     std::string vid = vaek->GetString();
+                     int type = vaet->GetInt();
+                     //printf("mid [%s] vid [%s] type %d json value %s\n", mid.c_str(), vid.c_str(), type, vaed->Stringify().c_str());
+                     if (type == 1 || type == 2 || type == 3 || type == 4 || type == 5 || type == 6) {
+                        WRI(mid.c_str(), vid.c_str(), JsonToIntArray(vaed));
+                     } else if (type == 11) {
+                        WR(mid.c_str(), vid.c_str(), vaed->GetString().c_str());
+                     } else if (type == 12) {
+                        WRB(mid.c_str(), vid.c_str(), JsonToBoolArray(vaed));
+                     } else {
+                        WR(mid.c_str(), vid.c_str(), vaed->Stringify().c_str());
+                     }
+                     variables.push_back(vid);
+                  }
+               }
+            }
+         }
+      }
+
+      delete jtree;
+
+      return variables;
+   }
+
    void Read()
    {
       s->fReadTimeout = 5*1000;
       s->fHttpKeepOpen = false;
 
+      std::vector<std::string> modules = GetModules();
+
+      if (modules.size() < 1)
+         return;
+
+      for (unsigned i=0; i<modules.size(); i++) {
+         if (modules[i] == "sp32wv")
+            continue;
+         if (modules[i] == "sp16wv")
+            continue;
+         std::vector<std::string> variables = GetVariables(modules[i]);
+      }
+
+#if 0
       KOtcpError e;
 
       std::vector<std::string> headers;
@@ -192,7 +396,10 @@ public:
       std::vector<std::string> reply_headers;
       std::string reply_body;
 
-      e = s->HttpGet(headers, "/", &reply_headers, &reply_body);
+      //e = s->HttpGet(headers, "/read_var?vid=elf_build_str&mid=board&offset=0&len=0&dataOnly=y", &reply_headers, &reply_body);
+      //e = s->HttpGet(headers, "/read_node?includeMods=y&includeVars=y&includeAttrs=y", &reply_headers, &reply_body);
+      //e = s->HttpGet(headers, "/read_node?includeMods=y", &reply_headers, &reply_body);
+      e = s->HttpGet(headers, "/read_module?includeVars=y&mid=board&includeData=y", &reply_headers, &reply_body);
 
       if (e.error) {
          eq->SetStatus("http error", "red");
@@ -200,27 +407,28 @@ public:
          return;
       }
 
-      printf("reply headers:\n");
-      for (unsigned i=0; i<reply_headers.size(); i++)
-         printf("%d: %s\n", i, reply_headers[i].c_str());
+      //printf("reply headers:\n");
+      //for (unsigned i=0; i<reply_headers.size(); i++)
+      //   printf("%d: %s\n", i, reply_headers[i].c_str());
 
       //printf("json: %s\n", reply_body.c_str());
 
+      //WR("DI", reply_body_di.c_str());
+
+      MJsonNode* jtree = MJsonNode::Parse(reply_body.c_str());
+      jtree->Dump();
+
 #if 0
-      WR("DI", reply_body_di.c_str());
-
-      MJsonNode* di = MJsonNode::Parse(reply_body_di.c_str());
-      //di->Dump();
-
       std::vector<double> di_counter_value;
 
       di_counter_value.push_back(di->FindObjectNode("io")->FindObjectNode("di")->GetArray()->at(0)->FindObjectNode("diCounterValue")->GetInt());
 
       printf("di_counter_value %f\n", di_counter_value[0]);
+#endif
 
-      delete di;
+      delete jtree;
 
-      WVD("di_counter_value", di_counter_value);
+      //WVD("di_counter_value", di_counter_value);
 #endif
 
       eq->SetStatus("Ok", "#00FF00");
@@ -268,6 +476,7 @@ int main(int argc, char* argv[])
    a16->mfe = mfe;
    a16->eq = eq;
    a16->s = s;
+   a16->fOdbName = "mod8";
 
    mfe->RegisterRpcHandler(a16);
 
