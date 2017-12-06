71dc2119be91d764d4cf8b3da13434dbb57b6709
Konstantin Olchanski <olchansk@alphagdaq.triumf.ca>
2017-12-06 15:31:16 -0800

    Add more pulser waveform time plots

--- ana/final_module.cxx
+++ ana/final_module.cxx
@@ -93,6 +93,30 @@ public:
    TH2D* h_aw_pad_amp_pc;
 
    // time across to next channel
+   TH1D* h_cal_adc05_0_1 = NULL;
+
+   // time across to another 100 MHz ADC on the same module
+   TH1D* h_cal_adc05_0_4 = NULL;
+   TH1D* h_cal_adc05_0_8 = NULL;
+   TH1D* h_cal_adc05_0_12 = NULL;
+
+   // time across to the 62.5 MHz 32ch ADC on the same module
+   TH1D* h_cal_adc05_0_16 = NULL;
+
+   // time across to next channel 62.5 MHz ADC on the same module
+   TH1D* h_cal_adc05_16_17 = NULL;
+   TH1D* h_cal_adc06_16_20 = NULL;
+
+   // time across to another 62.5 MHz ADC on the same module
+   TH1D* h_cal_adc05_16_24 = NULL;
+   TH1D* h_cal_adc05_16_32 = NULL;
+   TH1D* h_cal_adc05_16_40 = NULL;
+
+   // time across to the next module, 100 MHz and 62.5 MHz ADC
+   TH1D* h_cal_adc_05_06_chan0 = NULL;
+   TH1D* h_cal_adc_05_06_chan16 = NULL;
+
+   // time across to next channel
    TH1D* h_cal_time_pos00_seqsca_04_05 = NULL;
    TH1D* h_cal_time_pos01_seqsca_04_05 = NULL;
    TH1D* h_cal_time_pos02_seqsca_04_05 = NULL;
@@ -108,6 +132,14 @@ public:
    // time across 2 PWB boards
    TH1D* h_cal_time_pos_01_02_seqsca04 = NULL;
    TH1D* h_cal_time_pos_01_02_seqsca05 = NULL;
+   TH1D* h_cal_time_pos_01_03_seqsca04 = NULL;
+   TH1D* h_cal_time_pos_01_03_seqsca05 = NULL;
+
+   // time across 100MHz ADC to PWB
+   TH1D* h_cal_time_pos01_seqsca_04_minus_adc5_0 = NULL;
+   TH1D* h_cal_time_pos01_seqsca_04_minus_adc5_16 = NULL;
+   TH1D* h_cal_time_pos02_seqsca_04_minus_adc5_0 = NULL;
+   TH1D* h_cal_time_pos02_seqsca_04_minus_adc5_16 = NULL;
 
    FinalModule(TARunInfo* runinfo)
       : TARunObject(runinfo)
@@ -172,12 +204,12 @@ public:
       h_aw_352 = new TH1D("h_aw_352", "h_aw_352", NUM_AW, -0.5, NUM_AW-0.5);
 
       h_num_pad_hits = new TH1D("h_num_pad_hits", "number of cathode pad hits", 100, 0, 100);
-      h_pad_time = new TH1D("h_pad_time", "pad hit time", 500, 0, 500);
+      h_pad_time = new TH1D("h_pad_time", "pad hit time", 100, 0, MAX_TIME);
       h_pad_amp = new TH1D("h_pad_amp", "pad hit pulse height", 100, 0, MAX_PAD_AMP);
-      h_pad_amp_time = new TH2D("h_pad_amp_time", "pad p.h vs time", 50, 0, 500, 50, 0, MAX_PAD_AMP);
+      h_pad_amp_time = new TH2D("h_pad_amp_time", "pad p.h vs time", 50, 0, MAX_TIME, 50, 0, MAX_PAD_AMP);
       int npads = MAX_FEAM*MAX_FEAM_PAD_COL*MAX_FEAM_PAD_ROWS;
       h_pad_amp_pad = new TH2D("h_pad_amp_pad", "pad p.h vs pad number", npads, -0.5, npads-0.5, 100, 0, MAX_PAD_AMP);
-      h_pad_time_pad = new TH2D("h_pad_time_pad", "pad time vs pad number", npads, -0.5, npads-0.5, 500, 0, 500);
+      h_pad_time_pad = new TH2D("h_pad_time_pad", "pad time vs pad number", npads, -0.5, npads-0.5, 100, 0, MAX_TIME);
       if (fPH) {
          fPH->cd(2);
          h_pad_amp_pad->Draw();
@@ -187,28 +219,61 @@ public:
 
       h_aw_pad_hits = new TH2D("h_aw_pad_hits", "hits in aw vs hits in pads", 4*8, -0.5, 4*8-0.5, 256, -0.5, 256-0.5);
 
-      h_aw_pad_time = new TH2D("h_aw_pad_time", "time of hits in aw vs pads", 50, 0, 500, 70, 0, 700);
+      h_aw_pad_time = new TH2D("h_aw_pad_time", "time of hits in aw vs pads", 50, 0, MAX_TIME, 50, 0, MAX_TIME);
 
       if (fPH) {
          fPH->cd(1);
       }
 
-      h_aw_pad_time_drift = new TH2D("h_aw_pad_time_drift", "time of hits in aw vs pads, drift region", 50, 0, 500, 70, 0, 700);
+      h_aw_pad_time_drift = new TH2D("h_aw_pad_time_drift", "time of hits in aw vs pads, drift region", 50, 0, 500, 50, 0, MAX_TIME);
 
       h_aw_pad_amp_pc = new TH2D("h_aw_pad_amp_pc", "p.h. of hits in aw vs pads, pc region", 50, 0, MAX_PAD_AMP, 50, 0, MAX_AW_AMP);
 
-      h_cal_time_pos00_seqsca_04_05 = new TH1D("h_cal_time_pos00_seqsca_04_05", "calibration pulse time, pos00 seqsca 05-04", 101, -10, 10);
+      dir->mkdir("pulser")->cd();
 
-      h_cal_time_pos01_seqsca_04_05 = new TH1D("h_cal_time_pos01_seqsca_04_05", "calibration pulse time, pos01 seqsca 05-04", 101, -10, 10);
-      h_cal_time_pos01_seqsca_04_84 = new TH1D("h_cal_time_pos01_seqsca_04_84", "calibration pulse time, pos01 seqsca 84-04", 101, -10, 10);
-      h_cal_time_pos01_seqsca_04_164 = new TH1D("h_cal_time_pos01_seqsca_04_164", "calibration pulse time, pos01 seqsca 164-04", 101, -10, 10);
+      // ADC timing
 
-      h_cal_time_pos02_seqsca_04_05 = new TH1D("h_cal_time_pos02_seqsca_04_05", "calibration pulse time, pos02 seqsca 05-04", 101, -10, 10);
-      h_cal_time_pos02_seqsca_04_84 = new TH1D("h_cal_time_pos02_seqsca_04_84", "calibration pulse time, pos02 seqsca 84-04", 101, -10, 10);
-      h_cal_time_pos02_seqsca_04_164 = new TH1D("h_cal_time_pos02_seqsca_04_164", "calibration pulse time, pos02 seqsca 164-04", 101, -10, 10);
+      h_cal_adc05_0_1 = new TH1D("h_cal_time_adc05_0_1", "calibration pulse time, adc05 chan 1-0; time, ns", 101, -50, 50);
+      h_cal_adc05_0_4 = new TH1D("h_cal_time_adc05_0_4", "calibration pulse time, adc05 chan 4-0; time, ns", 101, -50, 50);
+      h_cal_adc05_0_8 = new TH1D("h_cal_time_adc05_0_8", "calibration pulse time, adc05 chan 8-0; time, ns", 101, -50, 50);
+      h_cal_adc05_0_12 = new TH1D("h_cal_time_adc05_0_12", "calibration pulse time, adc05 chan 12-0; time, ns", 101, -50, 50);
+      h_cal_adc05_0_16 = new TH1D("h_cal_time_adc05_0_16", "calibration pulse time, adc05 chan 16-0; time, ns", 101, -50, 50);
 
-      h_cal_time_pos_01_02_seqsca04 = new TH1D("h_cal_time_pos_01_02_seqsca04", "calibration pulse time, pos02-pos01 seqsca 04", 101, -10, 10);
-      h_cal_time_pos_01_02_seqsca05 = new TH1D("h_cal_time_pos_01_02_seqsca05", "calibration pulse time, pos02-pos01 seqsca 05", 101, -10, 10);
+      h_cal_adc_05_06_chan0 = new TH1D("h_cal_time_adc_05_06_chan0", "calibration pulse time, adc06 - adc05 chan 0; time, ns", 101, -50, 50);
+      h_cal_adc_05_06_chan16 = new TH1D("h_cal_time_adc_05_06_chan16", "calibration pulse time, adc06 - adc05 chan 16; time, ns", 101, -50, 50);
+
+      h_cal_adc05_16_17 = new TH1D("h_cal_time_adc05_16_17", "calibration pulse time, adc05 chan 17-16; time, ns", 101, -50, 50);
+      h_cal_adc06_16_20 = new TH1D("h_cal_time_adc06_16_20", "calibration pulse time, adc06 chan 20-16; time, ns", 101, -50, 50);
+
+      h_cal_adc05_16_24 = new TH1D("h_cal_time_adc05_16_24", "calibration pulse time, adc05 chan 24-16; time, ns", 101, -50, 50);
+      h_cal_adc05_16_32 = new TH1D("h_cal_time_adc05_16_32", "calibration pulse time, adc05 chan 32-16; time, ns", 101, -50, 50);
+      h_cal_adc05_16_40 = new TH1D("h_cal_time_adc05_16_40", "calibration pulse time, adc05 chan 40-16; time, ns", 101, -50, 50);
+
+
+      // PWB timing
+
+      h_cal_time_pos00_seqsca_04_05 = new TH1D("h_cal_time_pos00_seqsca_04_05", "calibration pulse time, pos00 seqsca 05-04", 101, -50, 50);
+
+      h_cal_time_pos01_seqsca_04_05 = new TH1D("h_cal_time_pos01_seqsca_04_05", "calibration pulse time, pos01 seqsca 05-04", 101, -50, 50);
+      h_cal_time_pos01_seqsca_04_84 = new TH1D("h_cal_time_pos01_seqsca_04_84", "calibration pulse time, pos01 seqsca 84-04", 101, -50, 50);
+      h_cal_time_pos01_seqsca_04_164 = new TH1D("h_cal_time_pos01_seqsca_04_164", "calibration pulse time, pos01 seqsca 164-04", 101, -50, 50);
+
+      h_cal_time_pos02_seqsca_04_05 = new TH1D("h_cal_time_pos02_seqsca_04_05", "calibration pulse time, pos02 seqsca 05-04", 101, -50, 50);
+      h_cal_time_pos02_seqsca_04_84 = new TH1D("h_cal_time_pos02_seqsca_04_84", "calibration pulse time, pos02 seqsca 84-04", 101, -50, 50);
+      h_cal_time_pos02_seqsca_04_164 = new TH1D("h_cal_time_pos02_seqsca_04_164", "calibration pulse time, pos02 seqsca 164-04", 101, -50, 50);
+
+      h_cal_time_pos_01_02_seqsca04 = new TH1D("h_cal_time_pos_01_02_seqsca04", "calibration pulse time, pos02-pos01 seqsca 04; time, ns", 101, -50, 50);
+      h_cal_time_pos_01_02_seqsca05 = new TH1D("h_cal_time_pos_01_02_seqsca05", "calibration pulse time, pos02-pos01 seqsca 05; time, ns", 101, -50, 50);
+
+      h_cal_time_pos_01_03_seqsca04 = new TH1D("h_cal_time_pos_01_03_seqsca04", "calibration pulse time, pos03-pos01 seqsca 04; time, ns", 101, -50, 50);
+      h_cal_time_pos_01_03_seqsca05 = new TH1D("h_cal_time_pos_01_03_seqsca05", "calibration pulse time, pos03-pos01 seqsca 05; time, ns", 101, -50, 50);
+
+      // timing across PWB and Alpha16
+
+      h_cal_time_pos01_seqsca_04_minus_adc5_0 = new TH1D("h_cal_time_pos01_seqsca_04_minus_adc5_0", "calibration pulse time, PWB pos01 - adc05 chan 0; time, ns", 101, -50, 50);
+      h_cal_time_pos01_seqsca_04_minus_adc5_16 = new TH1D("h_cal_time_pos01_seqsca_04_minus_adc5_16", "calibration pulse time, PWB pos01 - adc05 chan 16; time, ns", 101, -50, 50);
+      h_cal_time_pos02_seqsca_04_minus_adc5_0 = new TH1D("h_cal_time_pos02_seqsca_04_minus_adc5_0", "calibration pulse time, PWB pos02 - adc05 chan 0; time, ns", 101, -50, 50);
+      h_cal_time_pos02_seqsca_04_minus_adc5_16 = new TH1D("h_cal_time_pos02_seqsca_04_minus_adc5_16", "calibration pulse time, PWB pos02 - adc05 chan 16; time, ns", 101, -50, 50);
    }
 
    void EndRun(TARunInfo* runinfo)
@@ -286,6 +351,20 @@ public:
          printf("Have AgEvent: %d %d, %f %f, diff %f, ts 0x%08x 0x%08x, ns: %12d %12d, diff %d\n", age->a16->counter, age->feam->counter, atr, ftr, dtr, a16_tsr, feam_tsr, a16_tsr_ns, feam_tsr_ns, dts_ns);
       }
 
+      double adc5_0  = -1010;
+      double adc5_1  = -1020;
+      double adc5_4  = -1030;
+      double adc5_8  = -1040;
+      double adc5_12 = -1050;
+      double adc5_16 = -1150;
+      double adc5_17 = -1160;
+      double adc5_24 = -1170;
+      double adc5_32 = -1180;
+      double adc5_40 = -1190;
+      double adc6_0  = -2060;
+      double adc6_16 = -2150;
+      double adc6_20 = -2160;
+
       if (eawh) {
          if (1) {
             printf("AW event %d, time %f, anode wire hits: %d\n", ef->fEvent->counter, ef->fEvent->time, (int)eawh->fAwHits.size());
@@ -294,13 +373,36 @@ public:
          h_num_aw_hits->Fill(eawh->fAwHits.size());
 
          for (unsigned j=0; j<eawh->fAwHits.size(); j++) {
-            h_aw_time->Fill(eawh->fAwHits[j].time);
-            h_aw_amp->Fill(eawh->fAwHits[j].amp);
-            h_aw_amp_time->Fill(eawh->fAwHits[j].time, eawh->fAwHits[j].amp);
+            int wire = eawh->fAwHits[j].wire;
+            double time = eawh->fAwHits[j].time;
+            double amp = eawh->fAwHits[j].amp;
 
-            h_aw_map->Fill(eawh->fAwHits[j].wire);
-            h_aw_map_time->Fill(eawh->fAwHits[j].wire, eawh->fAwHits[j].time);
-            h_aw_map_amp->Fill(eawh->fAwHits[j].wire, eawh->fAwHits[j].amp);
+            h_aw_time->Fill(time);
+            h_aw_amp->Fill(amp);
+            h_aw_amp_time->Fill(time, amp);
+
+            h_aw_map->Fill(wire);
+            h_aw_map_time->Fill(wire, time);
+            h_aw_map_amp->Fill(wire, amp);
+
+            if (eawh->fAwHits[j].module == 5) {
+               if (eawh->fAwHits[j].channel == 0) adc5_0 = time;
+               if (eawh->fAwHits[j].channel == 1) adc5_1 = time;
+               if (eawh->fAwHits[j].channel == 4) adc5_4 = time;
+               if (eawh->fAwHits[j].channel == 8) adc5_8 = time;
+               if (eawh->fAwHits[j].channel == 12) adc5_12 = time;
+               if (eawh->fAwHits[j].channel == 16) adc5_16 = time;
+               if (eawh->fAwHits[j].channel == 17) adc5_17 = time;
+               if (eawh->fAwHits[j].channel == 24) adc5_24 = time;
+               if (eawh->fAwHits[j].channel == 32) adc5_32 = time;
+               if (eawh->fAwHits[j].channel == 40) adc5_40 = time;
+            }
+
+            if (eawh->fAwHits[j].module == 6) {
+               if (eawh->fAwHits[j].channel == 0) adc6_0 = time;
+               if (eawh->fAwHits[j].channel == 16) adc6_16 = time;
+               if (eawh->fAwHits[j].channel == 20) adc6_20 = time;
+            }
 
             for (unsigned k=0; k<eawh->fAwHits.size(); k++) {
                if (k==j)
@@ -341,6 +443,9 @@ public:
       double pos02_seqsca05 = -60;
       double pos02_seqsca84 = -150;
       double pos02_seqsca164 = -160;
+
+      double pos03_seqsca04 = -70;
+      double pos03_seqsca05 = -80;
 
       if (eph) {
          if (1) {
@@ -384,6 +489,11 @@ public:
                if (seqsca == 164) pos02_seqsca164 = time;
             }
 
+            if (pos==3) {
+               if (seqsca == 4) pos03_seqsca04 = time;
+               if (seqsca == 5) pos03_seqsca05 = time;
+            }
+
             h_pad_time->Fill(time);
             h_pad_amp->Fill(amp);
             h_pad_amp_time->Fill(time, amp);
@@ -423,6 +533,40 @@ public:
                 && (pos02_seqsca04 > 0)
                 && (pos02_seqsca05 > 0))) {
 
+#if 0
+         printf("ADC times: adc05: %f %f %f %f %f, adc06: %f\n",
+                adc5_0,
+                adc5_1,
+                adc5_4,
+                adc5_8,
+                adc5_12,
+                adc6_0
+                );
+#endif
+
+         if (adc5_0 > 0) {
+            h_cal_adc05_0_1->Fill(adc5_1-adc5_0);
+            h_cal_adc05_0_4->Fill(adc5_4-adc5_0);
+            h_cal_adc05_0_8->Fill(adc5_8-adc5_0);
+            h_cal_adc05_0_12->Fill(adc5_12-adc5_0);
+            h_cal_adc05_0_16->Fill(adc5_16-adc5_0);
+
+            h_cal_adc05_16_17->Fill(adc5_17-adc5_16);
+            h_cal_adc05_16_24->Fill(adc5_24-adc5_16);
+            h_cal_adc05_16_32->Fill(adc5_32-adc5_16);
+            h_cal_adc05_16_40->Fill(adc5_40-adc5_16);
+
+            h_cal_adc06_16_20->Fill(adc6_20-adc6_16);
+
+            h_cal_adc_05_06_chan0->Fill(adc6_0-adc5_0);
+            h_cal_adc_05_06_chan16->Fill(adc6_16-adc5_16);
+         }
+
+         double pulse_width = 5350.0 + 30.0;
+         double xpad = pos01_seqsca04;
+         double t = xpad - adc5_0 - pulse_width;
+         printf("aw %.1f pad %.1f %.1f, diff %.1f\n", adc5_0, pos01_seqsca04, xpad, t);
+
          h_cal_time_pos00_seqsca_04_05->Fill(pos00_seqsca05-pos00_seqsca04);
 
          h_cal_time_pos01_seqsca_04_05->Fill(pos01_seqsca05-pos01_seqsca04);
@@ -436,6 +580,15 @@ public:
 
          h_cal_time_pos_01_02_seqsca04->Fill(pos02_seqsca04-pos01_seqsca04);
          h_cal_time_pos_01_02_seqsca05->Fill(pos02_seqsca05-pos01_seqsca05);
+
+         h_cal_time_pos_01_03_seqsca04->Fill(pos03_seqsca04-pos01_seqsca04);
+         h_cal_time_pos_01_03_seqsca05->Fill(pos03_seqsca05-pos01_seqsca05);
+
+         h_cal_time_pos01_seqsca_04_minus_adc5_0->Fill(pos01_seqsca04 - adc5_0 - pulse_width);
+         h_cal_time_pos01_seqsca_04_minus_adc5_16->Fill(pos01_seqsca04 - adc5_16 - pulse_width);
+
+         h_cal_time_pos02_seqsca_04_minus_adc5_0->Fill(pos02_seqsca04 - adc5_0 - pulse_width);
+         h_cal_time_pos02_seqsca_04_minus_adc5_16->Fill(pos02_seqsca04 - adc5_16 - pulse_width);
       }
 
 
