407d405a77b32cb59ae60221e1d0922453c71df0
Konstantin Olchanski <olchansk@alphagdaq.triumf.ca>
2018-01-18 20:36:43 -0800

    PWB00 replaced with PWB77.

--- ana/adc_module.cxx
+++ ana/adc_module.cxx
@@ -453,7 +453,7 @@ public:
       fH = new PlotHistograms();
 
       TDirectory* fft_file = aw->mkdir("noise_fft");
-      TDirectory* fft_tmp = runinfo->fRoot->fgDir->mkdir("noise_fft");
+      TDirectory* fft_tmp = runinfo->fRoot->fgDir->mkdir("aw_noise_fft");
 
       if (fFlags->fFft) {
          fAN16 = new AnalyzeNoise("adc16", fft_file, fft_tmp, 701);
