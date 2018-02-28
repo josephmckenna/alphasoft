960c4571654dda33ba16bbad9e57a1ec9b1ca20b
a-capra <andrea.capra85@gmail.com>
2018-02-28 14:19:46 -0800

    Added occupancy histos

--- ana/histo_module.cxx
+++ ana/histo_module.cxx
@@ -30,7 +30,8 @@ using std::endl;
 class HistoRun: public TARunObject
 {
 public:
-   TH2D* h_aw_padcol;
+   TH2D* h_aw_padcol; 
+   TH2D* h_aw_padcol_nocoinc;
    TH1D* h_timediff;
    TH1D* h_atimes;
    TH1D* h_ptimes;
@@ -56,6 +57,11 @@ public:
 
    TH2D *h_amp_a;
    TH2D *h_amp_p;
+
+   TH1D* hoccaw;
+   TH1D* hoccawpos;
+   TH2D* hoccpd;
+   TH2D* hoccpdpos;
 
    TH1D* h3DDist;
    TH1D* hRphiDist;
@@ -99,13 +105,17 @@ public:
       int naw = TPCBase::TPCBaseInstance()->GetNumberOfAnodeWires();
       int nps = TPCBase::TPCBaseInstance()->GetNumberPadsRow();
       h_aw_padcol = new TH2D("h_aw_padcol", "anode pad coincidences;anode;pad sector", naw, 0, naw, nps, 0, nps);
+      h_aw_padcol_nocoinc = new TH2D("h_aw_padcol_nocoinc", "anode pad no coincidences;anode;pad sector", naw, 0, naw, nps, 0, nps);
       h_timediff = new TH1D("h_timediff", "pad / anode time difference;t_a-t_p",4000,-2000,2000);
       h_atimes = new TH1D("h_atimes", "anode times;t in ns",1000,0,10000);
       h_ptimes = new TH1D("h_ptimes", "pad times;t in ns",500,0,8000);
       h_firsttimediff = new TH1D("h_firsttimediff", "pad / anode first time difference;t_a0-t_p0",4000,-2000,2000);
       h_firsttime = new TH2D("h_firsttime", "pad / anode first time;t_a0;t_p0",1000,0,10000,500,0,8000);
 
-      h_times_aw_p = new TH2D("h_times_aw_p", "pad / anode times;t_a;t_p",1000,0,10000,500,0,8000);
+      h_times_aw_p = new TH2D("h_times_aw_p", "pad / anode times;t_a;t_p",4000,-2000.,2000.,4000,-2000.,2000.);
+      h_times_aw_p->SetMarkerStyle(8);
+      h_times_aw_p->SetMarkerColor(kBlack);
+
       h_times_aw_p_match = new TH2D("h_times_aw_p_match", "pad / anode times matched;t_a;t_p",1000,0,10000,500,0,8000);
       h_timediff_aw_p_match = new TH1D("h_timediff_aw_p_match", "pad / anode time difference;t_a-t_p",4000,-2000,2000);
 
@@ -125,50 +135,64 @@ public:
       h_amp_a = new TH2D("h_amp_a","Amplitude of waveform of anode;anode;ADC[?]", naw, 0, naw,2000,0,10000);
       h_amp_p = new TH2D("h_amp_p","Amplitude of waveform of pad;pad;ADC[?]", nps*TPCBase::TPCBaseInstance()->GetNumberPadsColumn(), 0, nps*TPCBase::TPCBaseInstance()->GetNumberPadsColumn(),2000,0,10000);
 
+      hoccaw = new TH1D("hoccaw","AW occupancy;AW",TPCBase::TPCBaseInstance()->GetNumberOfAnodeWires(),
+                        0.,double(TPCBase::TPCBaseInstance()->GetNumberOfAnodeWires()));
+      hoccawpos = new TH1D("hoccawpos","AW pos occupancy;#phi [deg]",TPCBase::TPCBaseInstance()->GetNumberOfAnodeWires(),
+                           0.,360.);
+
+      hoccpd = new TH2D("hoccpd","PAD occupancy;PAD",TPCBase::TPCBaseInstance()->GetNumberPadsRow(),
+                        0.,double(TPCBase::TPCBaseInstance()->GetNumberPadsRow()),
+                        TPCBase::TPCBaseInstance()->GetNumberPadsColumn(),
+                        0.,double(TPCBase::TPCBaseInstance()->GetNumberPadsColumn()) );
+      hoccpdpos = new TH2D("hoccpdpos","PAD pos occupancy;#phi [deg]",TPCBase::TPCBaseInstance()->GetNumberPadsRow(),
+                           0.,360.,
+                           TPCBase::TPCBaseInstance()->GetNumberPadsColumn(),
+                           -TPCBase::TPCBaseInstance()->GetHalfLengthZ(),TPCBase::TPCBaseInstance()->GetHalfLengthZ());
+
       runinfo->fRoot->fOutputFile->mkdir("analysis")->cd();
-      h3DDist = new TH1D("h3DDist","3D Distance;d [mm];Points",1000,0.,100.);
-      hRphiDist = new TH1D("hRphiDist","#sqrt{R#phi^{2}+z^{2}};#delta [mm];Points",
-                           1000,0.,100.);
+      // h3DDist = new TH1D("h3DDist","3D Distance;d [mm];Points",1000,0.,100.);
+      // hRphiDist = new TH1D("hRphiDist","#sqrt{R#phi^{2}+z^{2}};#delta [mm];Points",
+      //                      1000,0.,100.);
 
-      hpoints2D = new TH2D("hpoints2D","Points;#phi [rad];r [mm];Points",
-                           1200,0.,TMath::TwoPi(),
-                           1000,
-                           TPCBase::TPCBaseInstance()->GetCathodeRadius(true),
-                           TPCBase::TPCBaseInstance()->GetROradius(true));
-      hpoints3D = new TH2D("hpoints3D","Points;r#phi [mm];z [mm];Points",
-                           2000,
-                           0.,
-                           TPCBase::TPCBaseInstance()->GetROradius(true)*TMath::TwoPi(),
-                           2500,
-                           -TPCBase::TPCBaseInstance()->GetHalfLengthZ(true),
-                           TPCBase::TPCBaseInstance()->GetHalfLengthZ(true));
+      // hpoints2D = new TH2D("hpoints2D","Points;#phi [rad];r [mm];Points",
+      //                      1200,0.,TMath::TwoPi(),
+      //                      1000,
+      //                      TPCBase::TPCBaseInstance()->GetCathodeRadius(true),
+      //                      TPCBase::TPCBaseInstance()->GetROradius(true));
+      // hpoints3D = new TH2D("hpoints3D","Points;r#phi [mm];z [mm];Points",
+      //                      2000,
+      //                      0.,
+      //                      TPCBase::TPCBaseInstance()->GetROradius(true)*TMath::TwoPi(),
+      //                      2500,
+      //                      -TPCBase::TPCBaseInstance()->GetHalfLengthZ(true),
+      //                      TPCBase::TPCBaseInstance()->GetHalfLengthZ(true));
 
-      hpointsXY = new TH2D("hpointsXY","Points;x [mm];y [mm];Points",
-                           1000,
-                           -TPCBase::TPCBaseInstance()->GetROradius(true),
-                           TPCBase::TPCBaseInstance()->GetROradius(true),
-                           1000,
-                           -TPCBase::TPCBaseInstance()->GetROradius(true),
-                           TPCBase::TPCBaseInstance()->GetROradius(true));
-      hpointsZR = new TH2D("hpointsZR","Points;z [mm];r [mm];Points",
-                           1000,
-                           -TPCBase::TPCBaseInstance()->GetHalfLengthZ(true),
-                           TPCBase::TPCBaseInstance()->GetHalfLengthZ(true),
-                           1000,
-                           TPCBase::TPCBaseInstance()->GetCathodeRadius(true),
-                           TPCBase::TPCBaseInstance()->GetROradius(true));
+      // hpointsXY = new TH2D("hpointsXY","Points;x [mm];y [mm];Points",
+      //                      1000,
+      //                      -TPCBase::TPCBaseInstance()->GetROradius(true),
+      //                      TPCBase::TPCBaseInstance()->GetROradius(true),
+      //                      1000,
+      //                      -TPCBase::TPCBaseInstance()->GetROradius(true),
+      //                      TPCBase::TPCBaseInstance()->GetROradius(true));
+      // hpointsZR = new TH2D("hpointsZR","Points;z [mm];r [mm];Points",
+      //                      1000,
+      //                      -TPCBase::TPCBaseInstance()->GetHalfLengthZ(true),
+      //                      TPCBase::TPCBaseInstance()->GetHalfLengthZ(true),
+      //                      1000,
+      //                      TPCBase::TPCBaseInstance()->GetCathodeRadius(true),
+      //                      TPCBase::TPCBaseInstance()->GetROradius(true));
 
       htw = new TH2D("htw","Drift Time Vs Wire;t [ns];anode",10000,0.,10000.,256,0.,256.);
 
-      h3DDistVsR = new TH2D("h3DDistVsR","3D Distance;d [mm];r [mm];Points",100,0.,100.,
-                            81,
-                            TPCBase::TPCBaseInstance()->GetCathodeRadius(true),
-                            TPCBase::TPCBaseInstance()->GetROradius(true));
-      hRphiDistVsR = new TH2D("hRphiDistVsR","#sqrt{R#phi^{2}+z^{2}};r [mm];#delta [mm];Points",
-                              100,0.,100.,
-                              81,
-                              TPCBase::TPCBaseInstance()->GetCathodeRadius(true),
-                              TPCBase::TPCBaseInstance()->GetROradius(true));
+      // h3DDistVsR = new TH2D("h3DDistVsR","3D Distance;d [mm];r [mm];Points",100,0.,100.,
+      //                       81,
+      //                       TPCBase::TPCBaseInstance()->GetCathodeRadius(true),
+      //                       TPCBase::TPCBaseInstance()->GetROradius(true));
+      // hRphiDistVsR = new TH2D("hRphiDistVsR","#sqrt{R#phi^{2}+z^{2}};r [mm];#delta [mm];Points",
+      //                         100,0.,100.,
+      //                         81,
+      //                         TPCBase::TPCBaseInstance()->GetCathodeRadius(true),
+      //                         TPCBase::TPCBaseInstance()->GetROradius(true));
 
       hNhits = new TH1D("hNhits","Number of Spacepoints per Event;Points [a.u.];Events [a.u.]",
                         2000,0.,2000.);
@@ -187,10 +211,10 @@ public:
       h_ptimes->SetFillStyle(0);
       h_atimes->SetLineColor(kBlue);
       h_ptimes->SetLineColor(kRed);
-      h_atimes->SetMinimum(0);
-      h_atimes->SetMaximum(2);
-      h_atimes->Draw();
-      h_ptimes->Draw("same");
+      h_atimes->SetMinimum(0.);
+      // h_atimes->SetMaximum(2);
+      // h_atimes->Draw();
+      // h_ptimes->Draw("same");
    }
 
    void EndRun(TARunInfo* runinfo)
@@ -235,11 +259,11 @@ public:
       TStoreEvent* anEvent = analysis_flow->fEvent;
 
 
-      double t_pad_first = 1.e6;
-      double t_aw_first = 1.e6;
+      double t_pad_first = 9.e9;
+      double t_aw_first = 9.e9;
       // h_atimes->Reset();
       // h_ptimes->Reset();
-       int unmatchedAnodeSignals = anEvent->GetNumberOfUnmatchedAnodes(),
+      int unmatchedAnodeSignals = anEvent->GetNumberOfUnmatchedAnodes(),
          unmatchedPadSignals = anEvent->GetNumberOfUnmatchedPads();
       //      anEvent.GetNumberOfUnmatched(unmatchedAnodeSignals,unmatchedPadSignals);
       h_unmatched->Fill(unmatchedAnodeSignals,unmatchedPadSignals);
@@ -255,103 +279,135 @@ public:
       bool first = true;
       cout << "NA: " << NA << endl;
       cout << "NP: " << NP << endl;
-      for(auto sa: SigFlow->awSig){
-         if(sa.t < t_aw_first){
-            t_aw_first = sa.t;
-            std::cout<<"HistoModule: Early "<<t_aw_first<<" @ AW #"<<sa.i<<std::endl;}
-         h_atimes->Fill(sa.t);
-         double r, phi;
-         TPCBase::TPCBaseInstance()->GetAnodePosition(sa.i, r, phi, true);
-         std::pair<int,int> pad = TPCBase::TPCBaseInstance()->FindPad(0, phi);
-         for(auto sp: SigFlow->pdSig){
-            if(first){
-               if(sp.t < t_pad_first) t_pad_first = sp.t;
-               h_ptimes->Fill(sp.t);
-            }
-            //cout << "KKKK " << sa.i << '\t' << sp.sec << endl;
-            h_timediff->Fill(sa.t-sp.t);
-            h_times_aw_p->Fill(sa.t, sp.t);
+      for(auto sa: SigFlow->awSig)
+         {
+            if(sa.t < t_aw_first)
+               {
+                  t_aw_first = sa.t;
+                  //std::cout<<"HistoModule: Early "<<t_aw_first<<" @ AW #"<<sa.i<<std::endl;
+               }
 
-            if(sp.sec == pad.first){
-               h_times_aw_p_match->Fill(sa.t, sp.t);
-               h_timediff_aw_p_match->Fill(sa.t-sp.t);
-            }
+            h_atimes->Fill(sa.t);
+            htw->Fill(sa.t,sa.i);
 
-            if(abs(sa.t-sp.t) < 16)
-               h_aw_padcol->Fill(sa.i, sp.sec);
-         }// pads loop
-         first = false;
-      }// aw loop
-      cTimes->Modified();
-      cTimes->Update();
+            double r, phi;
+            TPCBase::TPCBaseInstance()->GetAnodePosition(sa.i, r, phi, true);
+            std::pair<int,int> pad = TPCBase::TPCBaseInstance()->FindPad(0., phi);
+            
+            for(auto sp: SigFlow->pdSig)
+               {
+                  if(first)
+                     {
+                        if(sp.t < t_pad_first) 
+                           t_pad_first = sp.t;
+                        h_ptimes->Fill(sp.t);
+                     }
+                  //cout << "KKKK " << sa.i << '\t' << sp.sec << endl;
+                  h_timediff->Fill(sa.t-sp.t);
+                  h_times_aw_p->Fill(sa.t, sp.t);
+                  
+                  if(sp.sec == pad.first)
+                     {
+                        h_times_aw_p_match->Fill(sa.t, sp.t);
+                        h_timediff_aw_p_match->Fill(sa.t-sp.t);
+                     }
+
+                  if( fabs(sa.t-sp.t) < 16. )
+                     h_aw_padcol->Fill(sa.i, sp.sec);
+
+                  h_aw_padcol_nocoinc->Fill(sa.i, sp.sec);
+
+               }// pads loop
+            first = false;
+         }// aw loop
+      // cTimes->Modified();
+      // cTimes->Update();
+
 
       h_firsttimediff->Fill(t_aw_first-t_pad_first);
       h_firsttime->Fill(t_aw_first,t_pad_first);
 
       const vector<TPCBase::electrode> &anodes = SigFlow->awIndex;
       const vector<double> &resRMS_a = SigFlow->awResRMS;
-      for(unsigned int i= 0; i < anodes.size(); i++){
-         h_resRMS_a->Fill(anodes[i].i, resRMS_a[i]);
-         int wf_aw_index = SigFlow->AWwf[i].i;
-         const vector<int> *wf = SigFlow->AWwf[i].wf;
-         double ped = SigFlow->awPed[i];
-         h_amp_a->Fill( wf_aw_index, (*std::max_element( wf->begin(),
-                                                         wf->end() )) - ped );
-      }
+      for(unsigned int i = 0; i < anodes.size(); i++)
+         {
+
+            hoccaw->Fill(anodes[i].i);
+            double r, phi;
+            TPCBase::TPCBaseInstance()->GetAnodePosition(anodes[i].i, r, phi, true);
+            hoccawpos->Fill(phi*TMath::RadToDeg());
+            
+            h_resRMS_a->Fill(anodes[i].i, resRMS_a[i]);
+            int wf_aw_index = SigFlow->AWwf[i].i;
+          
+            const vector<int> *wf = SigFlow->AWwf[i].wf;
+            double ped = SigFlow->awPed[i];
+            h_amp_a->Fill( wf_aw_index, (*std::max_element( wf->begin(),
+                                                            wf->end() )) - ped );
+         }
       const vector<TPCBase::electrode> &pads = SigFlow->pdIndex;
       const vector<double> &resRMS_p = SigFlow->pdResRMS;
-      for(unsigned int i= 0; i < pads.size(); i++){
-         h_resRMS_p->Fill(pads[i].sec*TPCBase::TPCBaseInstance()->GetNumberPadsColumn()+pads[i].i, resRMS_p[i]);
-         const vector<int> *wf = SigFlow->PADwf[i].wf;
-         double ped = SigFlow->pdPed[i];
-         h_amp_p->Fill( SigFlow->PADwf[i].sec*TPCBase::TPCBaseInstance()->GetNumberPadsColumn()+SigFlow->PADwf[i].i,
-                        (*std::max_element( wf->begin(),
-                                            wf->end() )) - ped );
-      }
+      for(unsigned int i= 0; i < pads.size(); i++)
+         {
+            hoccpd->Fill(pads[i].sec,pads[i].i);
+            double z,phi;
+            TPCBase::TPCBaseInstance()->GetPadPosition(pads[i].i, pads[i].sec, z, phi);
+            hoccpdpos->Fill(phi*TMath::RadToDeg(),z);
+
+            h_resRMS_p->Fill(pads[i].sec*TPCBase::TPCBaseInstance()->GetNumberPadsColumn()+pads[i].i, resRMS_p[i]);
+            const vector<int> *wf = SigFlow->PADwf[i].wf;
+
+            double ped = SigFlow->pdPed[i];
+            h_amp_p->Fill( SigFlow->PADwf[i].sec*TPCBase::TPCBaseInstance()->GetNumberPadsColumn()+SigFlow->PADwf[i].i,
+                           (*std::max_element( wf->begin(),
+                                               wf->end() )) - ped );
+         }
+
       for(auto sa: SigFlow->awSig)
          htH_anode->Fill(sa.t,sa.height);
       for(auto sp: SigFlow->pdSig)
          htH_pad->Fill(sp.t,sp.height);
 
-      const TObjArray* points = anEvent->GetSpacePoints();
-      for(int i=0; i<points->GetEntries(); ++i)
-         {
-            TSpacePoint* spi = (TSpacePoint*) points->At(i);
-            double ri = spi->GetR(),
-               phii = spi->GetPhi(),
-               zi = spi->GetZ(),
-               yi = spi->GetY(),
-               xi = spi->GetX();
-            hpoints2D->Fill(phii,ri);
-            double rphii = ri*phii;
-            hpoints3D->Fill(rphii,zi);
+      // RECO HISTOs
+      // const TObjArray* points = anEvent->GetSpacePoints();
+      // for(int i=0; i<points->GetEntries(); ++i)
+      //    {
+      //       TSpacePoint* spi = (TSpacePoint*) points->At(i);
+      //       double ri = spi->GetR(),
+      //          phii = spi->GetPhi(),
+      //          zi = spi->GetZ(),
+      //          yi = spi->GetY(),
+      //          xi = spi->GetX();
+      //       hpoints2D->Fill(phii,ri);
+      //       double rphii = ri*phii;
+      //       hpoints3D->Fill(rphii,zi);
 
-            hpointsXY->Fill(xi,yi);
-            hpointsZR->Fill(zi,ri);
+      //       hpointsXY->Fill(xi,yi);
+      //       hpointsZR->Fill(zi,ri);
 
-            double td = spi->GetTime(),
-               aw = double(spi->GetWire());
-            htw->Fill(td,aw);
+      //       double td = spi->GetTime(),
+      //          aw = double(spi->GetWire());
+      //       htw->Fill(td,aw);
 
-            for(int j=0; j<points->GetEntries(); ++j)
-               {
-                  if( i == j ) continue;
+      //       for(int j=0; j<points->GetEntries(); ++j)
+      //          {
+      //             if( i == j ) continue;
 
-                  TSpacePoint* spj = (TSpacePoint*) points->At(j);
-                  double rj = spj->GetR();
+      //             TSpacePoint* spj = (TSpacePoint*) points->At(j);
+      //             double rj = spj->GetR();
 
-                  if( ri<rj ) continue;
+      //             if( ri<rj ) continue;
 
-                  double dist = spi->Distance( spj );
-                  double rpd = spi->DistanceRphi( spj );
+      //             double dist = spi->Distance( spj );
+      //             double rpd = spi->DistanceRphi( spj );
 
-                  h3DDist->Fill(dist);
-                  hRphiDist->Fill(rpd);
+      //             h3DDist->Fill(dist);
+      //             hRphiDist->Fill(rpd);
 
-                  h3DDistVsR->Fill(dist,ri);
-                  hRphiDistVsR->Fill(rpd,ri);
-               }
-         }
+      //             h3DDistVsR->Fill(dist,ri);
+      //             hRphiDistVsR->Fill(rpd,ri);
+      //          }
+      //    }
 
       int Nhits = anEvent->GetNumberOfHits();
       printf("HistoRun::Analyze   Number of Hits: %d\n",Nhits);
