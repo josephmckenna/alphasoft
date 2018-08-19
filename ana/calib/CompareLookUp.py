328081e3fa5f77a9e00517a95d1482e149989fa7
Andrea <andrea.capra85@gmail.com>
2018-08-19 17:31:57 +0200

    script to compare STRs

---
+++ ana/calib/CompareLookUp.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+xmax=np.array([])
+basename='LookUp_0.00T_STRR'
+
+#runlist=[]
+#runlist=[2350,2361,2362,2363,2364]
+#runlist=[2350,2361,2362,2363]
+#runlist=[2361]
+#runlist=[2363,2364]
+#runlist=[2000,2364]
+runlist=[2364,2382]
+
+#fname='run.list'
+#runlist=open(fname)
+
+for run in runlist:
+
+    '''    
+    rawdata='%s%d.dat'%(basename,run)
+    #print(rawdata)
+    t0, rmin, r0, rmax, phimin, phi0, phimax = np.loadtxt(rawdata, 
+                                                          delimiter=' ', 
+                                                          skiprows=2, unpack=True)
+    xmax = np.append(xmax,max(t0)*1.1)
+    plt.plot(t0,r0,'o',label='R%d'%run)
+    '''    
+
+    fitdata='%s%d_fit.dat'%(basename,int(run))
+    print(fitdata)
+    tt,rr,pp = np.loadtxt(fitdata, 
+                          delimiter='\t', 
+                          skiprows=2, unpack=True)
+    xmax = np.append(xmax,max(tt)*1.1)
+    plt.plot(tt,rr,'-',label='R%d fit'%int(run))
+
+    '''
+    fitdata='./calib/%s%d_fit.dat'%(basename,int(run))
+    tt,rr,pp = np.loadtxt(fitdata, 
+                          delimiter='\t', 
+                          skiprows=2, unpack=True)
+    xmax = np.append(xmax,max(tt)*1.1)
+    plt.plot(tt,rr,'-',label='R%d calib fit'%int(run))
+    '''
+#runlist.close()
+
+tt,rr,pp = np.loadtxt('LookUp_0.00T_good.dat', 
+                      delimiter='\t', 
+                      skiprows=2, unpack=True)
+xmax = np.append(xmax,max(tt)*1.1)
+plt.plot(tt,rr,'-',label='good')
+
+tt,rr,pp = np.loadtxt('garfppSTR_B0.00T_Ar70CO230_CERN.dat', 
+                      delimiter='\t', 
+                      skiprows=2, unpack=True)
+xmax = np.append(xmax,max(tt)*1.1)
+plt.plot(tt,rr,'-',label='garf++ CH')
+
+tt,rr,pp = np.loadtxt('garfppSTR_B0.00T_Ar70CO230_TRIUMF.dat', 
+                      delimiter='\t', 
+                      skiprows=2, unpack=True)
+xmax = np.append(xmax,max(tt)*1.1)
+plt.plot(tt,rr,'-',label='garf++ CA')
+
+plt.xlabel('t [ns]')
+plt.ylabel('r [mm]')
+#plt.xlim(0.,max(xmax))
+plt.xlim(0.,4200.)
+plt.ylim(109.,185.)
+plt.legend(loc='best')
+plt.grid(True)
+
+fig=plt.gcf()
+fig.set_size_inches(18.5, 10.5)
+fig.tight_layout()
+
+plt.show()
