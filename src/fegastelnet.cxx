fb290222724c7889aceb07b6c943fd18ea082f60
agdaq (ALPHA-g DAQ) <agdaq@alphacpc04.cern.ch>
2018-08-19 19:30:47 +0200

    changes

--- src/fegastelnet.cxx
+++ src/fegastelnet.cxx
@@ -10,6 +10,7 @@
 #include <signal.h>
 
 #include <vector>
+#include <sstream>
 
 #include "tmfe.h"
 #include "tmvodb.h"
@@ -61,42 +62,12 @@ public:
 
    time_t fFastUpdate = 0;
 
-#if 0
-   bool Wait(int wait_sec, const char* explain)
+   void WB(const char* varname, int i, bool v)
    {
-      time_t to = time(NULL) + wait_sec;
-      while (1) {
-         int a = 0;
-
-         KOtcpError e = s->BytesAvailable(&a);
-         if (e.error) {
-            mfe->Msg(MERROR, "Wait", "Socket error %s", e.message.c_str());
-            s->Close();
-            eq->SetStatus("Lost connection", "red");
-            return false;
-         }
-
-         //printf("Wait %d sec, available %d\n", wait_sec, a);
-         if (a > 0)
-            return true;
-
-         if (time(NULL) > to) {
-            mfe->Msg(MERROR, "Wait", "Timeout waiting for repy to command: %s", explain);
-            s->Close();
-            eq->SetStatus("Lost connection", "red");
-            return false;
-         }
-
-         mfe->SleepMSec(1);
-
-         if (mfe->fShutdown) {
-            mfe->Msg(MERROR, "Wait", "Shutdown command while waiting for reply to command: %s", explain);
-            return false;
-         }
-      }
-      // not reached
-   }
-#endif
+      std::ostringstream oss;
+      oss << varname << "[" << i << "]";
+      fS->WB(oss.str().c_str(), v);
+   };
 
    bool Wait(int wait_sec, const char* explain)
    {
@@ -258,245 +229,23 @@ public:
       return vv;
    }
 
-   void WRStat(const std::vector<double> &stat)
-   {
-      if (mfe->fShutdown)
-         return;
-
-      std::string path;
-      path += "/Equipment/";
-      path += eq->fName;
-      path += "/Readback/";
-      path += "STAT_BITS";
-
-      std::string v;
-
-      for (unsigned i=0; i<stat.size(); i++) {
-         if (i>0)
-            v += ";";
-
-         int b = stat[i];
-         char buf[256];
-         sprintf(buf, "0x%04x", b);
-         v += buf;
-
-         if (b & (1<<0)) v += " ON";
-         if (b & (1<<1)) { v += " RUP"; fFastUpdate = time(NULL) + 10; }
-         if (b & (1<<2)) { v += " RDW"; fFastUpdate = time(NULL) + 10; }
-         if (b & (1<<3)) v += " OVC";
-         if (b & (1<<4)) v += " OVV";
-         if (b & (1<<5)) v += " UNV";
-         if (b & (1<<6)) v += " MAXV";
-         if (b & (1<<7)) v += " TRIP";
-         if (b & (1<<8)) v += " OVP";
-         if (b & (1<<9)) v += " OVT";
-         if (b & (1<<10)) v += " DIS";
-         if (b & (1<<11)) v += " KILL";
-         if (b & (1<<12)) v += " ILK";
-         if (b & (1<<13)) v += " NOCAL";
-         if (b & (1<<14)) v += " bit14";
-         if (b & (1<<15)) v += " bit15";
-      }
-
-      //printf("Write ODB %s value %s\n", C(path), C(v));
-
-      int status = db_set_value(mfe->fDB, 0, C(path), C(v), v.length()+1, 1, TID_STRING);
-      if (status != DB_SUCCESS) {
-         printf("WR: db_set_value status %d\n", status);
-      }
-   }
-
-   void WRAlarm(const std::string &alarm)
-   {
-      if (mfe->fShutdown)
-         return;
-
-      std::string path;
-      path += "/Equipment/";
-      path += eq->fName;
-      path += "/Readback/";
-      path += "BDALARM_BITS";
-
-      std::string v;
-
-      int b = atoi(C(alarm));
-
-      char buf[256];
-      sprintf(buf, "0x%04x", b);
-      v += buf;
-
-      if (b & (1<<0)) v += " CH0";
-      if (b & (1<<1)) v += " CH1";
-      if (b & (1<<2)) v += " CH2";
-      if (b & (1<<3)) v += " CH3";
-      if (b & (1<<4)) v += " PWFAIL";
-      if (b & (1<<5)) v += " OVP";
-      if (b & (1<<6)) v += " HVCKFAIL";
-
-      //printf("Write ODB %s value %s\n", C(path), C(v));
-      int status = db_set_value(mfe->fDB, 0, C(path), C(v), v.length()+1, 1, TID_STRING);
-      if (status != DB_SUCCESS) {
-         printf("WR: db_set_value status %d\n", status);
-      }
-
-      if (b) {
-         std::string vv = "Alarm: " + v;
-         eq->SetStatus(C(vv), "#FF0000");
-
-         std::string aa = eq->fName + " alarm " + v;
-         mfe->TriggerAlarm(C(eq->fName), C(aa), "Alarm");
-      } else {
-         eq->SetStatus("Ok", "#00FF00");
-         mfe->ResetAlarm(C(eq->fName));
-      }
-   }
-
-#if 0
-   void WED(const char* name, int ch, double v)
-   {
-      char cmd[256];
-      sprintf(cmd, "$BD:00:CMD:SET,CH:%d,PAR:%s,VAL:%f", ch, name, v);
-      Exch(cmd);
-   }
-#endif
-
    void ReadImportant()
    {
-#if 0
-      RE1("BDILK"); // interlock status
-
-      std::string bdalarm = RE1("BDALARM"); // alarm status
-      if (bdalarm.length() > 0) {
-         WRAlarm(bdalarm);
-      }
-
-      VE("VSET"); // voltage setpoint VSET
-      VE("VMON"); // voltage actual value VMON
-
-      VE("ISET"); // current setpoint ISET, uA
-      VE("IMON"); // current actual value, uA
-
-      std::vector<double> stat = VE("STAT"); // channel status
-      WRStat(stat);
-#endif
    }
 
    void ReadSettings()
    {
-#if 0
-      RE1("BDILKM"); // interlock mode
-      RE1("BDCTR"); // control mode
-      RE1("BDTERM"); // local bus termination
-
-      mfe->PollMidas(1);
-
-      VE("VMIN"); // VSET minimum value
-      VE("VMAX"); // VSET maximum value
-      VE("VDEC"); // VSET number of decimal digits
-
-      mfe->PollMidas(1);
-
-      VE("IMIN");    // ISET minimum value
-      VE("IMAX");    // ISET maximum value
-      VE("ISDEC");   // ISET number of decimal digits
-      RE("IMRANGE"); // current monitor range HIGH/LOW
-      VE("IMDEC");   // IMON number of decimal digits
-
-      mfe->PollMidas(1);
-
-      VE("MAXV");  // MAXVSET max VSET value
-      VE("MVMIN"); // MAXVSET minimum value
-      VE("MVMAX"); // MAXVSET maximum value
-      VE("MVDEC"); // MAXVSET number of decimal digits
-
-      mfe->PollMidas(1);
-
-      VE("RUP"); // ramp up V/s
-      VE("RUPMIN");
-      VE("RUPMAX");
-      VE("RUPDEC");
-
-      mfe->PollMidas(1);
-
-      VE("RDW"); // ramp down V/s
-      VE("RDWMIN");
-      VE("RDWMAX");
-      VE("RDWDEC");
-
-      mfe->PollMidas(1);
-
-      VE("TRIP"); // trip time, sec
-      VE("TRIPMIN");
-      VE("TRIPMAX");
-      VE("TRIPDEC");
-
-      mfe->PollMidas(1);
-
-      RE("PDWN"); // power down RAMP/KILL
-      RE("POL"); // polarity
-#endif
    }
 
-#if 0
-   void TurnOn(int chan)
+   void SetValve(int chan, int state)
    {
-      mfe->Msg(MINFO, "TurnOn", "Turning on channel %d", chan);
-      WE("ON", chan);
+      mfe->Msg(MINFO, "SetValve", "SetValve(%d, %d)", chan, state);
+      WB("do", chan, state);
    }
-
-   void TurnOff(int chan)
-   {
-      mfe->Msg(MINFO, "TurnOff", "Turning off channel %d", chan);
-      WE("OFF", chan);
-   }
-#endif
 
    void UpdateSettings()
    {
       mfe->Msg(MINFO, "UpdateSettings", "Writing settings from ODB to hardware");
-
-#if 0
-      //Exch(mfe, s, "$BD:00:CMD:SET,PAR:BDILKM,VAL:OPEN"); // set interlock mode
-      //Exch(mfe, s, "$BD:00:CMD:SET,PAR:BDILKM,VAL:CLOSED");
-
-      //Exch(mfe, s, "$BD:00:CMD:SET,PAR:BDCLR"); // clear alarm signal
-
-      //Exch(mfe, s, "$BD:00:CMD:SET,CH:4,PAR:VSET,VAL:1;2;3;4");
-
-      int nch = fNumChan;
-
-      for (int i=0; i<nch; i++) {
-         WED("VSET", i, OdbGetValue(mfe, eq->fName, "VSET", i, nch));
-         WED("ISET", i, OdbGetValue(mfe, eq->fName, "ISET", i, nch));
-         WED("MAXV", i, OdbGetValue(mfe, eq->fName, "MAXV", i, nch));
-         WED("RUP",  i, OdbGetValue(mfe, eq->fName, "RUP",  i, nch));
-         WED("RDW",  i, OdbGetValue(mfe, eq->fName, "RDW",  i, nch));
-         WED("TRIP", i, OdbGetValue(mfe, eq->fName, "TRIP", i, nch));
-
-         double pdwn = OdbGetValue(mfe, eq->fName, "PDWN", i, nch);
-         if (pdwn == 1) {
-            WES("PDWN", i, "RAMP");
-         } else if (pdwn == 2) {
-            WES("PDWN", i, "KILL");
-         }
-
-         double imrange = OdbGetValue(mfe, eq->fName, "IMRANGE", i, nch);
-         if (imrange == 1) {
-            WES("IMRANGE", i, "HIGH");
-         } else if (imrange == 2) {
-            WES("IMRANGE", i, "LOW");
-         }
-
-#if 0
-         double onoff = OdbGetValue(mfe, eq->fName, "ONOFF", i, nch);
-         if (onoff == 1) {
-            WE(mfe, eq, s, "ON", i);
-         } else if (onoff == 2) {
-            WE(mfe, eq, s, "OFF", i);
-         }
-#endif
-      }
-#endif
 
       fFastUpdate = time(NULL) + 30;
    }
@@ -505,59 +254,16 @@ public:
    {
       mfe->Msg(MINFO, "HandleRpc", "RPC cmd [%s], args [%s]", cmd, args);
 
-#if 0
-      int mask = 0;
-      int all = 0;
-      int chan = 0;
-      if (strcmp(args, "all") == 0) {
-         all = 1;
-         mask = 0xF;
-      } else {
-         chan = atoi(args);
-         mask |= (1<<chan);
-      }
-#endif
-
-      return "";
-#if 0
-      //printf("mask 0x%x\n", mask);
-
-      if (strcmp(cmd, "turn_on")==0) {
+      if (strcmp(cmd, "set_valve")==0) {
 
          if (gUpdate) {
             UpdateSettings();
          }
 
-         if (all) {
-            for (int i=0; i<fNumChan; i++) {
-               TurnOn(i);
-            }
-         } else {
-            TurnOn(chan);
-         }
-
-         sleep(1);
-         sleep(1);
-
-         ReadImportant();
-
-         //gUpdate = true;
-         fFastUpdate = time(NULL) + 30;
-
-         return "OK";
-      } else if (strcmp(cmd, "turn_off")==0) {
-
-         if (gUpdate) {
-            UpdateSettings();
-         }
-
-         if (all) {
-            for (int i=0; i<fNumChan; i++) {
-               TurnOff(i);
-            }
-         } else {
-            TurnOff(chan);
-         }
+         std::istringstream iss(args);
+         int chan, state;
+         iss >> chan >> state;
+         SetValve(chan,state);
 
          sleep(1);
          sleep(1);
@@ -571,7 +277,6 @@ public:
       } else {
          return "";
       }
-#endif
    }
 };
 
@@ -635,32 +340,6 @@ int main(int argc, char* argv[])
    setup_watch(mfe, eq);
 
    gUpdate = true;
-#if 0
-   //while (!mfe->fShutdown) {
-   //   mfe->PollMidas(1000);
-   //}
-   //exit(123);
-
-   if (0) { // test events
-      while (1) {
-         char buf[25600];
-         eq->ComposeEvent(buf, sizeof(buf));
-         eq->BkInit(buf, sizeof(buf));
-         short* ptr = (short*)eq->BkOpen(buf, bank, TID_WORD);
-         for (int i=0; i<10; i++)
-            *ptr++ = i;
-         eq->BkClose(buf, ptr);
-         if (0) {
-            for (int i=0; i<30; i++) {
-               printf("buf[%d]: 0x%08x\n", i, ((int*)buf)[i]);
-            }
-         }
-         eq->SendEvent(buf);
-         eq->WriteStatistics();
-         sleep(1);
-      }
-   }
-#endif
 
    const char* name = "algas";
    const char* port = "23";
@@ -845,66 +524,6 @@ int main(int argc, char* argv[])
          gas->fV->WDA("gas_flow_sccm", gas_flow);
 
          eq->SetStatus("Ok", "#00FF00");
-#if 0
-         //Exch(mfe, s, "$BD:00:CMD:MON,PAR:BDNAME");
-         std::string bdname = hv->RE1("BDNAME"); // mainframe name and type
-         std::string bdnch  = hv->RE1("BDNCH"); // channels number
-
-         if (mfe->fShutdown) {
-            break;
-         }
-
-         if (bdname.length() < 1 || bdnch.length() < 1) {
-            mfe->Msg(MERROR, "main", "Cannot read BDNAME or BDNCH, will try to reconnect after 10 sec...");
-            mfe->PollMidas(10000);
-            break;
-         }
-
-         hv->fBdnch = bdnch;
-         hv->fNumChan = atoi(bdnch.c_str());
-
-         std::string bdfrel = hv->RE1("BDFREL"); // firmware release
-         std::string bdsnum = hv->RE1("BDSNUM"); // serial number
-
-         if (first_time) {
-            mfe->Msg(MINFO, "main", "Device %s is model %s with %s channels, firmware %s, serial %s", name, C(bdname), C(bdnch), C(bdfrel), C(bdsnum));
-         }
-
-         hv->ReadImportant();
-
-         if (gUpdate) {
-            gUpdate = false;
-            hv->UpdateSettings();
-            continue;
-         }
-
-         hv->ReadSettings();
-
-         //time_t end_time = time(NULL);
-
-         //printf("readout time: %d sec\n", (int)(end_time - start_time));
-
-         if (first_time) {
-            hv->UpdateSettings();
-         }
-
-         first_time = false;
-
-         if (0) {
-            //mfe->SleepMSec(1000);
-
-            //Exch(mfe, s, "$BD:00:CMD:SET,PAR:BDILKM,VAL:OPEN");
-            hv->Exch("$BD:00:CMD:SET,PAR:BDILKM,VAL:CLOSED");
-
-            hv->Exch("$BD:00:CMD:SET,PAR:BDCLR");
-
-            //Exch(mfe, s, "$BD:00:CMD:SET,CH:4,PAR:VSET,VAL:1;2;3;4");
-            hv->Exch("$BD:00:CMD:SET,CH:0,PAR:VSET,VAL:10");
-            hv->Exch("$BD:00:CMD:SET,CH:1,PAR:VSET,VAL:11");
-            hv->Exch("$BD:00:CMD:SET,CH:2,PAR:VSET,VAL:12");
-            hv->Exch("$BD:00:CMD:SET,CH:3,PAR:VSET,VAL:13");
-         }
-#endif
 
          if (gas->fFastUpdate != 0) {
             if (time(NULL) > gas->fFastUpdate)
@@ -926,39 +545,6 @@ int main(int argc, char* argv[])
                break;
          }
 
-#if 0
-         char buf[2560000];
-         eq->ComposeEvent(buf, sizeof(buf));
-         eq->BkInit(buf, sizeof(buf));
-         unsigned short* xptr = (unsigned short*)eq->BkOpen(buf, bank, TID_WORD);
-         unsigned short* ptr = xptr;
-
-         char*s = (char*)rrr.c_str();
-         for (int i=0; i<1000000; i++) {
-            while (*s == '+')
-               s++;
-            if (*s == 0)
-               break;
-            if (*s == '\n')
-               break;
-            if (*s == '\r')
-               break;
-            int v = strtoul(s, &s, 10);
-            *ptr++ = v;
-         }
-         eq->BkClose(buf, ptr);
-
-         printf("found %d entries\n", (int)(ptr-xptr));
-
-         if (0) {
-            for (int i=0; i<30; i++) {
-               printf("buf[%d]: 0x%08x\n", i, ((int*)buf)[i]);
-            }
-         }
-
-         eq->SendEvent(buf);
-         eq->WriteStatistics();
-#endif
       }
    }
 
