3c5f1440a8ef9c461450deaa474cf0142abb8da8
Joseph McKenna <joseph.mckenna@cern.ch>
2018-08-23 16:54:38 +0200

    Add LD_LIBRARY_PATH settings for agconfig.sh

--- agconfig.sh
+++ agconfig.sh
@@ -20,19 +20,42 @@ echo "Hostname: " `hostname`
 echo "Username: " `whoami`
 echo "#########################################"
 
+#Setup LD_LIBRARY_PATH
+for AG_LIB_PATH in ana analib aged; do
+  if echo "${LD_LIBRARY_PATH}" | grep "${AGRELEASE}/${AG_LIB_PATH}/" > /dev/null; then
+    NOTHING_TO_DO=1
+  else
+    echo "Adding ${AG_LIB_PATH} to LD_LIBRARY_PATH"
+    export  LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${AGRELEASE}/${AG_LIB_PATH}/
+  fi
+done
+
+
+#Quit if ROOT and ROOTANA are setup...
 if [ "${1}" == "clean" ]; then
-  echo "overwriting any settings"
+  echo "Clean setup of environment variables"
 else
   if [ ${#ROOTANASYS} -gt 3 ]; then
     echo "ROOTANASYS set... not over writing"
+    if [ ${#ROOTSYS} -lt 3 ]; then
+      echo "Please setup root manually (or run . agconfig.sh clean)"
+    else
+      echo "ROOTSYS set... not over writing"
+    fi
     return;
   fi
   if [ ${#ROOTSYS} -gt 3 ]; then
     echo "ROOTSYS set... not over writing"
+    if [ ${#ROOTANASYS} -lt 3 ]; then
+      echo "Please setup rootana manually (or run . agconfig.sh clean)"
+    else
+      echo "ROOTANASYS set... not over writing"
+    fi
     return;
   fi
 fi
- 
+
+#Setup ROOT and ROOTANA if we havn't quit yet...
 case `hostname` in
 alphavme*  )
   echo "alphavme detected..."
