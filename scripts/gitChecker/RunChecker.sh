c19be3d25eae6ecd1bae1a894d6c22c1b437e9c7
Joseph McKenna <joseph.mckenna@cern.ch>
2018-08-24 16:49:10 +0200

    Generalise RunChecker to use runNumber as input

--- scripts/gitChecker/RunChecker.sh
+++ scripts/gitChecker/RunChecker.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+RUNNO=${1}
+if [ `echo "${RUNNO}" | wc -c` -gt 3 ]; then
+  echo "Running for RUNNO=${RUNNO}"
+else
+  RUNNO=02364
+  echo "Using default RUNNO of ${RUNNO}"
+fi
 
 if [ `echo "${AGRELEASE}" | wc -c` -gt 3 ]; then
   echo "AGRELEASE set ok: $AGRELEASE"
@@ -14,15 +21,26 @@ fi
 cd $AGRELEASE/ana
 export EOS_MGM_URL=root://eospublic.cern.ch
 
-eos cp /eos/experiment/alpha/run02364sub000.mid.lz4 .
-
+if [ ! -f run${RUNNO}sub000.mid.lz4  ]; then
+  eos cp /eos/experiment/alpha/run${RUNNO}sub000.mid.lz4 .
+else
+  echo "run${RUNNO}sub000.mid.lz4 found locally"
+fi
 
 #Calling -h returns with a non-zero exit code
 #./agana.exe -h
 #Calling with a fake input file and --help finishes with a exit code 0 (not fail)
-mkdir test-results
-./agana.exe run02364sub000.mid.lz4 -- --usetimerange 0 2 &> test-results/agana_run_02364
-tail -n 50 test-results/agana_run_02364
+mkdir -p test-results
+./agana.exe run02364sub000.mid.lz4 -- --usetimerange 0. 1.0 | tee test-results/agana_run_${RUNNO}.txt
+#./agana.exe run02364sub000.mid.lz4 -- ---useeventrange  0 2 | tee test-results/agana_run_${RUNNO}.txt
+
+tail -n 50 test-results/agana_run_${RUNNO}.txt
+
+echo ".L macros/ReadEventTree.C 
+ReadEventTree()
+.q
+" | root -l *${RUNNO}*.root
+
 #./agana.exe fakefile -- --help
 echo "Add more here"
 
