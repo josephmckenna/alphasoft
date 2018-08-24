5bf25ed6b62097278acbc770f36ef0545165902b
Joseph McKenna <joseph.mckenna@cern.ch>
2018-08-24 10:13:00 +0200

    Try copying from EOS and running real analysis in RunChecker.sh

--- scripts/gitChecker/RunChecker.sh
+++ scripts/gitChecker/RunChecker.sh
@@ -9,13 +9,20 @@ else
   exit
 fi
 
-
+mkdir test-results
 
 cd $AGRELEASE/ana
+export EOS_MGM_URL=root://eospublic.cern.ch
+
+eos cp /eos/experiment/alpha/run02364sub000.mid.lz4 .
+
+
 #Calling -h returns with a non-zero exit code
 #./agana.exe -h
 #Calling with a fake input file and --help finishes with a exit code 0 (not fail)
-./agana.exe fakefile -- --help
+./agana.exe run02364sub000.mid.lz4 -- --usetimerange 0 2 &> $AGRELEASE/test-results
+tail -n 50 $AGRELEASE/test-results
+#./agana.exe fakefile -- --help
 echo "Add more here"
 
 
