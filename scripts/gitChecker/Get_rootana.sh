07e2d49940ae627e6eb528899156157c6eaa2559
Joseph McKenna <joseph.mckenna@cern.ch>
2018-08-25 16:10:48 +0200

    Move CI rootana fetch to script

---
+++ scripts/gitChecker/Get_rootana.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -d rootana ]; then
+  echo "rootana fould"
+  cd rootana
+  UPSTREAM=${1:-'@{u}'}
+  LOCAL=$(git rev-parse @{0})
+  REMOTE=$(git rev-parse "$UPSTREAM")
+  BASE=$(git merge-base @{0} "$UPSTREAM")
+
+  echo "Checking rootana status"
+  if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+  elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+    git pull
+    make clean && make
+  elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+  else
+    echo "Diverged"
+  fi
+  export ROOTANASYS=`pwd`
+else
+  git clone https://jtkm@bitbucket.org/tmidas/rootana.git
+  cd rootana
+  make
+  export ROOTANASYS=`pwd`
+fi
