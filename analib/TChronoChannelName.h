1261a72fc0ea91c808b827be665b14c4982dd226
Joseph McKenna <joseph.mckenna@cern.ch>
2018-09-01 12:20:32 +0200

    Add TObject to store ChronoChannelNames

---
+++ analib/TChronoChannelName.h
@@ -0,0 +1,33 @@
+#ifndef _TChronoChannelName_
+#define _TChronoChannelName_
+
+#include <iostream>
+#ifndef ROOT_TObject
+#include "TObject.h"
+#endif
+
+#ifndef ROOT_TString
+#include "TString.h"
+#endif
+
+#include "../ana/chrono_module.h"
+
+class TChronoChannelName : public TObject
+{
+  private:
+  Int_t fChronoBoxIndex;
+  Int_t fChronoBoardIndex;
+  TString Name[CHRONO_N_CHANNELS];
+  public:
+   TChronoChannelName();
+   using TObject::Print;
+   virtual void Print();
+   virtual ~TChronoChannelName();
+   Int_t GetBoxIndex()      { return fChronoBoxIndex; }
+   Int_t GetBoardIndex()    { return fChronoBoardIndex; }
+   TString GetChannelName(Int_t Channel) { return Name[Channel]; }
+   Int_t GetChannel(Int_t ChannelName, Bool_t exact_match=kFALSE);
+   void SetBoardIndex(Int_t _index) { fChronoBoardIndex = _index; }
+   void SetChannelName(TString _name, Int_t i) { Name[i]=_name; }
+};
+#endif
