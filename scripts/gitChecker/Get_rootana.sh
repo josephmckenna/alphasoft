0f71c682c7860997f3e95c5fd36a4c3f27af0ece
Joseph McKenna <joseph.mckenna@cern.ch>
2018-09-20 14:00:51 +0200

    Fix bitbucket CI again

--- scripts/gitChecker/Get_rootana.sh
+++ scripts/gitChecker/Get_rootana.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -d rootana ] && [ `git remote show origin | grep rootana | wc -l` -gt 1 ]; then
+if [ -d rootana ] && [ `ls -l rootana | wc -l` -gt 1 ]; then
   echo "rootana fould"
   cd rootana
   UPSTREAM=${1:-'@{u}'}
