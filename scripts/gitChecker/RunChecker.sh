1aa779f6c13f7f668111b64490c7e6e7172093da
Joseph McKenna <joseph.mckenna@cern.ch>
2018-09-29 18:35:51 +0200

    Export elog number

--- scripts/gitChecker/RunChecker.sh
+++ scripts/gitChecker/RunChecker.sh
@@ -108,7 +108,9 @@ if [[ $(hostname -s) = *runner* ]]; then
    #Move files to alphadaq (so that they can be added to elog post)
    scp -r ~/${GITHASH} alpha@alphadaq:~/gitCheckerReports/
    echo "~/packages/elog/elog -h localhost -a Author=${HOSTNAME} -a Run=\"${RUNNO}\" -a Subject=\"git-checker: $GITHASH (${BRANCH})\" -a Tags=\"gitcheck\" -m ~/gitCheckerReports/${GITHASH}/elogMessage.txt ${FILES}  -p 8080 -l AutoAnalysis -v "
-   ssh -X alpha@alphadaq "~/packages/elog/elog -h localhost -a Author=${HOSTNAME} -a Run=\"${RUNNO}\" -a Subject=\"git-checker: $GITHASH (${BRANCH})\" -a Tags=\"gitcheck\" -m ~/gitCheckerReports/${GITHASH}/elogMessage.txt ${FILES}  -p 8080 -l AutoAnalysis -v "
+   ssh -X alpha@alphadaq "~/packages/elog/elog -h localhost -a Author=${HOSTNAME} -a Run=\"${RUNNO}\" -a Subject=\"git-checker: $GITHASH (${BRANCH})\" -a Tags=\"gitcheck\" -m ~/gitCheckerReports/${GITHASH}/elogMessage.txt ${FILES}  -p 8080 -l AutoAnalysis -v " &> elog_posting.log
+   cat elog_posting.log
+   ELOG_NO=`cat elog_posting.log  | grep ID= | tr 'Message successfully transmitted, ID=' "\n"| grep [0-9]`
 fi
 #./agana.exe fakefile -- --help
 #echo "Add more here"
