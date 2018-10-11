5d7c689e7b387c4326c9189861223dd4905f9b3d
Joseph McKenna <joseph.mckenna@cern.ch>
2018-10-11 12:35:06 +0200

    Basic skeleton of TPC/Chronobox calibration

--- ana/official_time_module.cxx
+++ ana/official_time_module.cxx
@@ -30,7 +30,8 @@ class OfficialTime: public TARunObject
 private:
 
   std::vector<double> TPCts;
-  std::vector<double> TPCchronoTS[CHRONO_N_BOARDS];
+  double TPCZeroTime;
+  std::vector<double> Chrono_TPC;
   std::vector<double> ChronoSyncTS[CHRONO_N_BOARDS];
   
   std::vector<ChronoEvent*>* ChronoEventsFlow=NULL;
@@ -56,7 +57,8 @@ public:
    {
       if (fTrace)
          printf("OfficialTime::BeginRun, run %d\n", runinfo->fRunNo);
-
+      Chrono_TPC.clear();
+      TPCts.clear();
    }
 
    void EndRun(TARunInfo* runinfo)
@@ -78,15 +80,37 @@ public:
          printf("ResumeModule, run %d\n", runinfo->fRunNo);
    }
 
+   void TPCMatchTime()
+   {
+      uint ChronoEvents=Chrono_TPC.size();
+      uint TPCEvents=TPCts.size();
+      if (ChronoEvents==0 || TPCEvents==0)
+         return;
+         
+      //Find smaller arrays
+      int Events;
+      if (ChronoEvents>TPCEvents)
+         Events=TPCEvents;
+      else
+         Events=ChronoEvents;
+      std::cout <<"CALIB SIZE"<<ChronoEvents<<"\t"<<TPCEvents<<"   "<<Events<<std::endl;
+
+      for (int  i=0; i<Events-1; i++)
+      {
+         //std::cout<<"CALIB   "<<i<<":"<<Chrono_TPC.at(i)<<" - "<<TPCts.at(i)<<" = "<< Chrono_TPC[i]-TPCts[i]<<std::endl;
+         std::cout<<"CALIB   "<<i<<":"<<Chrono_TPC[i]<<" - "<<TPCts[i]<<" = "<< Chrono_TPC[i]-TPCts[i]<<std::endl;
+      }
+   }
+
    TAFlowEvent* Analyze(TARunInfo* runinfo, TMEvent* me, TAFlags* flags, TAFlowEvent* flow)
    {
       //printf("Analyze, run %d, event serno %d, id 0x%04x, data size %d\n", runinfo->fRunNo, event->serial_number, (int)event->event_id, event->data_size);
       std::cout<<"OfficialTime::Analyze   Event # "<<me->serial_number<<std::endl;
 
-      if( me->event_id != 10 ) // sequencer event id
-         return flow;
-      AgSignalsFlow* SigFlow = flow->Find<AgSignalsFlow>();
-      if( !SigFlow ) return flow;
+      //if( me->event_id != 10 ) // sequencer event id
+      //   return flow;
+      //AgSignalsFlow* SigFlow = flow->Find<AgSignalsFlow>();
+      //if( !SigFlow ) return flow;
       
       AgChronoFlow* ChronoFlow = flow->Find<AgChronoFlow>();
       if (ChronoFlow)
@@ -98,8 +122,17 @@ public:
                   if (e->Channel==CHRONO_SYNC_CHANNEL)
                      ChronoSyncTS[e->ChronoBoard].push_back(e->RunTime);
                   //if TPC trigger... add it too
+                  if (e->Channel==CHRONO_N_TS_CHANNELS && e->ChronoBoard==0)
+                     Chrono_TPC.push_back(e->RunTime);
                }
          }
+      AgEventFlow *ef = flow->Find<AgEventFlow>();
+      if (ef && ef->fEvent)
+      {
+         AgEvent* age = ef->fEvent;
+         TPCts.push_back(age->time);
+         TPCMatchTime();
+      }
       #ifdef _TIME_ANALYSIS_
          if (TimeModules) flow=new AgAnalysisReportFlow(flow,"official_time_module");
       #endif
