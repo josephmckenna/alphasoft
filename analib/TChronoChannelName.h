e49ae06080d94ae21daafccab265c59ea0ba502c
Joseph McKenna <joseph.mckenna@cern.ch>
2018-10-19 02:28:32 +0200

    Add reading and writing TChronoChannelName to json

--- analib/TChronoChannelName.h
+++ analib/TChronoChannelName.h
@@ -12,25 +12,34 @@
 #include "TString.h"
 #endif
 
+
+#include <TBufferJSON.h>
+#include <fstream>
 #include "../ana/chrono_module.h"
 
 class TChronoChannelName : public TObject
 {
   private:
-  Int_t fChronoBoxIndex;
+  Int_t fChronoBoxIndex; //Box index isn't used yet...
   Int_t fChronoBoardIndex;
   TString Name[CHRONO_N_CHANNELS];
   public:
    TChronoChannelName();
-   TChronoChannelName(VirtualOdb* Odb, Int_t b);
+   TChronoChannelName(VirtualOdb* Odb, Int_t b, Int_t BoxIndex=-1);
+   TChronoChannelName(TString json, Int_t b);
+
+   void DumpToJson(int runno);
+
    using TObject::Print;
    virtual void Print();
    virtual ~TChronoChannelName();
-   Int_t GetBoxIndex()      { return fChronoBoxIndex; }
-   Int_t GetBoardIndex()    { return fChronoBoardIndex; }
+   Int_t GetBoxIndex()                   { return fChronoBoxIndex; }
+   Int_t GetBoardIndex()                 { return fChronoBoardIndex; }
    TString GetChannelName(Int_t Channel) { return Name[Channel]; }
    Int_t GetChannel(TString ChannelName, Bool_t exact_match=kTRUE);
-   void SetBoardIndex(Int_t _index) { fChronoBoardIndex = _index; }
+
+   void SetBoxIndex(Int_t _index)        { fChronoBoxIndex = _index; }
+   void SetBoardIndex(Int_t _index)      { fChronoBoardIndex = _index; }
    void SetChannelName(TString _name, Int_t i) { Name[i]=_name; }
    
   ClassDef( TChronoChannelName, 1 )
