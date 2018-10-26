676f7f7dfce7a34311147026c4a34ff7a665fcc8
Joseph McKenna <joseph.mckenna@cern.ch>
2018-10-26 15:38:36 +0200

    Upgrade to 9 sequencers

--- analib/Sequencer_Channels.h
+++ analib/Sequencer_Channels.h
@@ -3,14 +3,15 @@
 #ifndef _SEQNAMES_
 #define _SEQNAMES_
 
-#define NUMSEQ 9  //Decared also in Sequencer2
-#define USED_SEQ 7  //Decared also in Sequencer2
+#define NUMSEQ 9 
+#define USED_SEQ 9 
 extern TString SeqNames[NUMSEQ];
 extern TString StartDumpName[NUMSEQ];
 extern TString StopDumpName[NUMSEQ];
 extern TString StartSeqName[NUMSEQ];
 extern uint USED_SEQ_NUM[USED_SEQ];
 
+//Dont change the order of these please: (Edit Sequencer2.cxx instead)
 enum {PBAR,RECATCH,ATOM,POS,RCT_BOTG,ATM_BOTG,ATM_TOPG,RCT_TOPG,BML};
 
 enum {NOTADUMP,DUMP,EPDUMP};  
