90e3b16963365e39e670e2e24c4c72699de2d387
Andrea <andrea.capra85@gmail.com>
2018-11-04 00:40:00 +0100

    Multi-run vertices

--- analib/RootUtils/PlotGetters.h
+++ analib/RootUtils/PlotGetters.h
@@ -17,6 +17,7 @@ void Plot_Delta_Chrono(Int_t runNumber, const char* ChannelName, const char* des
 
 void Plot_TPC(Int_t runNumber,  Double_t tmin=0., Double_t tmax=-1.);
 void Plot_TPC(Int_t runNumber,  const char* description, Int_t repetition=1, Int_t offset=0);
+void Plot_TPC(Int_t* runNumber, Int_t Nruns, const char* description, Int_t repetition=1, Int_t offset=0);
 void Plot_ClockDrift_TPC(Int_t runNumber, Double_t tmin=0., Double_t tmax=-1.);
 void Plot_ClockDrift_Chrono(Int_t runNumber, Double_t tmin=0., Double_t tmax=-1.);
 void Plot_Chrono_Sync(Int_t runNumber, Double_t tmin=0., Double_t max=-1.);
