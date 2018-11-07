cc8f6a053783ffcbcf8bdb3e8d4667e2e1eb4855
Joseph McKenna <joseph.mckenna@cern.ch>
2018-11-07 06:59:53 +0100

    Update CI to newer newer run (magnet, TPC and bars)

--- scripts/gitChecker/RunChecker_Part2.sh
+++ scripts/gitChecker/RunChecker_Part2.sh
@@ -7,7 +7,7 @@ if [ `echo "${RUNNO}" | wc -c` -gt 3 ]; then
 else
   #RUNNO=02364
   #RUNNO=03213
-  RUNNO=03498
+  RUNNO=03586
   echo "Using default RUNNO of ${RUNNO}"
 fi
 
@@ -27,7 +27,7 @@ BRANCH=`git branch --remote --verbose --no-abbrev --contains | sed -rne 's/^[^\/
 
 rm -vf $AGRELEASE/LookUp*.dat
 cd $AGRELEASE/scripts/UnitTest/
-./SpeedTest.sh ${RUNNO} NOBUILD 100 --time
+./SpeedTest.sh ${RUNNO} NOBUILD 1500 --time
 
 
 
