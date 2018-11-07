72bd80355bac29708b5be031186a9fd982335f2b
Andrea <andrea.capra85@gmail.com>
2018-11-07 05:04:18 +0100

    cope with non-sense root

--- scripts/runfast.sh
+++ scripts/runfast.sh
@@ -16,6 +16,7 @@ echo "agana starting..."
 echo "agana finished..."
 tail -70 R${RUNNO}_recoff.log
 
+cd $AGRELEASE/ana
 echo "void tempmacroR${RUNNO}() {" > tempmacroR${RUNNO}.C
 echo "PrintSequenceQOD(${RUNNO});" >> tempmacroR${RUNNO}.C
 echo "}" >> tempmacroR${RUNNO}.C
@@ -39,13 +40,15 @@ echo "cout<<\"TSTOP \"<<GetTrigTimeAfter(${RUNNO},${stop_dump})<<endl;" >> tempm
 echo "}" >> tempmacroR${RUNNO}.C
 stop_time=$(root -l -q -b tempmacroR${RUNNO}.C | grep "TSTOP " | awk '{print $2}')
 
-mv output${RUNNO_FILE}.root output${RUNNO_FILE}_noreco.root
+mv ../output${RUNNO_FILE}.root output${RUNNO_FILE}_noreco.root
 echo "agana starting..."
 echo "./agana.exe ${MIDAS_PATH}/run${RUNNO_FILE}sub*.mid.lz4 -- --usetimerange $start_time $stop_time"
+cd $AGRELEASE
 { time ./agana.exe ${MIDAS_PATH}/run${RUNNO_FILE}sub*.mid.lz4 -- --usetimerange $start_time $stop_time ; } &> R${RUNNO}_timerange${start_time}-${stop_time}.log
 echo "agana finished..."
 tail -70 R${RUNNO}_timerange${start_time}-${stop_time}.log
 
+cd $AGRELEASE/ana
 echo "void tempmacroR${RUNNO}() {" > tempmacroR${RUNNO}.C
 echo "Plot_TPC(${RUNNO},${start_dump},${stop_dump});" >> tempmacroR${RUNNO}.C
 echo "Plot_Chrono(${RUNNO},\"SiPM_A_AND_D\",${start_dump},${stop_dump});" >> tempmacroR${RUNNO}.C
@@ -65,4 +68,4 @@ root -l tempmacroR${RUNNO}.C
 
 rm -f tempmacroR${RUNNO}.C
 
-mv output${RUNNO_FILE}.root "output${RUNNO_FILE}_timerange${start_time}-${stop_time}.root"
+#mv output${RUNNO_FILE}.root "output${RUNNO_FILE}_timerange${start_time}-${stop_time}.root"
