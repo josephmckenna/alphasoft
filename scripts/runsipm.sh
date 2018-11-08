d70e03cba7f696e4335d0991307d6fbf86c2bd36
Andrea <andrea.capra85@gmail.com>
2018-11-08 04:55:27 +0100

    scripts for real-time analysis, ah-ah

--- scripts/runsipm.sh
+++ scripts/runsipm.sh
@@ -12,11 +12,10 @@ else
 fi
 
 echo "agana starting..."
-{ time ./agana.exe ${MIDAS_PATH}/run${RUNNO_FILE}sub*.mid.lz4 -- --recoff ; } &> R${RUNNO}_recoff.log
+{ time ./agana.exe ${MIDAS_PATH}/run${RUNNO_FILE}sub*.mid.lz4 -- --nounpack ; } &> R${RUNNO}_nounpack.log
 echo "agana finished..."
-tail -70 R${RUNNO}_recoff.log
+tail -70 R${RUNNO}_nounpack.log
 
-#cd $AGRELEASE/ana
 echo "void tempmacroR${RUNNO}() {" > tempmacroR${RUNNO}.C
 echo "PrintSequenceQOD(${RUNNO});" >> tempmacroR${RUNNO}.C
 echo "}" >> tempmacroR${RUNNO}.C
@@ -33,30 +32,22 @@ echo "Enter Stop Time:"
 read stop_dump
 
 echo "void tempmacroR${RUNNO}() {" > tempmacroR${RUNNO}.C
-echo "cout<<\"TSTART \"<<GetTrigTimeBefore(${RUNNO},${start_dump})<<endl;" >> tempmacroR${RUNNO}.C
-echo "}" >> tempmacroR${RUNNO}.C
-start_time=$(root -l -q -b tempmacroR${RUNNO}.C | grep "TSTART " | awk '{print $2}')
-
-echo "void tempmacroR${RUNNO}() {" > tempmacroR${RUNNO}.C
-echo "cout<<\"TSTOP \"<<GetTrigTimeAfter(${RUNNO},${stop_dump})<<endl;" >> tempmacroR${RUNNO}.C
-echo "}" >> tempmacroR${RUNNO}.C
-stop_time=$(root -l -q -b tempmacroR${RUNNO}.C | grep "TSTOP " | awk '{print $2}')
-
-mv output${RUNNO_FILE}.root output${RUNNO_FILE}_noreco.root
-echo "agana starting..."
-echo "./agana.exe ${MIDAS_PATH}/run${RUNNO_FILE}sub*.mid.lz4 -- --usetimerange $start_time $stop_time"
-cd $AGRELEASE
-{ time ./agana.exe ${MIDAS_PATH}/run${RUNNO_FILE}sub*.mid.lz4 -- --usetimerange $start_time $stop_time --stopunpackafter $stop_time ; } &> R${RUNNO}_timerange${start_time}-${stop_time}.log
-echo "agana finished..."
-tail -70 R${RUNNO}_timerange${start_time}-${stop_time}.log
-
-#cd $AGRELEASE/ana
-echo "void tempmacroR${RUNNO}() {" > tempmacroR${RUNNO}.C
-echo "Plot_TPC(${RUNNO},${start_dump},${stop_dump});" >> tempmacroR${RUNNO}.C
+echo "SetBinNumber(1000);" >> tempmacroR${RUNNO}.C
 echo "Plot_Chrono(${RUNNO},\"SiPM_A_AND_D\",${start_dump},${stop_dump});" >> tempmacroR${RUNNO}.C
 echo "Plot_Chrono(${RUNNO},\"SiPM_C_AND_F\",${start_dump},${stop_dump});" >> tempmacroR${RUNNO}.C
 echo "Plot_Chrono(${RUNNO},\"SiPM_B\",${start_dump},${stop_dump});" >> tempmacroR${RUNNO}.C
 echo "Plot_Chrono(${RUNNO},\"SiPM_E\",${start_dump},${stop_dump});" >> tempmacroR${RUNNO}.C
+echo "Plot_Chrono(${RUNNO},\"SiPM_A\",${start_dump},${stop_dump});" >> tempmacroR${RUNNO}.C
+echo "Plot_Chrono(${RUNNO},\"SiPM_F\",${start_dump},${stop_dump});" >> tempmacroR${RUNNO}.C
+echo "Plot_Chrono(${RUNNO},\"SiPM_C\",${start_dump},${stop_dump});" >> tempmacroR${RUNNO}.C
+
+#echo "Plot_Chrono(${RUNNO},\"SiPM_A_AND_D\",\"Hold Dump\");" >> tempmacroR${RUNNO}.C
+#echo "Plot_Chrono(${RUNNO},\"SiPM_C_AND_F\",\"Hold Dump\");" >> tempmacroR${RUNNO}.C
+#echo "Plot_Chrono(${RUNNO},\"SiPM_B\",\"Hold Dump\");" >> tempmacroR${RUNNO}.C
+#echo "Plot_Chrono(${RUNNO},\"SiPM_E\",\"Hold Dump\");" >> tempmacroR${RUNNO}.C
+#echo "Plot_Chrono(${RUNNO},\"SiPM_C\",\"Hold Dump\");" >> tempmacroR${RUNNO}.C
+#echo "Plot_Chrono(${RUNNO},\"SiPM_F\",\"Hold Dump\");" >> tempmacroR${RUNNO}.C
+
 echo "TSeqCollection* cc = gROOT->GetListOfCanvases();" >> tempmacroR${RUNNO}.C
 echo "TString fold = MakeAutoPlotsFolder(\"\");" >> tempmacroR${RUNNO}.C
 echo "for( int ic =0 ; ic <cc->GetEntries(); ++ic ) {" >> tempmacroR${RUNNO}.C
@@ -67,7 +58,3 @@ echo "}" >> tempmacroR${RUNNO}.C
 echo "}" >> tempmacroR${RUNNO}.C
 
 root -l tempmacroR${RUNNO}.C
-
-rm -f tempmacroR${RUNNO}.C
-
-#mv output${RUNNO_FILE}.root "output${RUNNO_FILE}_timerange${start_time}-${stop_time}.root"
