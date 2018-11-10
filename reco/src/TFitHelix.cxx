fc6ea31f98d9c867aa9db9bc3785ef8cee2c1695
Joseph McKenna <joseph.mckenna@cern.ch>
2018-11-10 03:24:33 +0100

    Reduce memory copying and undo TMath::Sqrt(1-cos2(x)) approximation for sin(x)

--- reco/src/TFitHelix.cxx
+++ reco/src/TFitHelix.cxx
@@ -900,9 +900,8 @@ TVector2 TFitHelix::EvaluateMinus(double r2, double c, double phi, double D)
     y0 = D*u0,
     beta = GetBetaMinus(r2, c, D);
   double beta2 = beta*beta;
-  TVector2 p( x0 + u0 * beta * TMath::Sqrt(1.-beta2) / c - v0 * beta2 / c,
-	      y0 + v0 * beta * TMath::Sqrt(1.-beta2) / c + u0 * beta2 / c);
-  return p;
+  return TVector2( x0 + u0 * beta * TMath::Sqrt(1.-beta2) / c - v0 * beta2 / c,
+                   y0 + v0 * beta * TMath::Sqrt(1.-beta2) / c + u0 * beta2 / c);
 }
 
 // FitHelix Radial for -1 Branch, beta -ve root
@@ -914,9 +913,13 @@ TVector2 TFitHelix::EvaluateMinus_(double r2, double c, double phi, double D)
     y0 = D*u0,
     beta = GetBetaMinus(r2, c, D);
   double beta2 = beta*beta;
-  TVector2 p( x0 - u0 * beta * TMath::Sqrt(1.-beta2) / c - v0 * beta2 / c,
-	      y0 - v0 * beta * TMath::Sqrt(1.-beta2) / c + u0 * beta2 / c);
-  return p;
+/*  double beta_c = beta / c;
+  double beta2_c = beta2 / c;
+  double beta_ = beta_c * TMath::Sqrt(1.-beta2);
+  return TVector2( x0 - u0 * beta_ - v0 * beta2_c,
+                   y0 - v0 * beta_ + u0 * beta2_c);*/
+  return TVector2( x0 - u0 * beta * TMath::Sqrt(1.-beta2) / c - v0 * beta2 / c,
+                   y0 - v0 * beta * TMath::Sqrt(1.-beta2) / c + u0 * beta2 / c);
 }
 
 // FitHelix Axial
@@ -929,61 +932,69 @@ double TFitHelix::Evaluate(double s, double l, double z0)
 // FitHelix for +1 Branch, beta +ve root
 TVector3 TFitHelix::EvaluatePlus(double r2, double c, double phi, double D, double l, double z0)
 {
-  double u0 = TMath::Cos(phi);
-  double v0 = TMath::Sqrt(1-u0*u0);
+  double u0 = TMath::Cos(phi),
+    v0 = TMath::Sin(phi);
   double x0 = -D*v0,
     y0 = D*u0,
     beta = GetBetaPlus(r2, c, D);
   double beta2 = beta*beta;
-  TVector3 p( x0 + u0 * beta * TMath::Sqrt(1.-beta2) / c - v0 * beta2 / c,
-	      y0 + v0 * beta * TMath::Sqrt(1.-beta2) / c + u0 * beta2 / c,
-	      z0 + l * GetArcLengthPlus(r2, c, D) );
-  return p;
+  double beta_c = beta / c;
+  double beta2_c = beta2 / c;
+  double beta_ = beta_c * TMath::Sqrt(1.-beta2);
+  return TVector3( x0 + u0 * beta_ - v0 * beta2_c,
+                   y0 + v0 * beta_ + u0 * beta2_c,
+                   z0 + l * GetArcLengthPlus(r2, c, D) );
 }
 
 // FitHelix for -1 Branch, beta +ve root
 TVector3 TFitHelix::EvaluatePlus_(double r2, double c, double phi, double D, double l, double z0)
 {
-  double u0 = TMath::Cos(phi);
-  double v0 = TMath::Sqrt(1-u0*u0);
+  double u0 = TMath::Cos(phi),
+    v0 = TMath::Sin(phi);
   double x0 = -D*v0,
     y0 = D*u0,
     beta = GetBetaPlus(r2, c, D);
   double beta2 = beta*beta;
-  TVector3 p( x0 - u0 * beta * TMath::Sqrt(1.-beta2) / c - v0 * beta2 / c,
-	      y0 - v0 * beta * TMath::Sqrt(1.-beta2) / c + u0 * beta2 / c,
-	      z0 + l * GetArcLengthPlus_(r2, c, D) );
-  return p;
+  double beta_c = beta / c;
+  double beta2_c = beta2 / c;
+  double beta_ = beta_c * TMath::Sqrt(1.-beta2);  
+  return TVector3( x0 - u0 * beta_ - v0 * beta2_c,
+                   y0 - v0 * beta_ + u0 * beta2_c,
+                   z0 + l * GetArcLengthPlus_(r2, c, D) );
 }
 
 // FitHelix for +1 Branch, beta -ve root
 TVector3 TFitHelix::EvaluateMinus(double r2, double c, double phi, double D, double l, double z0)
 {
-  double u0 = TMath::Cos(phi);
-  double v0 = TMath::Sqrt(1-u0*u0);
+  double u0 = TMath::Cos(phi),
+    v0 = TMath::Sin(phi);
   double x0 = -D*v0,
     y0 = D*u0,
     beta = GetBetaMinus(r2, c, D);
   double beta2 = beta*beta;
-  TVector3 p( x0 + u0 * beta * TMath::Sqrt(1.-beta2) / c - v0 * beta2 / c,
-	      y0 + v0 * beta * TMath::Sqrt(1.-beta2) / c + u0 * beta2 / c,
-	      z0 + l * GetArcLengthMinus(r2, c, D) );
-  return p;
+  double beta_c = beta / c;
+  double beta2_c = beta2 / c;
+  double beta_ = beta_c * TMath::Sqrt(1.-beta2);
+  return TVector3( x0 + u0 * beta_ - v0 * beta2_c,
+                   y0 + v0 * beta_ + u0 * beta2_c,
+                   z0 + l * GetArcLengthMinus(r2, c, D) );
 }
 
 // FitHelix for -1 Branch, beta -ve root
 TVector3 TFitHelix::EvaluateMinus_(double r2, double c, double phi, double D, double l, double z0)
 {
-  double u0 = TMath::Cos(phi);
-  double v0 = TMath::Sqrt(1-u0*u0);
+  double u0 = TMath::Cos(phi),
+    v0 = TMath::Sin(phi);
   double x0 = -D*v0,
     y0 = D*u0,
     beta = GetBetaMinus(r2, c, D);
   double beta2 = beta*beta;
-  TVector3 p( x0 - u0 * beta * TMath::Sqrt(1.-beta2) / c - v0 * beta2 / c,
-	      y0 - v0 * beta * TMath::Sqrt(1.-beta2) / c + u0 * beta2 / c,
-	      z0 + l * GetArcLengthMinus_(r2, c, D) );
-  return p;
+  double beta_c = beta / c;
+  double beta2_c = beta2 / c;
+  double beta_ = beta_c * TMath::Sqrt(1.-beta2);
+  return TVector3( x0 - u0 * beta_ - v0 * beta2_c,
+                   y0 - v0 * beta_ + u0 * beta2_c,
+                   z0 + l * GetArcLengthMinus_(r2, c, D) );
 }
 //===============================================================================================
 
@@ -997,8 +1008,7 @@ TVector3 TFitHelix::Evaluate(double r2)
   else if(fBranch==-1)
     r.Set(Evaluate_(r2, fc, fphi0, fD));
 
-  TVector3 p( r.X(), r.Y(), Evaluate(s,flambda,fz0) );
-  return p;
+  return TVector3( r.X(), r.Y(), Evaluate(s,flambda,fz0) );
 }
 
 // FitVertex
@@ -1020,10 +1030,9 @@ TVector3 TFitHelix::EvaluateErrors2(double r2)
     dydD = cp,
     dzdl = GetArcLength(r2);
 
-  TVector3 sigma2(dxdc*dxdc*ferr2c + dxdphi*dxdphi*ferr2phi0 + dxdD*dxdD*ferr2D,
+  return TVector3(dxdc*dxdc*ferr2c + dxdphi*dxdphi*ferr2phi0 + dxdD*dxdD*ferr2D,
 		  dydc*dydc*ferr2c + dydphi*dydphi*ferr2phi0 + dydD*dydD*ferr2D,
 		  dzdl*dzdl*ferr2lambda + ferr2z0);
-  return sigma2;
 }
 
 // Draw routine
@@ -1039,8 +1048,7 @@ TVector3 TFitHelix::EvaluateB(double r2)
     return EvaluateMinus_(r2,fc,fphi0,fD,flambda,fz0);
   else
     {
-      TVector3 p(-9999999.,-9999999.,-9999999.);
-      return p;
+      return TVector3(-9999999.,-9999999.,-9999999.);
     }
 }
 
@@ -1068,10 +1076,9 @@ TVector3 TFitHelix::EvaluateErrors2B(double r2)
     dydD = cp,
     dzdl = GetArcLengthB(r2);
 
-  TVector3 sigma2(dxdc*dxdc*ferr2c + dxdphi*dxdphi*ferr2phi0 + dxdD*dxdD*ferr2D,
+  return TVector3(dxdc*dxdc*ferr2c + dxdphi*dxdphi*ferr2phi0 + dxdD*dxdD*ferr2D,
 		  dydc*dydc*ferr2c + dydphi*dydphi*ferr2phi0 + dydD*dydD*ferr2D,
 		  dzdl*dzdl*ferr2lambda + ferr2z0);
-  return sigma2;
 }
 
 // FitVertex
@@ -1081,10 +1088,9 @@ TVector3 TFitHelix::GetPosition(double s)
     cp=TMath::Cos(fphi0),sp=TMath::Sin(fphi0),
     crs=TMath::Cos(rho*s),srs=TMath::Sin(rho*s);
 
-  TVector3 p(fx0+cp*srs/rho-sp*(1.-crs)/rho,
+  return TVector3(fx0+cp*srs/rho-sp*(1.-crs)/rho,
 	     fy0+sp*srs/rho+cp*(1.-crs)/rho,
 	     fz0+flambda*s);
-  return p;
 }
 
 // FitVertex::EvaluateMeanPointError2
@@ -1100,10 +1106,9 @@ TVector3 TFitHelix::GetError2(double s)
     dydc = -(1.-crs)*cp/rho/fc + s*cp*srs/fc + s*crs*sp/fc - srs*sp/rho/fc,
     dydp = fx0 + cp*srs/rho - (1.-crs)*sp/rho,
     dydD = cp;
-  TVector3 sigma2(dxdc*dxdc*ferr2c + dxdp*dxdp*ferr2phi0 + dxdD*dxdD*ferr2D,
+  return TVector3(dxdc*dxdc*ferr2c + dxdp*dxdp*ferr2phi0 + dxdD*dxdD*ferr2D,
 		  dydc*dydc*ferr2c + dydp*dydp*ferr2phi0 + dydD*dydD*ferr2D,
 		  s*s*ferr2lambda + ferr2z0);
-  return sigma2;
 }
 
 //==============================================================================================
