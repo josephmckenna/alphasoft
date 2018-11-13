4d6ee2f7689e58e9ba9473d1be8dc2d020614671
Joseph McKenna <joseph.mckenna@cern.ch>
2018-11-13 15:57:05 +0100

    More gcc8 warnings

--- analib/RootUtils/PrintTools.cxx
+++ analib/RootUtils/PrintTools.cxx
@@ -4,17 +4,19 @@
 
 void PrintSequences(int runNumber, int SeqNum)
 {
-  TTree *sequencerTree = Get_Seq_Event_Tree(runNumber);
-  TSeq_Event *seqEvent = new TSeq_Event();
-  sequencerTree->SetBranchAddress("SequencerEvent", &seqEvent);
-  for (Int_t i = 0; i < sequencerTree->GetEntries(); i++)
-  {
-    sequencerTree->GetEntry(i);
-    if (SeqNum>0)
-    if (SeqNum!=seqEvent->GetSeqNum())
-      continue;
-    
-
+   TTree *sequencerTree = Get_Seq_Event_Tree(runNumber);
+   TSeq_Event *seqEvent = new TSeq_Event();
+   sequencerTree->SetBranchAddress("SequencerEvent", &seqEvent);
+   for (Int_t i = 0; i < sequencerTree->GetEntries(); i++)
+   {
+      sequencerTree->GetEntry(i);
+      if (SeqNum>0)
+      {
+         if (SeqNum!=seqEvent->GetSeqNum())
+         {
+            continue;
+         }
+      }
       std::cout<< "Sequencer Name:" << seqEvent->GetSeq().Data()
                << "\t Seq Num:" << seqEvent->GetSeqNum()
                << "\t ID:" << seqEvent->GetID()
@@ -22,8 +24,7 @@ void PrintSequences(int runNumber, int SeqNum)
                << "\t Description: " << seqEvent->GetDescription()
                << "\t RunTime: "<< GetRunTimeOfEvent(runNumber,seqEvent)
                << std::endl;
-  }
-  
+   }
    delete seqEvent;
    delete sequencerTree;
 }
