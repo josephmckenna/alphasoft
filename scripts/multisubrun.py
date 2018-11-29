0958bc0e57a7bac81ef5e878719bdb6d921e7c5b
Andrea <andrea.capra85@gmail.com>
2018-11-29 20:12:13 +0100

    add functions to remove subdirs and to merge logfiles. argparser now takes care of the inputs

--- scripts/multisubrun.py
+++ scripts/multisubrun.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 from os import mkdir,chdir,environ,getcwd,path
-from shutil import copy
+from shutil import copy, rmtree
 from time import time
 import datetime
 import re
 import subprocess as sp
 import multiprocessing as mp
-from sys import argv
 from pathlib import Path
+import argparse
 
 def getlogname(cmd):
     run=int(re.search('[0-9]{5}',cmd).group(0))
@@ -61,6 +61,8 @@ def assemble(run,argx):
         if len(argx) > 0:
             cmd+=' -- '
         for a in argx:
+            if not a.isdigit():
+                cmd+='--'
             cmd+=a
             cmd+=' '
         cmdlist.append(cmd)
@@ -84,18 +86,61 @@ def addsubs(run):
     print(cmd)
     sp.call(cmd, shell=True, stdout=open('/dev/null','w'),stderr=sp.STDOUT)
     
+def addlogs(run):
+    foutname='%s/R%d.log'%(environ['AGRELEASE'],run)
+    with open(foutname,'w') as fout:
+        sub=0
+        subrun='%s/R%d/sub%03d/output%05d.root'%(environ['AGRELEASE'],run,sub,run)
+        subfile=Path(subrun)
+        while subfile.is_file():
+            finname='%s/R%d/sub%03d/R%d.log'%(environ['AGRELEASE'],run,sub,run)
+            print(finname)
+            with open(finname,'r') as fin:
+                for line in fin:
+                    fout.write(line)
+            sub+=1
+            subrun='%s/R%d/sub%03d/output%05d.root'%(environ['AGRELEASE'],run,sub,run)
+            subfile=Path(subrun)
+
+def rmsubs(run):
+    folder_name=environ['AGRELEASE']+'/R'+str(run)
+    rmtree(folder_name, ignore_errors=True)
+
 
 if __name__=='__main__':
  
-    run=int(argv[1])
-    argx=[a for a in argv[2:]]
+    parser = argparse.ArgumentParser(description='Parallelize the analysis of multiple subruns of a given run')
 
-    commands=assemble(run,argx)
+    parser.add_argument('run', type=int,
+                        help='Run number')
+    parser.add_argument('-o','--opt', nargs='*',
+                        help='optional arguments for agana\'s modules')
+    
+    parser.add_argument('-m', '--merge', action='store_true',
+                        help='merge log files')
+    parser.add_argument('-r', '--remove', action='store_true',
+                        help='remove subruns folders')
 
-    count=int(len(commands)/8)
-    pool=mp.Pool(processes=count)
+    parser.add_argument('-p', '--proc', type=int,
+                        default=mp.cpu_count(),
+                        help='number of concurrent subprocesses')
+
+    parser.add_argument('-s', '--subs', action='store_false',
+                        help='do not merge subruns')
+    
+    args = parser.parse_args()
+
+    commands=assemble(args.run,args.opt)
+
+    pool=mp.Pool(processes=args.proc)
     pool.map(work, commands)
     pool.close()
     pool.join()
     
-    addsubs(run)
+    if args.subs:
+        addsubs(args.run)
+
+    if args.merge:
+        addlogs(args.run)
+    if args.remove:
+        rmsubs(args.run)
