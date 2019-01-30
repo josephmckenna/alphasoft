ed6875b4f4a8c6ffadc1eed9a7d831483cfffadc
Lars Martin <lmartin@tardis>
2019-01-30 12:21:48 -0800

    More histograms

--- ana/macros/AnalyzeLaserTrees.C
+++ ana/macros/AnalyzeLaserTrees.C
@@ -29,6 +29,42 @@ void padAnalysis(TTree *fPadTree){
     c->cd(2);
     TH2D *hp2 = new TH2D("hp2","timecut 2",576,0,576,32,0,32);
     fPadTree->Draw("col:row>>hp2",timecut2,"colz");
+
+    c = new TCanvas("cpad2","pad col amp",1100,1100);
+    c->Divide(1,2);
+    c->cd(1);
+    TH2D *hpc1 = new TH2D("hpc1","timecut 1",32,0,32,520,0,4160);
+    fPadTree->Draw("amp:col>>hpc1",timecut1,"colz");
+    c->cd(2);
+    TH2D *hpc2 = new TH2D("hpc2","timecut 2",32,0,32,520,0,4160);
+    fPadTree->Draw("amp:col>>hpc2",timecut2,"colz");
+
+    c = new TCanvas("cpad2t","pad col time",1100,1100);
+    c->Divide(1,2);
+    c->cd(1);
+    TH2D *hpct1 = new TH2D("hpct1","timecut 1",32,0,32,800,1200,2000);
+    fPadTree->Draw("time:col>>hpct1",timecut1,"colz");
+    c->cd(2);
+    TH2D *hpct2 = new TH2D("hpct2","timecut 2",32,0,32,500,5100,5600);
+    fPadTree->Draw("time:col>>hpct2",timecut2,"colz");
+
+    c = new TCanvas("cpad3","pad row amp",1100,1100);
+    c->Divide(1,2);
+    c->cd(1);
+    TH2D *hpr1 = new TH2D("hpr1","timecut 1",576,0,576,520,0,4160);
+    fPadTree->Draw("amp:row>>hpr1",timecut1,"colz");
+    c->cd(2);
+    TH2D *hpr2 = new TH2D("hpr2","timecut 2",576,0,576,520,0,4160);
+    fPadTree->Draw("amp:row>>hpr2",timecut2,"colz");
+
+    c = new TCanvas("cpad3t","pad row time",1100,1100);
+    c->Divide(1,2);
+    c->cd(1);
+    TH2D *hprt1 = new TH2D("hprt1","timecut 1",576,0,576,800,1200,2000);
+    fPadTree->Draw("time:row>>hprt1",timecut1,"colz");
+    c->cd(2);
+    TH2D *hprt2 = new TH2D("hprt2","timecut 2",576,0,576,500,5100,5600);
+    fPadTree->Draw("time:row>>hprt2",timecut2,"colz");
 }
 
 void awAnalysis(TTree *fAnodeTree){
