028dc2436e73834aba98cfb86d60b47f91706248
Lars Martin <lmartin@triumf.ca>
2019-01-29 17:27:45 -0800

    Laser analysis macro stuff.

--- ana/macros/AnalyzeLaserTrees.C
+++ ana/macros/AnalyzeLaserTrees.C
@@ -1,17 +1,52 @@
-{
-    TTree *fPadTree = (TTree*)_file0->FindObjectAny("fPadTree");
-    TH2D hp1("hp1","",576,0,576,32,0,32);
-    hp1.SetMarkerColor(kRed);
-    fPadTree->Draw("col:row>>hp1","time<4000");
-    TH2D hp2("hp2","",576,0,576,32,0,32);
-    fPadTree->Draw("col:row>>hp2","time>4000","same");
+TString timecut1 = "time>1200 & time<2000";
+TString timecut2 = "time>5100 & time<5600";
+int awbin = 10;
+int padbin = 16;
 
+void timeAnalysis(TTree *fPadTree, TTree *fAnodeTree){
+    TCanvas *c = new TCanvas("ctime","time",800,600);
+    int nawbin = 7000/awbin;
+    int awmax = awbin*nawbin;
+    TH1D *hat = new TH1D("hat","anode times",nawbin,0,awmax);
+    int npadbin = 7000/padbin;
+    int padmax = padbin*npadbin;
+    TH1D *hpt = new TH1D("hpt","pad times",npadbin,0,padmax);
+    hpt->SetLineColor(kRed);
+    hat->SetBit(TH1::kNoTitle);
+    hpt->SetBit(TH1::kNoTitle);
+    fAnodeTree->Draw("time >> hat");
+    fPadTree->Draw("time >> hpt","","same");
+    c->BuildLegend();
+    c->Update();
+}
+
+void padAnalysis(TTree *fPadTree){
+    TCanvas *c = new TCanvas("cpad","pads",1100,1100);
+    c->Divide(1,2);
+    c->cd(1);
+    TH2D *hp1 = new TH2D("hp1","timecut 1",576,0,576,32,0,32);
+    fPadTree->Draw("col:row>>hp1",timecut1,"colz");
+    c->cd(2);
+    TH2D *hp2 = new TH2D("hp2","timecut 2",576,0,576,32,0,32);
+    fPadTree->Draw("col:row>>hp2",timecut2,"colz");
+}
+
+void awAnalysis(TTree *fAnodeTree){
+    TCanvas *c = new TCanvas("caw","AW",1100,1100);
+    c->Divide(1,2);
+    c->cd(1);
+    TH2D *ha1 = new TH2D("ha1","timecut 1",256,256,512,700,0,70000);
+    fAnodeTree->Draw("amp:wire>>ha1",timecut1,"colz");
+    c->cd(2);
+    TH2D *ha2 = new TH2D("ha2","timecut 2",256,256,512,700,0,70000);
+    fAnodeTree->Draw("amp:wire>>ha2",timecut2,"colz");
+}
+
+void AnalyzeLaserTrees(){
+    TTree *fPadTree = (TTree*)_file0->FindObjectAny("fPadTree");
     TTree *fAnodeTree = (TTree*)_file0->FindObjectAny("fAnodeTree");
-    fAnodeTree->Draw("time");
-    TH2D ha1("ha1","",256,256,512,700,0,70000);
-    ha1.SetMarkerColor(kRed);
-    fAnodeTree->Draw("amp:wire>>ha1","time<4000");
-    TH2D ha2("ha2","",256,256,512,700,0,70000);
-    ha2.SetMarkerColor(kBlack);
-    fAnodeTree->Draw("amp:wire>>ha2","time>4000","same");
+    gStyle->SetOptStat(0);
+    timeAnalysis(fPadTree, fAnodeTree);
+    padAnalysis(fPadTree);
+    awAnalysis(fAnodeTree);
 }
