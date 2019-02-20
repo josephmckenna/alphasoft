7e04482dd13f3311bd948d2d6cd6a7a5fb1f9d20
Andrea <andrea.capra85@gmail.com>
2019-02-20 01:21:31 +0100

    add help to reco module

--- ana/src/reco_module.cxx
+++ ana/src/reco_module.cxx
@@ -642,10 +642,17 @@ public:
    void Help()
    {
       printf("RecoModuleFactory::Help\n");
-      printf("\t---usetimerange 123.4 567.8\t\tLimit reconstruction to a time range\n");
-      printf("\t---useeventrange 123 456\t\tLimit reconstruction to an event range\n");
-      printf("\t---Bmap xx\t\tSet STR using Babcock Map OBSOLETE!!! This is now default\n");
-      printf("--loadcalib\t\t Load calibration STR file made by this analysis\n");
+      printf("\t--usetimerange 123.4 567.8\t\tLimit reconstruction to a time range\n");
+      printf("\t--useeventrange 123 456\t\tLimit reconstruction to an event range\n");
+      //      printf("\t--Bmap xx\t\tSet STR using Babcock Map OBSOLETE!!! This is now default\n");
+      printf("\t--Bfield 0.1234\t\t set magnetic field value in Tesla\n");
+      printf("\t--loadcalib\t\t Load calibration STR file made by this analysis\n");
+      printf("\t--recoff\t\t disable reconstruction");
+      printf("\t--diag\t\t enable histogramming");
+      printf("\t--anasettings /path/to/settings.json\t\t load the specified analysis settings");
+      printf("\t--rfudge 0.12\t\t Fudge or alter the STR radius by a fraction");
+      printf("\t--pfudge 0.12\t\t Fudge or alter the STR azimuth by a fraction");
+      printf("\t--fiduc\t\t skip over points in the inhomogenous field region");
    }
    void Usage()
    {
