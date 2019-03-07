c3b8050c00aa3747674be40c13d71a7454eedc69
a-capra <andrea.capra85@gmail.com>
2019-03-06 17:00:03 -0800

    control printing/drawing

--- simulation/g4ana/g4ana.cc
+++ simulation/g4ana/g4ana.cc
@@ -52,8 +52,14 @@ int main(int argc, char** argv)
   TClonesArray* PADsignals = new TClonesArray("TWaveform");
   tSig->SetBranchAddress("PAD",&PADsignals);
 
-  double ADCThres=5000., PWBThres=100., ADCpeak=1000., PWBpeak=10.;
+  //double ADCThres=5000., PWBThres=100., ADCpeak=1000., PWBpeak=10.;
+  // double ADCThres=atof(argv[2]), PWBThres=atof(argv[3]), 
+  // ADCpeak=atof(argv[4]), PWBpeak=atof(argv[5]);
+  double ADCThres=10., PWBThres=10., ADCpeak=5., PWBpeak=10.;
   Deconv d(ADCThres, PWBThres, ADCpeak, PWBpeak);
+
+  // ofstream fout("deconv_goodness.dat", ios::out | ios::app);
+  // fout<<ADCThres<<"\t"<<PWBThres<<"\t"<<ADCpeak<<"\t"<<PWBpeak<<"\t";
 
   string json_file = "ana_settings.json";
   ostringstream json_filepath;
@@ -64,14 +70,27 @@ int main(int argc, char** argv)
   double B=1.;
   Reco r(json_filepath.str(),B);
 
-  TApplication app("g4ana",&argc,argv);
+  bool draw = true;
+  bool verb = false;
 
-  TCanvas* csig = new TCanvas("csig","csig",1400,1400);
-  csig->Divide(2,2);
   double tmax = 4500.;
 
-  TCanvas* creco = new TCanvas("creco","creco",1400,1400);
-  creco->Divide(2,2);
+
+  TApplication* app;
+  if( draw )
+    app = new TApplication("g4ana",&argc,argv);
+
+  TCanvas* csig;
+  TCanvas* creco;
+
+  if( draw )
+    {
+      csig = new TCanvas("csig","csig",1400,1400);
+      csig->Divide(2,2);
+
+      creco = new TCanvas("creco","creco",1400,1400);
+      creco->Divide(2,2);
+    }
 
   for( int i=0; i<tSig->GetEntries(); ++i )
     {
@@ -81,28 +100,37 @@ int main(int argc, char** argv)
       int nsig = d.FindAnodeTimes( AWsignals );
       cout<<"[main]# "<<i<<"\tFindAnodeTimes: "<<nsig<<endl;
       // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+      // fout<<std::setprecision(15)<<Average( d.GetAnodeDeconvRemainder() )<<"\t";
 
-      //PrintSignals( d.GetAnodeSignal() );
-      TH1D* haw = PlotSignals( d.GetAnodeSignal(), "anodes" );
-      haw->Scale(1./haw->Integral());
-      haw->SetLineColor(kRed);
-      cout<<"[main]# "<<i<<"\tPlotAnodeTimes: "<<haw->GetEntries()<<endl;
-      csig->cd(1);
-      haw->Draw("hist");
-      haw->SetTitle("Deconv Times");
-      haw->GetXaxis()->SetRangeUser(0.,tmax);
-     
+      if( verb ) PrintSignals( d.GetAnodeSignal() );
+      TH1D* haw;
+      if( draw )
+	{
+	  haw = PlotSignals( d.GetAnodeSignal(), "anodes" );
+	  haw->Scale(1./haw->Integral());
+	  haw->SetLineColor(kRed);
+	  cout<<"[main]# "<<i<<"\tPlotAnodeTimes: "<<haw->GetEntries()<<endl;
+	  csig->cd(1);
+	  haw->Draw("hist");
+	  haw->SetTitle("Deconv Times");
+	  haw->GetXaxis()->SetRangeUser(0.,tmax);
+	}
+
       // pad deconv
       nsig = d.FindPadTimes( PADsignals );
       cout<<"[main]# "<<i<<"\tFindPadTimes: "<<nsig<<endl;
-      //PrintSignals( d.GetPadSignal() );
+      if( verb ) PrintSignals( d.GetPadSignal() );
       // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+      // fout<<std::setprecision(15)<<Average( d.GetPadDeconvRemainder() )<<endl;
 
-      TH1D* hpads = PlotSignals( d.GetPadSignal(), "pads" );
-      hpads->Scale(1./hpads->Integral());
-      hpads->SetLineColor(kBlue);
-      csig->cd(1);
-      hpads->Draw("histsame");
+      if( draw )
+	{
+	  TH1D* hpads = PlotSignals( d.GetPadSignal(), "pads" );
+	  hpads->Scale(1./hpads->Integral());
+	  hpads->SetLineColor(kBlue);
+	  csig->cd(1);
+	  hpads->Draw("histsame");
+	}
 
       m.Init();
 
@@ -111,31 +139,33 @@ int main(int argc, char** argv)
       cout<<"[main]# "<<i<<"\tCombinePads: "<<m.GetCombinedPads()->size()<<endl;
       // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
       
-      //PrintSignals( m.GetCombinedPads() );
-      TH1D* hcombpads = PlotSignals( m.GetCombinedPads(), "combinedpads" );
-      hcombpads->Scale(1./hcombpads->Integral());
-      hcombpads->SetLineColor(kBlue);
-      csig->cd(2);
-      haw->Draw("hist");
-      hcombpads->Draw("histsame");
+      if( verb ) PrintSignals( m.GetCombinedPads() );
+      if( draw )
+	{
+	  TH1D* hcombpads = PlotSignals( m.GetCombinedPads(), "combinedpads" );
+	  hcombpads->Scale(1./hcombpads->Integral());
+	  hcombpads->SetLineColor(kBlue);
+	  csig->cd(2);
+	  haw->Draw("hist");
+	  hcombpads->Draw("histsame");
 
-      TH2D* hmatch = PlotSignals( d.GetAnodeSignal(), m.GetCombinedPads(), "sector");
-      //TH2D* hmatch = PlotSignals( d.GetAnodeSignal(), d.GetPadSignal(), "sector");
-      csig->cd(3);
-      hmatch->Draw();
-      hmatch->GetXaxis()->SetRangeUser(0.,tmax);
-      hmatch->GetYaxis()->SetRangeUser(0.,tmax);
+	  TH2D* hmatch = PlotSignals( d.GetAnodeSignal(), m.GetCombinedPads(), "sector");
+	  //TH2D* hmatch = PlotSignals( d.GetAnodeSignal(), d.GetPadSignal(), "sector");
+	  csig->cd(3);
+	  hmatch->Draw();
+	  hmatch->GetXaxis()->SetRangeUser(0.,tmax);
+	  hmatch->GetYaxis()->SetRangeUser(0.,tmax);
 
-      TH1D* hoccaw = PlotOccupancy( d.GetAnodeSignal(), "anodes" );
-      hoccaw->Scale(1./hoccaw->Integral());
-      hoccaw->SetLineColor(kRed);
-      TH1D* hocccombpads = PlotOccupancy( m.GetCombinedPads(), "pads" );
-      hocccombpads->Scale(1./hocccombpads->Integral());
-      hocccombpads->SetLineColor(kBlue);
-      csig->cd(4);
-      hoccaw->Draw("hist");
-      hocccombpads->Draw("histsame");
-
+	  TH1D* hoccaw = PlotOccupancy( d.GetAnodeSignal(), "anodes" );
+	  hoccaw->Scale(1./hoccaw->Integral());
+	  hoccaw->SetLineColor(kRed);
+	  TH1D* hocccombpads = PlotOccupancy( m.GetCombinedPads(), "pads" );
+	  hocccombpads->Scale(1./hocccombpads->Integral());
+	  hocccombpads->SetLineColor(kBlue);
+	  csig->cd(4);
+	  hoccaw->Draw("hist");
+	  hocccombpads->Draw("histsame");
+	}
       // match electrodes
       m.MatchElectrodes( d.GetAnodeSignal() );
       cout<<"[main]# "<<i<<"\tMatchElectrodes: "<<m.GetSpacePoints()->size()<<endl;
@@ -144,20 +174,25 @@ int main(int argc, char** argv)
       r.Reset();
 
       // reco points
-      //r.SetTrace(true);
+      if( verb ) r.SetTrace(true);
       r.AddSpacePoint( m.GetSpacePoints() );
       // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-      tGarf->GetEntry(i);
-      PlotMCpoints(creco,garfpp_hits);
+      if( draw )
+	{
+	  tGarf->GetEntry(i);
+	  PlotMCpoints(creco,garfpp_hits);
 
-      const TClonesArray* sp = r.GetPoints();      
-      PlotRecoPoints(creco,sp);
+	  const TClonesArray* sp = r.GetPoints();      
+	  PlotRecoPoints(creco,sp);
 
-      DrawTPCxy(creco);
+	  DrawTPCxy(creco);
+	}
     }// events loop
+  // fout.close();
 
-  app.Run();
+  if( draw )
+    app->Run();
 
   return 0;
 }
