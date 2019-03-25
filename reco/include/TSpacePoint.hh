54d4337f0aca2eef663da7412926c5ad322b47f3
a-capra <andrea.capra85@gmail.com>
2019-03-25 14:04:26 -0700

    clean-up

--- reco/include/TSpacePoint.hh
+++ reco/include/TSpacePoint.hh
@@ -39,19 +39,10 @@ public:
 	      double er_str, double ep_str, double ez_from_pad,
 	      double amplitude);
   void Setup(int anode, int pad_col, int pad_row, 
-	      double t,
-	      double r, double lorentz, double z_from_pad,
-	      double er_str, double ep_str, double ez_from_pad,
-	      double amplitude);
-  // TSpacePoint(int w, int p, double t,
-  // 	      double r, double phi,
-  // 	      double er,
-  // 	      double H=999999.);
-
-  // TSpacePoint(int w, int s, int i, double t,
-  // 	      double r, double phi,
-  // 	      double er,
-  // 	      double H=999999.);
+	     double t,
+	     double r, double lorentz, double z_from_pad,
+	     double er_str, double ep_str, double ez_from_pad,
+	     double amplitude);
   
   TSpacePoint(double x, double y, double z,
 	      double ex, double ey, double ez);
