de2f1ff1f44a72664ee43cbb3d1c0bb47bd5eec5
Joseph McKenna <joseph.mckenna@cern.ch>
2019-05-09 16:41:14 +0200

    Check for suppression file for valgrind

--- scripts/UnitTest/LeakCheck.sh
+++ scripts/UnitTest/LeakCheck.sh
@@ -84,10 +84,12 @@ echo $LEAKTEST
 cd $AGRELEASE
 ls -l -h *.exe
 echo "Running..."
-
+if [ -f ${ROOTSYS}/etc/valgrind-root.supp ]; then
+SUPP="--suppressions=${ROOTSYS}/etc/valgrind-root.supp"
+fi
 set -x
 #Suppress false positives: https://root.cern.ch/how/how-suppress-understood-valgrind-false-positives
-valgrind --leak-check=full --error-limit=no --suppressions=${ROOTSYS}/etc/valgrind-root.supp  --log-file="${LEAKTEST}" ./agana.exe ${Event_Limit} run${RUNNO}sub000.mid.lz4 ${MODULESFLAGS} &> ${ALPHATEST}
+valgrind --leak-check=full --error-limit=no ${SUPP} --log-file="${LEAKTEST}" ./agana.exe ${Event_Limit} run${RUNNO}sub000.mid.lz4 ${MODULESFLAGS} &> ${ALPHATEST}
 set +x
 
  
