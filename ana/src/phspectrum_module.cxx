6e78488695ce271aa0ed3a68502e65fcb40fc8d5
a-capra <andrea.capra85@gmail.com>
2019-05-10 18:03:33 -0700

    support for magnetic field

--- ana/src/phspectrum_module.cxx
+++ ana/src/phspectrum_module.cxx
@@ -12,9 +12,11 @@ class PHspectrumFlags
 {
 public:  
    bool fEnabled;
+   double fMagneticField;
    AnaSettings* ana_settings;
   
    PHspectrumFlags():fEnabled(false),// ctor
+                     fMagneticField(1.0),
                      ana_settings(0)
    { }
   
@@ -82,14 +84,76 @@ public:
       
       std::vector<signal> aws = SigFlow->awSig;
       std::vector<signal> pads = SigFlow->pdSig;
-            
-      LinePHspect(e,adc32,pwb,aws,pads);
+
+      if( fFlags->fMagneticField > 0. )
+         HelPHspect(e,adc32,pwb,aws,pads);
+      else
+         LinePHspect(e,adc32,pwb,aws,pads);
       
 #ifdef _TIME_ANALYSIS_
       if (TimeModules) flow=new AgAnalysisReportFlow(flow,"PHspectrum_module");
 #endif
       return flow;
    }
+
+   void FillHistos(const TObjArray* points,
+                   std::vector<signal> &adc32, std::vector<signal> &pwb,
+                   std::vector<signal> &aws, std::vector<signal> &pads)
+   {
+      int nPoints = points->GetEntriesFast();
+      for(int j=0; j<nPoints; ++j )
+         {
+            TSpacePoint* sp = (TSpacePoint*) points->At(j);
+            double time = sp->GetTime();
+            int wire = sp->GetWire();
+            for(auto& s: aws)
+               {
+                  if( s.idx == wire && fabs(s.t - time) < 1.e-3 )
+                     {
+                        hawphspect->Fill(double(wire),s.height);
+                     }
+               }
+            for(auto& s: adc32)
+               {
+                  if( s.idx == wire )
+                     {
+                        hadcphspect->Fill(double(wire),s.height);
+                     }
+               }
+            
+            int sec,row,pad=sp->GetPad();
+            pmap->get(pad,sec,row);
+            for(auto& s: pads)
+               {
+                  if( s.idx == row && s.sec == sec && fabs(s.t - time) < fCoincTime )
+                     {
+                        hpadphspect->Fill(double(pad),s.height);
+                     }
+               }
+            for(auto& s: pwb)
+               {
+                  if( s.idx == row && s.sec == sec )
+                     {
+                        hpwbphspect->Fill(double(pad),s.height);
+                     }
+               }
+         }// points loop 
+   }
+
+   void HelPHspect(TStoreEvent* anEvent,
+                   std::vector<signal> &adc32, std::vector<signal> &pwb,
+                   std::vector<signal> &aws, std::vector<signal> &pads)
+   {
+      const TObjArray* lines = anEvent->GetLineArray();
+      int nTracks = lines->GetEntriesFast();
+      std::cout<<"PHspectrum::HelPHspect event # "<<anEvent->GetEventNumber()<<" @ "<<anEvent->GetTimeOfEvent()<<"s found: "<<nTracks<<" tracks"<<std::endl;
+      for( int i=0; i<nTracks; ++i )
+         {
+            TStoreLine* l = (TStoreLine*) lines->At(i);
+            const TObjArray* points = l->GetSpacePoints();
+            FillHistos( points, adc32, pwb, aws, pads );
+         }// tracks loop
+   }// function: HelPHspect
 
    void LinePHspect(TStoreEvent* anEvent,
                     std::vector<signal> &adc32, std::vector<signal> &pwb,
@@ -102,48 +166,7 @@ public:
          {
             TStoreLine* l = (TStoreLine*) lines->At(i);
             const TObjArray* points = l->GetSpacePoints();
-            int nPoints = points->GetEntriesFast();
-            for(int j=0; j<nPoints; ++j )
-               {
-                  TSpacePoint* sp = (TSpacePoint*) points->At(j);
-                  double time = sp->GetTime();
-                  int wire = sp->GetWire();
-                  for(auto& s: aws)
-                     {
-                        if( s.idx == wire && fabs(s.t - time) < 1.e-3 )
-                           {
-                              // do something
-                              hawphspect->Fill(double(wire),s.height);
-                           }
-                     }
-                  for(auto& s: adc32)
-                     {
-                        if( s.idx == wire )
-                           {
-                              // do something
-                              hadcphspect->Fill(double(wire),s.height);
-                           }
-                     }
-                  
-                  int sec,row,pad=sp->GetPad();
-                  pmap->get(pad,sec,row);
-                  for(auto& s: pads)
-                     {
-                        if( s.idx == row && s.sec == sec && fabs(s.t - time) < fCoincTime )
-                           {
-                              // do something
-                              hpadphspect->Fill(double(pad),s.height);
-                           }
-                     }
-                  for(auto& s: pwb)
-                     {
-                        if( s.idx == row && s.sec == sec )
-                           {
-                              // do something
-                              hpwbphspect->Fill(double(pad),s.height);
-                           }
-                     }
-               }// points loop
+            FillHistos( points, adc32, pwb, aws, pads );
          }// tracks loop
    }// function: LinePHspect
 };
