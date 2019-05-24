0073897c20e025c4d51cd2edc85a921052afe958
Joseph McKenna <joseph.mckenna@cern.ch>
2019-05-24 16:05:14 +0200

    Upgrade elog post of Simulation tests

--- scripts/gitChecker/SimChecker.sh
+++ scripts/gitChecker/SimChecker.sh
@@ -7,7 +7,7 @@ BRANCH=`git branch --remote --verbose --no-abbrev --contains | sed -rne 's/^[^\/
 
 mkdir -p $AGRELEASE/simlogs
 
-start_ana=`date +%s`
+start=`date +%s`
 
 cd ${AGRELEASE}/simulation
 rm *.root
@@ -19,7 +19,23 @@ ROOT_FILE=`ls ../simulation/*.root`
 echo "root file: ${ROOT_FILE}"
 ./g4ana.exe --rootfile ${ROOT_FILE} &>$AGRELEASE/simlogs/analysis_of_sim_${GITHASH}.log
 #cp $AGRELEASE/simlogs/analysis_${GITHASH}.log ~/${GITHASH}/
+end=`date +%s`
 
+   #Elog message:
+   runtime=$((end-start))
+   aganatime=$((end_ana-start_ana))
+   mtaganatime=$((mtend_ana-mtstart_ana))
+   echo "Simulation time: ${runtime}s (agana time st/mt: ${aganatime}/${mtaganatime}s) " >  $AGRELEASE/simlogs/elogMessage.txt
+   echo "Making diff of analysis..."
+   #Catch exit state (1 if there is a differnce) with ||
+   echo "Diff of analysis:
+   
+   " >>$AGRELEASE/simlogs/elogMessage.txt
+   diff -u `ls -tr $AGRELEASE/simlogs/analysis_of_sim_* | tail -n 2 ` > $AGRELEASE/simlogs/AnalysisDiff.log || :
+   if [ -f $AGRELEASE/simlogsAnalysisDiff.log ]; then
+       cat $AGRELEASE/simlogs/AnalysisDiff.log
+       cat $AGRELEASE/simlogs/AnalysisDiff.log >> $AGRELEASE/simlogs/elogMessage.txt
+   fi
 
 if [[ $(hostname -s) = *runner* ]]; then
 
@@ -34,7 +50,7 @@ if [[ $(hostname -s) = *runner* ]]; then
 
    #Prepare files for elog command
    HOSTNAME=`hostname`
-   FILES="$FILES -f ~/gitCheckerReports/${GITHASH}/simlogs/imulation_${GITHASH}.log"
+   FILES="$FILES -f ~/gitCheckerReports/${GITHASH}/simlogs/simulation_${GITHASH}.log"
    FILES="$FILES -f ~/gitCheckerReports/${GITHASH}/simlogs/analysis_of_sim_${GITHASH}.log"
    #for file in `ls ${AGRELEASE}/${GITHASH}/SpeedTest`; do
    #  FILES="$FILES -f ~/gitCheckerReports/${GITHASH}/SimTest/${file}"
@@ -42,8 +58,8 @@ if [[ $(hostname -s) = *runner* ]]; then
    echo "Files to attach: ${FILES}"
    scp -r $AGRELEASE/simlogs alpha@alphadaq:~/gitCheckerReports/${GITHASH}/
 
-   echo "~/packages/elog/elog -h localhost -a Author=${HOSTNAME} -a Run=\"${RUNNO}\" -a Subject=\"git-checker: $GITHASH (${BRANCH}) - SpeedTest\"  -r $ELOG_NO -a Tags=\"gitcheck\" -m ~/gitCheckerReports/${GITHASH}/SpeedTest/elogMessage.txt ${FILES}  -p 8080 -l AutoAnalysis -v "
-   ssh -X alpha@alphadaq "~/packages/elog/elog -h localhost -a Author=${HOSTNAME} -a Run=\"${RUNNO}\" -a Subject=\"git-checker: $GITHASH (${BRANCH}) - SpeedTest\"  -r $ELOG_NO -a Tags=\"gitcheck\" -m ~/gitCheckerReports/${GITHASH}/SpeedTest/elogMessage.txt ${FILES}  -p 8080 -l AutoAnalysis -v " 
+   echo "~/packages/elog/elog -h localhost -a Author=${HOSTNAME} -a Run=\"${RUNNO}\" -a Subject=\"git-checker: $GITHASH (${BRANCH}) - AGSimulation\"  -r $ELOG_NO -a Tags=\"gitcheck\" -m ~/gitCheckerReports/${GITHASH}/simlogs/elogMessage.txt ${FILES}  -p 8080 -l AutoAnalysis -v "
+   ssh -X alpha@alphadaq "~/packages/elog/elog -h localhost -a Author=${HOSTNAME} -a Run=\"${RUNNO}\" -a Subject=\"git-checker: $GITHASH (${BRANCH}) - AGSimulation\"  -r $ELOG_NO -a Tags=\"gitcheck\" -m ~/gitCheckerReports/${GITHASH}/simlogs/elogMessage.txt ${FILES}  -p 8080 -l AutoAnalysis -v " 
    # &> elog_posting.log
    #cat elog_posting.log
 
