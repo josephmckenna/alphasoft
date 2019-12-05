6c8b88a173605bee3ad2f2cd779f96c0ddf62c80
Joseph McKenna <joseph.mckenna@cern.ch>
2019-12-05 12:50:21 +0100

    Stop using EOS in CI, it seems eos has been removed fromt he docker image... hopefully EOS is still mounted

--- scripts/gitChecker/RunChecker_Part3_A2.sh
+++ scripts/gitChecker/RunChecker_Part3_A2.sh
@@ -20,10 +20,11 @@ fi
 
 
 cd $AGRELEASE/alpha2
-export EOS_MGM_URL=root://eospublic.cern.ch
+#export EOS_MGM_URL=root://eospublic.cern.ch
 
 if [ ! -f run${RUNNO}sub00000.mid.gz  ]; then
-  eos cp /eos/experiment/alpha/midasdata/run${RUNNO}sub00000.mid.gz .
+  #eos cp /eos/experiment/alpha/midasdata/run${RUNNO}sub00000.mid.gz .
+  cp -v /eos/experiment/alpha/midasdata/run${RUNNO}sub00000.mid.gz .
 else
   echo "run${RUNNO}sub00000.mid.gz found locally"
 fi
