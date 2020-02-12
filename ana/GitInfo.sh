631d4f4b4c4679bd63f837311df95cf5091584bf
Joseph McKenna <joseph.mckenna@cern.ch>
2020-02-12 17:33:00 +0100

    Get GitInfo header to work with make and cmake

--- ana/GitInfo.sh
+++ ana/GitInfo.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "#define GIT_DATE            "  $( git log -n 1 --date=raw | grep Date | cut -b 8-19) > include/GitInfo.h
-echo "#define GIT_REVISION      \" $( git rev-parse --short HEAD ) \"" >> include/GitInfo.h
-echo "#define GIT_REVISION_FULL \" $( git log -n 1 | grep commit | cut -b 8-99) \"" >> include/GitInfo.h
-echo "#define GIT_BRANCH        \" $( git branch --remote --no-abbrev --contains) \"" >> include/GitInfo.h
-echo "#define GIT_DIFF_SHORT_STAT \" $( git branch --no-abbrev --contains) : $( git diff --shortstat) \"" >> include/GitInfo.h
-echo "#define COMPILATION_DATE    " $( date +%s) >> include/GitInfo.h
+OUTPUT_PATH=${1}
+
+echo "#define GIT_DATE            "  $( git log -n 1 --date=raw | grep Date | cut -b 8-19) > ${OUTPUT_PATH}/GitInfo.h
+echo "#define GIT_REVISION      \" $( git rev-parse --short HEAD ) \"" >> ${OUTPUT_PATH}/GitInfo.h
+echo "#define GIT_REVISION_FULL \" $( git log -n 1 | grep commit | cut -b 8-99) \"" >> ${OUTPUT_PATH}/GitInfo.h
+echo "#define GIT_BRANCH        \" $( git branch --remote --no-abbrev --contains) \"" >> ${OUTPUT_PATH}/GitInfo.h
+echo "#define GIT_DIFF_SHORT_STAT \" $( git branch --no-abbrev --contains) : $( git diff --shortstat) \"" >> ${OUTPUT_PATH}/GitInfo.h
+echo "#define COMPILATION_DATE    " $( date +%s) >> ${OUTPUT_PATH}/GitInfo.h
