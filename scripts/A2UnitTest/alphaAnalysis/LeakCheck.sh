e8cdb616801d9c655a6b8c5c623128d105cbb229
Joseph McKenna <joseph.mckenna@cern.ch>
2020-02-26 14:05:46 +0100

    Fix typo

--- scripts/A2UnitTest/alphaAnalysis/LeakCheck.sh
+++ scripts/A2UnitTest/alphaAnalysis/LeakCheck.sh
@@ -91,7 +91,7 @@ SUPP="--suppressions=${ROOTSYS}/etc/valgrind-root.supp"
 fi
 set -x
 #Suppress false positives: https://root.cern.ch/how/how-suppress-understood-valgrind-false-positives
-valgrind --leak-check=full --error-limit=no ${SUPP} --log-file="${LEAKTEST}" ./alphaAnalysis.exe ${Event_Limit} run${RUNNO}sub00000.mid.gz ${MODULESFLAGS} &> ${ALPHATEST}
+valgrind --leak-check=full --error-limit=no ${SUPP} --log-file="${LEAKTEST}" ./alphaAnalysis.exe ${Event_Limit} run${RUNNO}sub00000.mid.gz ${MODULEFLAGS} &> ${ALPHATEST}
 set +x
 
  
