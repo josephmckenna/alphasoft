423183ba87b091bd2cc494b08952c9b5d2f3b4c7
a-capra <andrea.capra85@gmail.com>
2020-06-04 17:41:18 -0700

    better info

--- reco/scripts/rung4ana.sh
+++ reco/scripts/rung4ana.sh
@@ -5,14 +5,21 @@ if [ ! -d "./RunLogs" ]; then
 fi
 
 RUNNO=1
-while [ $RUNNO -le 50 ]; do
+while [ $RUNNO -le 1 ]; do
     for SEED in 10081985 18061985 3092016 26092019 28091956 18031956 20051985 23061990 17122013 30112015; do
-	F="$DATADIR/outAgTPC_det_AWtime16ns_PADtime16ns_B1.00T_Q30_multirun${RUNNO}_seed${SEED}.root"
-	ls -lh $F
+	F="$DATADIR/MCdata_oldsim/outAgTPC_det_AWtime16ns_PADtime16ns_B1.00T_Q30_multirun${RUNNO}_seed${SEED}.root"
+	if [[ -f "$F" ]]; then
+	    s=$(wc -c <"$F")
+	    sMb=$(echo "scale=2; $s/1024/1024" | bc -l)
+	    echo "`basename $F` size is $sMb Mb"
+	else
+	    continue
+	fi
 	LOGNAME="./RunLogs/Rec"$(basename $F .root)".log"
 	#echo ${LOGNAME}
-	{ time ../g4ana.exe -f $F -a $AGRELEASE/ana/oldsim.hjson ; } &> ${LOGNAME} &
-	echo "g4ana.exe -f $F -a $AGRELEASE/ana/oldsim.hjson"
+	cmd="$AGRELEASE/build/reco/g4ana.exe -f $F -a $AGRELEASE/ana/oldsim.hjson -l 1"
+	{ time $cmd; } &> ${LOGNAME} &
+	echo $cmd
 	RUNNO=$((RUNNO+1))
     done
 done
