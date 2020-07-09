f3e88e0e630fc9456aed29a1c8b87b73dc324e30
a-capra <andrea.capra85@gmail.com>
2020-07-08 18:54:28 -0700

    modified utility scripts

--- reco/scripts/rung4ana.sh
+++ reco/scripts/rung4ana.sh
@@ -5,7 +5,7 @@ if [ ! -d "./RunLogs" ]; then
 fi
 
 RUNNO=1
-while [ $RUNNO -le 1 ]; do
+while [ $RUNNO -le 1000 ]; do
     for SEED in 10081985 18061985 3092016 26092019 28091956 18031956 20051985 23061990 17122013 30112015; do
 	F="$DATADIR/MCdata_oldsim/outAgTPC_det_AWtime16ns_PADtime16ns_B1.00T_Q30_multirun${RUNNO}_seed${SEED}.root"
 	if [[ -f "$F" ]]; then
@@ -21,6 +21,7 @@ while [ $RUNNO -le 1 ]; do
 	{ time $cmd; } &> ${LOGNAME} &
 	echo $cmd
 	RUNNO=$((RUNNO+1))
+	echo ""
     done
 done
 
