24206c7ae4f22fe66fc68f4de79020e3eb22c948
a-capra <andrea.capra85@gmail.com>
2020-09-11 10:21:46 -0700

    clean up

--- ana/src/display_module.cxx
+++ ana/src/display_module.cxx
@@ -107,49 +107,39 @@ public:
       
       if( !age->feam || !age->a16 )
          return flow;
-      // if( !age->feam->complete || !age->a16->complete || age->feam->error || age->a16->error )
-      //    return flow;
 
+      // DISPLAY low-level stuff here
       AgSignalsFlow* SigFlow = flow->Find<AgSignalsFlow>();
       if( !SigFlow )
          return flow;
-      
+
+      // DISPLAY high-level stuff here
       AgAnalysisFlow* analysis_flow = flow->Find<AgAnalysisFlow>();
       if( !analysis_flow || !analysis_flow->fEvent )
         return flow;
-        
+
+      // DISPLAY BSC stuff here
       AgBarEventFlow* bar_flow = flow->Find<AgBarEventFlow>();
-      if( !analysis_flow || !analysis_flow->fEvent || !bar_flow)
+      if( !bar_flow || !bar_flow->BarEvent)
         return flow;
-      // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-      // DISPLAY low-level stuff here
-      // fetching them from SigFlow
-      // this object is defined in AgFlow.h
-      // and more members will and can be added
-      // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-      // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-      // DISPLAY high-level stuff here
-      // fetching them from TStoreEvent* anEvent
-      // this object is defined in $ANALYSIS_TPC/include/TStoreEvent.h
-      // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-                                                                  
-      // I guess there could be two tabs one for low-level and one for high-level
-      // a "next" button to go the next event, i.e., terminating this fuction
-      // and waiting for the reco_module to analyze the following event.
-
-      // I think that I would like also a "previous" button, but it's not clear
-      // to me how to implement a buffer.
       #ifdef _TIME_ANALYSIS_
       START_TIMER
       #endif   
-      printf("DisplayRun::Analyze event no %d\n", age->counter);
 
-      if (!aged) {
-      	 aged = new Aged();
+         printf("DisplayRun::Analyze event no %d, FlowEvent no %d, BarEvent no %d\n", age->counter,analysis_flow->fEvent->GetEventNumber(),bar_flow->BarEvent-> GetID());
+
+      if (!aged) 
+         {
+            printf("New Aged!\n");
+            aged = new Aged();
       }
-      // analysis_flow->fEvent->Print();
-      if (aged) flags=aged->ShowEvent(age,analysis_flow, SigFlow,bar_flow, flags, runinfo);
+
+      analysis_flow->fEvent->Print();
+      if (aged) {
+         flags=aged->ShowEvent(age,analysis_flow, SigFlow,bar_flow, flags, runinfo);
+         printf("Aged::ShowEvent is %d\n",flags);
+      }
       #ifdef _TIME_ANALYSIS_
          if (TimeModules) flow=new AgAnalysisReportFlow(flow,"display_module",timer_start);
       #endif
