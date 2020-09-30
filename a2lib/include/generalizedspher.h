31e5c4240b57545524845fa28321497205ea2864
Joseph McKenna <joseph.mckenna@cern.ch>
2020-09-30 16:40:59 +0200

    Remove instances of using namespace std. Its possibly causing conflicts when building on macos... just a good habit not to do in headers anyway

--- a2lib/include/generalizedspher.h
+++ a2lib/include/generalizedspher.h
@@ -4,6 +4,5 @@
 #include "TVector3.h"
 #include "TMatrixD.h"
 #include "TMatrixDEigen.h"
-using namespace std;
 
-void sphericity(vector<double> x, vector<double> y, vector<double> z, int r, TVector3** axis, TVector3** values);
+void sphericity(std::vector<double> x, std::vector<double> y, std::vector<double> z, int r, TVector3** axis, TVector3** values);
