75d4e8c214512bf10139eddf77f149a5f4f781e4
a-capra <andrea.capra85@gmail.com>
2020-12-07 16:07:01 -0800

    do not update hash

--- agconfig.sh
+++ agconfig.sh
@@ -250,8 +250,8 @@ if [ -z "$(ls -A agana)" ]; then
     git submodule update --init agcfmdb
     echo "agana submodule loaded"
 else
-    git submodule update --remote agana
-    git submodule update --remote agcfmdb
+    git submodule update agana
+    git submodule update agcfmdb
     echo "agana submodule updated"
 fi
 
