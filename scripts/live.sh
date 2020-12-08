3a8e9365d00a3169b94657ae6c53508add38aa93
agdaq <agdaq>
2020-12-08 14:08:03 -0800

    wfexport is not (yet) included in agana

--- scripts/live.sh
+++ scripts/live.sh
@@ -3,6 +3,6 @@
 if [ -z "$MIDASSYS" ]; then
     echo "MIDAS needs to be installed and available through MIDASSYS"
 else
-    #agana.exe -R8088 -Hlocalhost -- --wfexport --aged
-    agana.exe -R8088 -Hlocalhost -- --wfexport
+    agana.exe -R8088 -Hlocalhost -- --aged
+    #agana.exe -R8088 -Hlocalhost -- --wfexport
 fi
