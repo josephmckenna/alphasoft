a36d75c0d8fe26957ab0e5769e9857095f7bc47b
Germano Bonomi <germano.bonomi@cern.ch>
2020-12-16 16:38:19 +0100

    Adding the distribution of hits in the different layers in show_sil

--- analysis/show_sil.C
+++ analysis/show_sil.C
@@ -5,10 +5,11 @@ void    open_root(int);
 void    get_parameters();
 void    sil_geo();
 void    sil_ene();
-TH1D*   hDecayAnn();
+void    sil_hit_mult();
 
 TTree *tree = 0;
 ROOT::RDataFrame *d;
+Int_t  nEvents = 0;
 Bool_t muons = false;
 Bool_t pbars = false;
 
@@ -19,6 +20,7 @@ void show_sil(int runNumber) {
     gStyle->SetOptStat(stat_opt.c_str());
     open_root(runNumber);
     get_parameters();
+    sil_hit_mult();
     sil_geo();
     sil_ene();
     return;
@@ -35,12 +37,13 @@ void sil_geo() {
     c_sil_geo->Divide(3,2);
     c_sil_geo->cd(1);
         ostringstream s;
-        if(muons) s << "Muons RUN - Layer multiplicity";
-        if(pbars) s << "Pbars RUN - Layer multiplicity";
+        if(muons) s << "Muons RUN - Layer multiplicity (per event)";
+        if(pbars) s << "Pbars RUN - Layer multiplicity (per event)";
         TH1D *h_lay = new TH1D("h_lay",s.str().c_str(), 6, -0.5, 5.5);
         tree->Draw("SilHits.fLayN>>h_lay");
+        if(nEvents!=0) h_lay->Scale(1./nEvents);
         h_lay->SetMinimum(0);
-        h_lay->Draw("");
+        h_lay->Draw("HIST,E");
     c_sil_geo->cd(2);
         TH2D *h_lay_mod = new TH2D("h_lay_mod","Module vs Layer", 15, -0.5, 14.5, 6, -0.5, 5.5);
         tree->Draw("SilHits.fLayN:SilHits.fModN>>h_lay_mod");
@@ -65,6 +68,59 @@ void sil_geo() {
         tree->Draw("SilHits.fpStrp>>h_p_strip");
         h_p_strip->SetXTitle("#");
         h_p_strip->Draw("");
+    c_sil_geo->Modified(); c_sil_geo->Update();
+}
+
+void sil_hit_mult() {
+    //##########################################################
+    // Preparing the histos
+    gROOT->cd();
+//        auto h0 = d->Define("nh", "Sum(SilHits.fLayN==0)").Filter("nh>0").Histo1D("nh");
+//        h0->Draw("");
+        
+//    auto h0 = d->Define("nh0", "Sum(SilHits.fLayN==0)").Filter("nh0>0").Histo1D("nh0");
+//    auto h1 = d->Define("nh1", "Sum(SilHits.fLayN==1)").Filter("nh1>0").Histo1D("nh1");
+//    auto h2 = d->Define("nh2", "Sum(SilHits.fLayN==2)").Filter("nh2>0").Histo1D("nh2");
+//    auto h3 = d->Define("nh3", "Sum(SilHits.fLayN==3)").Filter("nh3>0").Histo1D("nh3");
+//    auto h4 = d->Define("nh4", "Sum(SilHits.fLayN==4)").Filter("nh4>0").Histo1D("nh4");
+//    auto h5 = d->Define("nh5", "Sum(SilHits.fLayN==5)").Filter("nh5>0").Histo1D("nh5");
+
+    auto nh0 = d->Define("nh0", "Sum(SilHits.fLayN==0)").Filter("nh0>0");
+    auto nh1 = d->Define("nh1", "Sum(SilHits.fLayN==1)").Filter("nh1>0");
+    auto nh2 = d->Define("nh2", "Sum(SilHits.fLayN==2)").Filter("nh2>0");
+    auto nh3 = d->Define("nh3", "Sum(SilHits.fLayN==3)").Filter("nh3>0");
+    auto nh4 = d->Define("nh4", "Sum(SilHits.fLayN==4)").Filter("nh4>0");
+    auto nh5 = d->Define("nh5", "Sum(SilHits.fLayN==5)").Filter("nh5>0");
+
+    auto h0 = nh0.Histo1D({"h0", "Number of hits in Layer 0", 16, -0.5, 15.5}, "nh0");
+    auto h1 = nh1.Histo1D({"h1", "Number of hits in Layer 1", 16, -0.5, 15.5}, "nh1");
+    auto h2 = nh2.Histo1D({"h2", "Number of hits in Layer 2", 16, -0.5, 15.5}, "nh2");
+    auto h3 = nh3.Histo1D({"h3", "Number of hits in Layer 3", 16, -0.5, 15.5}, "nh3");
+    auto h4 = nh4.Histo1D({"h4", "Number of hits in Layer 4", 16, -0.5, 15.5}, "nh4");
+    auto h5 = nh5.Histo1D({"h5", "Number of hits in Layer 5", 16, -0.5, 15.5}, "nh5");
+
+    TH1D *h0_Clone = (TH1D*)h0->Clone();
+    TH1D *h1_Clone = (TH1D*)h1->Clone();
+    TH1D *h2_Clone = (TH1D*)h2->Clone();
+    TH1D *h3_Clone = (TH1D*)h3->Clone();
+    TH1D *h4_Clone = (TH1D*)h4->Clone();
+    TH1D *h5_Clone = (TH1D*)h5->Clone();
+
+    TCanvas *c_sil_hit_mult = new TCanvas("c_sil_hit_mult","c_sil_hit_mult",1500,1200);
+    c_sil_hit_mult->Divide(3,2);
+    c_sil_hit_mult->cd(1);
+        h0_Clone->Draw("HIST, E");
+    c_sil_hit_mult->cd(2);
+        h1_Clone->Draw("HIST, E");
+    c_sil_hit_mult->cd(3);
+        h2_Clone->Draw("HIST, E");
+    c_sil_hit_mult->cd(4);
+        h3_Clone->Draw("HIST, E");
+    c_sil_hit_mult->cd(5);
+        h4_Clone->Draw("HIST, E");
+    c_sil_hit_mult->cd(6);
+        h5_Clone->Draw("HIST, E");
+    c_sil_hit_mult->Modified(); c_sil_hit_mult->Update();
 }
 
 void sil_ene() {
@@ -80,9 +136,11 @@ void sil_ene() {
         tree->Draw("SilHits.fEdep*1000.>>h_hit_edep");
         h_hit_edep->SetXTitle("MeV");
         h_hit_edep->Draw("");
+    c_sil_ene->Modified(); c_sil_ene->Update();
 }
 
 void get_parameters() {
+    nEvents = *d->Filter("fPdgCode!=0").Count();
     auto pdg = d->Max("fPdgCode");
     if((int)*pdg==-2212) pbars = true;
     if((int)*pdg==-13||(int)*pdg==+13) muons = true;
@@ -109,10 +167,4 @@ void open_root(int runNumber) {
     d = new ROOT::RDataFrame("a2MC",sfile.c_str());
 }
 
-TH1D*   hDecayAnn() {
-//    auto conta = d->Filter("MCTracks.fPdgCode < 10").Count();
-//    cout << *conta << endl;
-//    
-    return nullptr;
-}
 
