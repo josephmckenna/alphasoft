6cc754e43e191fa28e76e476ad20be4d0548dddc
Joseph McKenna <joseph.mckenna@cern.ch>
2021-01-14 08:40:50 +0100

    Be more explicit about build paths and install paths

--- scripts/cvmfs/update.sh
+++ scripts/cvmfs/update.sh
@@ -42,14 +42,14 @@ elif [ `echo "${THIS_SETUP}" | grep '.sh' | wc -l` -eq 1 ]; then
    source ${THIS_SETUP}
    source agconfig.sh
    echo ${JUPYTER_PATH}
-   LCG_VERSION=`echo ${JUPYTER_PATH} | awk -F: '{print $1}' | awk -F/ '{print $6"/"$7}' `
+   LCG_VERSION_PATH=`echo ${JUPYTER_PATH} | awk -F: '{print $1}' | awk -F/ '{print $6"/"$7}' `
    LCG_VERSION_NAME=`echo ${JUPYTER_PATH} | awk -F: '{print $1}' | awk -F/ '{print $6"_"$7}' `
    #Insure a clean build dir
-   rm -rf ${AGRELEASE}/${LCG_VERSION}_build
-   mkdir -p ${AGRELEASE}/${LCG_VERSION}_build
-   INSTALL_PATH="/cvmfs/alpha.cern.ch/alphasoft/${LCG_VERSION}"
+   rm -rf ${AGRELEASE}/${LCG_VERSION_PATH}_build
+   mkdir -p ${AGRELEASE}/${LCG_VERSION_PATH}_build
+   INSTALL_PATH="/cvmfs/alpha.cern.ch/alphasoft/${LCG_VERSION_PATH}"
    mkdir -p ${INSTALL_PATH}
-   cd ${AGRELEASE}/${LCG_VERSION}_build
+   cd ${AGRELEASE}/${LCG_VERSION_PATH}_build
    #Check if we need cmake3 command or cmake
    if [ `command -v cmake3` ]; then
       #command cmake3 found... lets use it
@@ -58,15 +58,16 @@ elif [ `echo "${THIS_SETUP}" | grep '.sh' | wc -l` -eq 1 ]; then
       #cmake3 not found. Default version of cmake is probably 3
       export CMAKE=cmake
    fi
-   rm -v ${INSTALL_PATH}/alphasoft_${LCG_VERSION_NAME}_build.log 
-   ${CMAKE} ${AGRELEASE} -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} \
+   rm -vf ${INSTALL_PATH}/alphasoft_${LCG_VERSION_NAME}_build.log 
+   ${CMAKE} ${AGRELEASE} -DCMAKE_INSTALL_PREFIX="${INSTALL_PATH}" \
                          -DPRE_CONFIG=${THIS_SETUP} \
                  &> ~/alphasoft_${LCG_VERSION_NAME}_build.log
    make          &>> ~/alphasoft_${LCG_VERSION_NAME}_build.log
    make install  &>> ~/alphasoft_${LCG_VERSION_NAME}_build.log
    cp ~/alphasoft_${LCG_VERSION_NAME}_build.log ${INSTALL_PATH}/
    cd ~/alphasoft
-   rm -rf ${AGRELEASE}/${LCG_VERSION}_build
+   echo "Cleaning up build path: ${AGRELEASE}/${LCG_VERSION_PATH}_build"
+   rm -rf ${AGRELEASE}/${LCG_VERSION_PATH}_build
    cd ${HOME}
    cvmfs_server publish alpha.cern.ch
    sleep 10
