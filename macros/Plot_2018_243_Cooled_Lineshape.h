744ea7a44fc1c85cae5189d6f35372e5fbee2d1a
Joseph McKenna <joseph.mckenna@cern.ch>
2021-01-29 13:57:37 +0100

    Add LabVIEW data to the 2018 243 analysis

--- macros/Plot_2018_243_Cooled_Lineshape.h
+++ macros/Plot_2018_243_Cooled_Lineshape.h
@@ -1,6 +1,6 @@
 
 #include "TA2Plot.h"
 #include "TA2Plot_Filler.h"
-int Plot_2018_243_Cooled_Lineshape();
+int Plot_2018_243_Cooled_Lineshape(bool DrawVertices = false);
 void Plot_243_Light_Lineshape(int runNumber, bool DrawVertices);
 
