fb4daa09267764378560e08d2ec552ab5e3e95e0
Joseph McKenna <joseph.mckenna@cern.ch>
2021-01-30 14:54:12 +0100

    Fix ifdef status

--- rootUtils/include/PairGetters.h
+++ rootUtils/include/PairGetters.h
@@ -3,15 +3,17 @@
 #include "IntGetters.h"
 #include <utility>
 
-#if BUILD_AG
+#ifdef BUILD_AG
 std::pair<Int_t,Int_t> GetChronoBoardChannel(Int_t runNumber, const char* ChannelName);
 
 #endif
 
-#if BUILD_A2
+#ifdef BUILD_A2
 #include "TSISEvent.h"
-std::vector<std::pair<double,int>> GetSISTimeAndCounts(Int_t runNumber, int SIS_Channel, std::vector<double> tmin, std::vector<double> tmax);
+std::vector<std::pair<double,int>> GetSISTimeAndCounts(Int_t runNumber, int SIS_Channel,         std::vector<double> tmin, std::vector<double> tmax);
 std::vector<std::pair<double,int>> GetSISTimeAndCounts(Int_t runNumber, const char* ChannelName, std::vector<double> tmin, std::vector<double> tmax);
+std::vector<std::pair<double,int>> GetSISTimeAndCounts(Int_t runNumber, int SIS_Channel,         std::vector<TA2Spill> spills);
+std::vector<std::pair<double,int>> GetSISTimeAndCounts(Int_t runNumber, const char* ChannelName, std::vector<TA2Spill> spills);
 
 #endif
 #endif
