ef3e32d575150fbf4b8aaecb9064bc65b9d067fd
Joseph McKenna <joseph.mckenna@cern.ch>
2021-02-01 21:30:17 +0100

    Add skeleton LyAlpha analysis macro

---
+++ macros/LyAlpha.C
@@ -0,0 +1,67 @@
+void PlotLyAlphaScan(int runNumber = 57019)
+{
+
+TA2Plot* freq[9];
+
+
+for (int i=0; i<9; i++)
+{
+    std::cout<<"Adding Freq "<<i<<std::endl;
+    freq[i] = new TA2Plot(-10.,10.);
+    std::string dump="Lyman Alpha Freq "+ std::to_string(i);
+    std::vector<TA2Spill> dumps=Get_A2_Spills(runNumber,{dump},{-1});
+    std::cout<<dumps.size() << " dumps found"<<std::endl;
+    std::vector<std::pair<double,int>> qpulses=GetSISTimeAndCounts(
+       57156,  //Run Number
+       "QPULSE",   //SIS CHannel name (you can also use the number, in this case "QPULSE" == 53)
+       dumps
+    );
+    std::cout<<qpulses.size()<< " pulses found"<<std::endl;
+    for (std::pair<double,int>& pulse: qpulses)
+    {
+        //std::cout<<pulse.first-0.01<<std::endl;
+        freq[i]->AddTimeGate(runNumber,pulse.first-0.1, pulse.first + 0.9);
+    }
+}
+
+TA2Plot_Filler DataLoader;
+for (int i=0; i<9; i++)
+{
+    DataLoader.BookPlot(freq[i]);
+}
+
+DataLoader.SetLVChannel("121P",6);
+
+std::cout<<"Loading data"<<std::endl;
+
+
+//Load all data in a single pass of each tree (I am aiming for efficiency)
+DataLoader.LoadData();
+
+std::vector<double> ActualFreq={1.,2.,3.,4.,5.,6.,7.,8.};
+std::vector<double> Counts;
+for (int i=0; i<9; i++)
+{
+    Counts.push_back(freq[i]->GetNPassedCuts());
+}
+TString canvasTitle="R";
+canvasTitle+=runNumber;
+TCanvas* c1=new TCanvas(canvasTitle);
+TGraph* shape=new TGraph(9,ActualFreq.data(),Counts.data());
+shape->SetTitle(canvasTitle+" ");
+shape->Draw("AP*");
+
+TCanvas* c[9];
+for (int i=0; i<9; i++)
+{
+    c[i]=freq[i]->DrawCanvas();
+    TString name="R";
+    name+=runNumber;
+    name+="_Freq";
+    name+=i;
+    name+=".png";
+    c[i]->SaveAs(name);
+}
+
+
+}
