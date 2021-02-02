9047eded1fffb6179cb6876105b0f3cc02c5dde0
Joseph McKenna <joseph.mckenna@cern.ch>
2021-02-02 23:04:14 +0100

    What is TFormula::GradientStorage? We are passing a std::vector right, so pass a std::vector

--- alpha2/include/A2Flow.h
+++ alpha2/include/A2Flow.h
@@ -250,11 +250,12 @@ public:
    {
    }
 
-    felabviewFlowEvent(TAFlowEvent* flowevent, std::string m_BankName, TFormula::GradientStorage m_pdata, 
+    felabviewFlowEvent(TAFlowEvent* flowevent, std::string m_BankName, std::vector<double> m_pdata, 
     uint32_t m_MIDAS_TIME, uint32_t m_run_time, double m_labview_time)
       : TAFlowEvent(flowevent), BankName(m_BankName), m_data(m_pdata), 
       MIDAS_TIME(m_MIDAS_TIME), run_time(m_run_time), labview_time(m_labview_time)
    {
+      
    }
 
       felabviewFlowEvent(TAFlowEvent* flowevent, felabviewFlowEvent* flow)
