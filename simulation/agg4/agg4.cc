0e39f92c913510beb0d01b9224ffd4cc8acf892b
a-capra <andrea.capra85@gmail.com>
2021-02-24 15:34:38 -0800

    fix vis

--- simulation/agg4/agg4.cc
+++ simulation/agg4/agg4.cc
@@ -29,11 +29,10 @@
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-#include <TMath.h>
 
+#include "G4RunManagerFactory.hh"
 #include "G4RunManager.hh"
 #include "G4UImanager.hh"
-
 
 #include "DetectorConstruction.hh"
 #include "PhysicsList.hh"
@@ -43,17 +42,11 @@
 #include "StackingAction.hh"
 #include "SteppingAction.hh"
 
-
-#include <fstream>
-
-#ifdef G4VIS_USE
 #include "G4VisExecutive.hh"
-#endif
-
-#ifdef G4UI_USE
 #include "G4UIExecutive.hh"
-#endif
 
+#include <TMath.h>
+#include <fstream>
 #include "ElectronDrift.hh"
 #include "TPCBase.hh"
 
@@ -85,6 +78,13 @@ int gVerb;
 
 int main(int argc,char** argv)
 {
+  // Detect interactive mode (if no arguments) and define UI session
+  //
+  G4UIExecutive* ui = 0;
+  if ( argc == 1 ) {
+    ui = new G4UIExecutive(argc, argv);
+  }
+
   // Choose the Random engine
   CLHEP::HepRandom::setTheEngine(new CLHEP::RanecuEngine);
 
@@ -155,43 +155,48 @@ int main(int argc,char** argv)
   // Initialize G4 kernel
   runManager->Initialize();
 
+  // Initialize visualization
+  //
+  G4VisManager* visManager = new G4VisExecutive;
+  // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
+  // G4VisManager* visManager = new G4VisExecutive("Quiet");
+  visManager->Initialize();
+
   // Get the pointer to the User Interface manager
   G4UImanager* UImanager = G4UImanager::GetUIpointer();
 
-  if(argc!=1)   // batch mode
-    {
-      if(argc==3) { run_action->SetRunName(G4String(argv[2])); } //Modify output file name from command line
-      G4String command = "/control/execute ";
-      G4String fileName = argv[1];
-      UImanager->ApplyCommand(command+fileName);
-    }
-
-  else  // interactive mode : define visualization and UI terminal
-    {
-#ifdef G4VIS_USE
-      G4VisManager* visManager = new G4VisExecutive;
-      visManager->Initialize();
-#ifdef G4UI_USE
-      G4UIExecutive* ui = new G4UIExecutive(argc, argv);
-#endif
-      //      UImanager->ApplyCommand("/control/execute EventDisplay.mac");
-      UImanager->ApplyCommand("/control/execute vis.mac");
-#endif
-
-#ifdef G4UI_USE
-      ui->SessionStart();
-      delete ui;
-#endif
-
-#ifdef G4VIS_USE
-      delete visManager;
-#endif
-    }
+  // Process macro or start UI session
+  //
+  if ( ! ui ) { 
+    // batch mode
+    G4String command = "/control/execute ";
+    G4String fileName = argv[1];
+    UImanager->ApplyCommand(command+fileName);
+  }
+  else { 
+    // interactive mode
+    UImanager->ApplyCommand("/control/execute vis.mac");
+    ui->SessionStart();
+    delete ui;
+  }
 
   // Job termination
+  // Free the store: user actions, physics_list and detector_description are
+  // owned and deleted by the run manager, so they should not be deleted 
+  // in the main() program !
+  
+  delete visManager;
   delete runManager;
 
   return 0;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+/* emacs
+ * Local Variables:
+ * tab-width: 8
+ * c-basic-offset: 3
+ * indent-tabs-mode: nil
+ * End:
+ */
