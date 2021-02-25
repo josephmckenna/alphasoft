bd52b66a186439c6f3ba52377b0a7f48843a17cf
a-capra <andrea.capra85@gmail.com>
2021-02-24 20:37:21 -0800

    RunManagerFactory not available for G4<10.7

--- simulation/geant4/rTPCsim.cc
+++ simulation/geant4/rTPCsim.cc
@@ -29,8 +29,7 @@
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-
-#include "G4RunManagerFactory.hh"
+//#include "G4RunManagerFactory.hh"
 #include "G4RunManager.hh"
 #include "G4MTRunManager.hh"
 #include "G4UImanager.hh"
@@ -86,11 +85,11 @@ int main(int argc,char** argv)
   // Choose the Random engine
   CLHEP::HepRandom::setTheEngine(new CLHEP::RanecuEngine);
 
-  // // Construct the default run manager
-  // G4RunManager * runManager = new G4RunManager;
   // Construct the default run manager
-  auto* runManager =
-    G4RunManagerFactory::CreateRunManager(G4RunManagerType::Default);
+  G4RunManager * runManager = new G4RunManager;
+  // Construct the default run manager
+  // auto* runManager =
+  //   G4RunManagerFactory::CreateRunManager(G4RunManagerType::Default);
 
   GasModelParameters* GasParam = new GasModelParameters();
 
