79e99ab37f94069ef19c64efb04bccf640782358
a-capra <andrea.capra85@gmail.com>
2021-03-10 00:01:39 -0800

    improv my scripts

--- scripts/andrea/vis.sh
+++ scripts/andrea/vis.sh
@@ -2,13 +2,14 @@
 
 cd $AGRELEASE/bin
 
-#agana.exe -O$DATADIR/agmini/cosmics904501vis.root $MIDASDATA/run904501sub000.mid.lz4 -- --aged --anasettings $AGRELEASE/ana/cyl_l2.5.json --location TRIUMF --Bfield 0 | tee RunLogs/R904501sub000.log
+./agana.exe -O$DATADIR/agmini/cosmics904501vis.root $MIDASDATA/run904501sub000.mid.lz4 -- --aged --anasettings $AGRELEASE/ana/cyl_l2.5.json --location TRIUMF --Bfield 0 |& tee $AGRELEASE/RunLogs/R904501sub000vis.log
 
 #
 #gdb -ex="b $AGRELEASE/aged/src/AgedWindow.cxx:180" -ex="b /usr/src/debug/libXt-1.1.5/src/Resources.c:474" --ex=r --args agana.exe -O$DATADIR/agmini/cosmics904501vis.root $MIDASDATA/run904501sub000.mid.lz4 -- --aged --anasettings $AGRELEASE/ana/cyl_l2.5.json --location TRIUMF --Bfield 0 
 # also  Resources.c:815
 
-gdb --ex=r --args ./agana.exe -O$DATADIR/agmini/cosmics904501vis.root $MIDASDATA/run904501sub000.mid.lz4 -- --aged --anasettings $AGRELEASE/ana/cyl_l2.5.json --location TRIUMF --Bfield 0 
+#gdb --ex=r --args ./agana.exe -O$DATADIR/agmini/cosmics904501vis.root $MIDASDATA/run904501sub000.mid.lz4 -- --aged --anasettings $AGRELEASE/ana/cyl_l2.5.json --location TRIUMF --Bfield 0 
+gdb --ex="set logging on" --ex=r --args ./agana.exe -O$DATADIR/agmini/cosmics904501vis.root $MIDASDATA/run904501sub000.mid.lz4 -- --aged --anasettings $AGRELEASE/ana/cyl_l2.5.json --location TRIUMF --Bfield 0 
 
 #agana.exe -O$AGRELEASE/cosmics3873vis.root $AGMIDASDATA/run03873sub000.mid.lz4 -- --aged --anasettings $AGRELEASE/ana/cosm.json --Bfield 0
 
