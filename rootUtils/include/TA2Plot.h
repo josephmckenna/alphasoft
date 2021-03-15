84a9e37ad3fb8674c0f64af60acb5557afebccf4
Lukas Golino <lgolino@cern.ch>
2021-03-15 19:28:46 +0000

    Added new SISPlotEvents object but facing build issues

--- rootUtils/include/TA2Plot.h
+++ rootUtils/include/TA2Plot.h
@@ -49,6 +49,73 @@ class SISPlotEvent
       }
 };
 
+class SISPlotEvents: public TObject
+{
+   public:
+      std::vector<int> runNumber;
+      std::vector<double> t;  //Plot time (based off official time)
+      std::vector<double> OfficialTime;
+      std::vector<int> Counts;
+      std::vector<int> SIS_Channel;
+   public:
+      SISPlotEvents()
+      {
+      }
+      ~SISPlotEvents()
+      {
+      }
+      SISPlotEvents(SISPlotEvents m_SISEvent)
+      {
+         runNumber = m_SISEvent.runNumber
+         t = m_SISEvent.t
+         OfficialTime = m_SISEvent.OfficialTime
+         Counts = m_SISEvent.Counts
+         SIS_Channel = m_SISEvent.SIS_Channel  
+      }
+      void AddEvent(int m_runNumber, double m_ti, double m_OfficialTime, int m_Counts, int m_SIS_Channel);
+      {
+         runNumber.push_back(m_runNumber);
+         t.push_back(m_ti);  
+         OfficialTime.push_back(m_OfficialTime);
+         Counts.push_back(m_Counts);
+         SIS_Channel.push_back(m_SIS_Channel);
+      }
+      void AddSISPlotEvent(SISPlotEvent Event)
+      {
+         runNumber.push_back(Event.runNumber);
+         t.push_back(Event.t);  
+         OfficialTime.push_back(Event.OfficialTime);
+         Counts.push_back(Event.Counts);
+         SIS_Channel.push_back(Event.SIS_Channel);
+      }
+      int GetEventRunNumber(int event) const { return runNumber.at(event); }
+      SISPlotEvents operator+(const SISPlotEvents& plotA, const SISPlotEvents& plotB)
+      {
+         std::cout << "SISPlotEvents addition operator" << std::endl;
+         SISPlotEvents outputplot(plotA); //Create new from copy
+
+         //Vectors- need concacting
+         outputplot.runNumber.insert(outputplot.runNumber.end(), plotB.runNumber.begin(), plotB.runNumber.end() );
+         outputplot.t.insert(outputplot.t.end(), plotB.t.begin(), plotB.t.end() );
+         outputplot.OfficialTime.insert(outputplot.OfficialTime.end(), plotB.OfficialTime.begin(), plotB.OfficialTime.end() );
+         outputplot.Counts.insert(outputplot.Counts.end(), plotB.Counts.begin(), plotB.Counts.end() );
+         outputplot.SIS_Channel.insert(outputplot.SIS_Channel.end(), plotB.SIS_Channel.begin(), plotB.SIS_Channel.end() );
+         
+         return outputplot;
+      }
+      SISPlotEvents operator+=(const SISPlotEvents &plotB) 
+      {
+         std::cout << "SISPlotEvents += operator" << std::endl;
+         this->runNumber.insert(outputplot.runNumber.end(), plotB.runNumber.begin(), plotB.runNumber.end() );
+         this->t.insert(outputplot.t.end(), plotB.t.begin(), plotB.t.end() );
+         this->OfficialTime.insert(outputplot.OfficialTime.end(), plotB.OfficialTime.begin(), plotB.OfficialTime.end() );
+         this->Counts.insert(outputplot.Counts.end(), plotB.Counts.begin(), plotB.Counts.end() );
+         this->SIS_Channel.insert(outputplot.SIS_Channel.end(), plotB.SIS_Channel.begin(), plotB.SIS_Channel.end() );
+
+         return *this;
+      }
+}
+
 class TA2Plot: public TAPlot
 {
 protected:
@@ -77,6 +144,7 @@ protected:
 public:
    void SetSISChannels(int runNumber);
    std::vector<SISPlotEvent> SISEvents;
+   SISPlotEvents SISEventsNew;  
 
    void AddSVDEvent(TSVD_QOD* SVDEvent);
    void AddSISEvent(TSISEvent* SISEvent);
