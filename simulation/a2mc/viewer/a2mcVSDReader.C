6988f5493c1a4d7326ed28fa6ee78e41af2ef98a
Germano Bonomi <germano.bonomi@cern.ch>
2021-03-24 19:05:53 +0100

    Removing MTRoot flag from cmake_config_and_install.sh

--- simulation/a2mc/viewer/a2mcVSDReader.C
+++ simulation/a2mc/viewer/a2mcVSDReader.C
@@ -183,7 +183,7 @@ void a2mcVSDReader::LoadMCTracks() {
     auto trkProp = fTrackListMC->GetPropagator();
     // !!!! Need to store field on file !!!!
     // Can store TEveMagField ?
-    // trkProp->SetMagField(0., 0., 0.3); ///< in T (???)
+    // trkProp->SetMagField(0., 0., 0.5); ///< in T (???)
     trkProp->SetMagField(0.); ///< No magnetic field here
     trkProp->SetStepper(TEveTrackPropagator::kRungeKutta);
     trkProp->SetMaxR(50.);  ///< Based on the Oxford magnet dimension and generation sphere
