00f3f3b0b733ed6f811d37ee3b248e391cd82385
Joseph McKenna <joseph.mckenna@cern.ch>
2021-05-29 03:02:39 +0200

    new CERN runs

--- scripts/andrea/CERN2021.sh
+++ scripts/andrea/CERN2021.sh
@@ -11,7 +11,25 @@ fi
 
 
 #nohup agana.exe -O/mnt/fast_disk/andrea/cosmics4513.root --mt run04513sub000.mid.lz4 -- --EOS --Bfield 0 --calib --diag --anasettings $AGRELEASE/ana/comm.json &> $AGRELEASE/RunLogs/R4513.log &
+#nohup agana.exe -O/mnt/fast_disk/andrea/cosmics4513calib.root --mt run04513sub000.mid.lz4 -- --EOS --calib --diag --anasettings $AGRELEASE/ana/comm.json --loadcalib &> $AGRELEASE/RunLogs/R4513calib.log &
+#nohup agana.exe -O/mnt/fast_disk/andrea/cosmics4513newstr.root --mt run04513sub000.mid.lz4 -- --EOS --calib --diag --anasettings $AGRELEASE/ana/comm.json --Bfield 0 &> $AGRELEASE/RunLogs/R4513newstr.log &
 
-nohup agana.exe -O/mnt/fast_disk/andrea/cosmics4533.root --mt run04533sub000.mid.lz4 -- --EOS --Bfield 0 --calib --diag --anasettings $AGRELEASE/ana/comm.json &> $AGRELEASE/RunLogs/R4533.log &
+#nohup agana.exe -O/mnt/fast_disk/andrea/cosmics4533.root --mt run04533sub000.mid.lz4 -- --EOS --Bfield 0 --calib --diag --anasettings $AGRELEASE/ana/comm.json &> $AGRELEASE/RunLogs/R4533.log &
+#nohup agana.exe -O/mnt/fast_disk/andrea/cosmics4533calib.root --mt run04533sub000.mid.lz4 -- --EOS --calib --diag --anasettings $AGRELEASE/ana/comm.json --loadcalib &> $AGRELEASE/RunLogs/R4533calib.log &
+#nohup agana.exe -O/mnt/fast_disk/andrea/cosmics4533newstr.root --mt run04533sub000.mid.lz4 -- --EOS --calib --diag --anasettings $AGRELEASE/ana/comm.json --Bfield 0 &> $AGRELEASE/RunLogs/R4533newstr.log &
 
 
+#nohup agana.exe -O/mnt/fast_disk/andrea/cosmics4541.root --mt run04541sub000.mid.lz4 -- --EOS --calib --diag --anasettings $AGRELEASE/ana/comm.json --Bfield 0 &> $AGRELEASE/RunLogs/R4541.log &
+#agana.exe -O/mnt/fast_disk/andrea/cosmics4541.root --mt run04541sub000.mid.lz4 -- --EOS --calib --diag --anasettings $AGRELEASE/ana/comm.json --Bfield 0 |& tee $AGRELEASE/RunLogs/R4541.log
+
+
+#nohup agana.exe -O/mnt/fast_disk/andrea/cosmics4548.root --mt run04548sub000.mid.lz4 -- --EOS --calib --diag --anasettings $AGRELEASE/ana/comm.json --Bfield 0 &> $AGRELEASE/RunLogs/R4548.log &
+
+#nohup agana.exe -O/mnt/fast_disk/andrea/cosmics4553.root --mt run04553sub000.mid.lz4 -- --EOS --calib --diag --anasettings $AGRELEASE/ana/comm.json --Bfield 0 &> $AGRELEASE/RunLogs/R4553.log &
+
+#nohup agana.exe -O/mnt/fast_disk/andrea/cosmics4574.root --mt run04574sub000.mid.lz4 -- --EOS --calib --diag --anasettings $AGRELEASE/ana/comm.json --Bfield 0 &> $AGRELEASE/RunLogs/R4574.log &
+
+#nohup agana.exe -O/mnt/fast_disk/andrea/cosmics4576.root --mt run04576sub000.mid.lz4 -- --EOS --calib --diag --anasettings $AGRELEASE/ana/comm.json --Bfield 0 &> $AGRELEASE/RunLogs/R4576.log &
+
+#nohup agana.exe -O/mnt/fast_disk/andrea/cosmics4579.root --mt run04579sub000.mid.lz4 -- --EOS --calib --diag --anasettings $AGRELEASE/ana/comm.json --Bfield 0 &> $AGRELEASE/RunLogs/R4579.log &
+nohup agana.exe -O/mnt/fast_disk/andrea/cosmics4579.root --mt run04579sub000.mid.lz4 -- --EOS --calib --diag --anasettings $AGRELEASE/ana/comm2.json --Bfield 0 &> $AGRELEASE/RunLogs/R4579.log &
--- scripts/andrea/CERN2021plot.sh
+++ scripts/andrea/CERN2021plot.sh
@@ -12,5 +12,29 @@ fi
 
 #MainEventTree.exe --rootfile /mnt/fast_disk/andrea/cosmics4513.root --plot 1 --save 1 |& tee $AGRELEASE/RunLogs/R4513plot.log
 
-MainEventTree.exe --rootfile /mnt/fast_disk/andrea/cosmics4533.root --plot 1 --save 1 |& tee $AGRELEASE/RunLogs/R4533plot.log
+#MainEventTree.exe --rootfile /mnt/fast_disk/andrea/cosmics4533.root --plot 1 --save 1 |& tee $AGRELEASE/RunLogs/R4533plot.log
 
+
+
+#MainEventTree.exe --rootfile /mnt/fast_disk/andrea/cosmics4513calib.root --plot 0 --save 1 &> $AGRELEASE/RunLogs/R4513calibplot.log
+
+#MainEventTree.exe --rootfile /mnt/fast_disk/andrea/cosmics4533calib.root --plot 0 --save 1 &> $AGRELEASE/RunLogs/R4533calibplot.log
+
+
+
+#MainEventTree.exe --rootfile /mnt/fast_disk/andrea/cosmics4513newstr.root --plot 0 --save 1 &> $AGRELEASE/RunLogs/R4513newstrplot.log
+
+#MainEventTree.exe --rootfile /mnt/fast_disk/andrea/cosmics4533newstr.root --plot 0 --save 1 &> $AGRELEASE/RunLogs/R4533newstrplot.log
+
+#MainEventTree.exe --rootfile /mnt/fast_disk/andrea/cosmics4541.root --plot 1 --save 1 |& tee $AGRELEASE/RunLogs/R4541.log
+
+
+#MainEventTree.exe --rootfile /mnt/fast_disk/andrea/cosmics4548.root --plot 1 --save 1 |& tee $AGRELEASE/RunLogs/R4548.log
+
+#MainEventTree.exe --rootfile /mnt/fast_disk/andrea/cosmics4553.root --plot 1 --save 1 |& tee $AGRELEASE/RunLogs/R4553.log
+
+
+#MainEventTree.exe --rootfile /mnt/fast_disk/andrea/cosmics4574.root --plot 0 --save 1 |& tee $AGRELEASE/RunLogs/R4574.log&
+#MainEventTree.exe --rootfile /mnt/fast_disk/andrea/cosmics4576.root --plot 0 --save 1 |& tee $AGRELEASE/RunLogs/R4576.log&
+
+MainEventTree.exe --rootfile /mnt/fast_disk/andrea/cosmics4579.root --plot 0 --save 1 |& tee $AGRELEASE/RunLogs/R4579.log&
