17c1d01082e6d3eb169d3768e9dd7ec2398456f0
a-capra <andrea.capra85@gmail.com>
2021-07-02 20:33:11 -0700

    update config script

--- agconfig.sh
+++ agconfig.sh
@@ -44,8 +44,6 @@ alphaBeast()
 
 alphaCrunch()
 {
-  . ~/packages/rootana/thisrootana.sh
-
   . /cvmfs/sft.cern.ch/lcg/releases/gcc/4.8.4/x86_64-centos7/setup.sh
   . /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.20.06/x86_64-centos7-gcc48-opt/bin/root/thisroot.sh
 
@@ -96,7 +94,7 @@ echo "##################################################"
 
 if [ `echo "${GARFIELD_HOME}" | wc -c` -gt 1 ]; then
    echo "GARFIELD_HOME set to ${GARFIELD_HOME}, configuring... "
-   cat ${GARFIELD_HOME}/install/share/Garfield/setupGarfield.sh
+   #cat ${GARFIELD_HOME}/install/share/Garfield/setupGarfield.sh
    source ${GARFIELD_HOME}/install/share/Garfield/setupGarfield.sh
 else
    echo "GARFIELD_HOME not set"
@@ -139,32 +137,20 @@ done
 
 
 
-#Quit if ROOT and ROOTANA are setup...
-if [ "${1}" = "clean" ]; then
-  echo "Clean setup of environment variables"
-  echo "Now using rootana git submodule"
-  export ROOTANASYS="${AGRELEASE}/rootana"
+#Quit if ROOT is setup...
+if [ ${#ROOTSYS} -lt 3 ]; then
+    echo "Please setup root manually (or run . agconfig.sh"
 else
-  if [ ${#ROOTANASYS} -gt 3 ]; then
-    echo "ROOTANASYS set... not over writing: $ROOTANASYS"
-  else
-    echo "Using rootana git submodule"
-    export ROOTANASYS="${AGRELEASE}/rootana"
-  fi
-  if [ ${#ROOTSYS} -lt 3 ]; then
-    echo "Please setup root manually (or run . agconfig.sh clean)"
-  else
     echo "ROOTSYS set... not over writing: $ROOTSYS"
-  fi
 fi
 
-
-if [ "$ROOTANASYS" = "${AGRELEASE}/rootana" ]; then
-    echo "ROOTANA submodule enabled: " ` cd ${AGRELEASE}/rootana && git log -1 --format=%h`
+if [ -z ${MIDASSYS+x} ]; then
+    echo "MIDASSYS not set, continuing without MIDAS"
+else
+    echo "MIDASSYS set: $MIDASSYS"
 fi
 
-
-#Setup ROOT and ROOTANA if we havn't quit yet...
+#Setup ROOT if we havn't quit yet...
 case `hostname` in
 alphavme*  )
   echo "alphavme detected..."
@@ -239,7 +225,6 @@ lxplus* )
   if [ `which root-config | wc -c` -gt 5 ]; then
   echo 'root      :' `root-config --version`
   fi
-  echo "ROOTANASYS: ${ROOTANASYS}"
   echo "AGRELEASE : ${AGRELEASE}"
   ;;
 esac
