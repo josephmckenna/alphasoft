67265f821c18fe8efa0b140e271944c62953881e
Joseph McKenna <joseph.mckenna@cern.ch>
2021-07-25 13:13:16 +0200

    Add Dark time plotting to the Plot_2018_243_Cooled_Lineshape macro, add more headers needed to compile the macro, but its still unhappy and must be run without compiling

--- alpha2/macros/Plot_2018_243_Cooled_Lineshape.C
+++ alpha2/macros/Plot_2018_243_Cooled_Lineshape.C
@@ -1,12 +1,13 @@
 #include "Plot_2018_243_Cooled_Lineshape.h"
 
-
+#include "TA2Plot.h"
 
 void Plot_243_Light_Lineshape(int runNumber, bool DrawVertices, bool ZeroTime)
 {
    double zcut=10.;
    
    TA2Plot* VertexPlot[9][2];
+   TA2Plot* DarkVertexPlot[9][2];
    std::vector<double> ActualFreq;
    if ( runNumber==57181 || runNumber==57195 )
       ActualFreq={-200,-100,   -50, -25,   0., 25,   50,100, 200};
@@ -17,7 +18,12 @@ void Plot_243_Light_Lineshape(int runNumber, bool DrawVertices, bool ZeroTime)
    {
       VertexPlot[i][0]=new TA2Plot(-zcut,zcut,ZeroTime);
       VertexPlot[i][1]=new TA2Plot(-zcut,zcut,ZeroTime);
+      DarkVertexPlot[i][0]=new TA2Plot(-zcut,zcut,ZeroTime);
+      DarkVertexPlot[i][1]=new TA2Plot(-zcut,zcut,ZeroTime);
+
+      
       std::cout<<"Populating frequency "<<i<<std::endl;
+
       for (int k=0; k<4; k++)
       {
          int Freq=i;
@@ -35,7 +41,45 @@ void Plot_243_Light_Lineshape(int runNumber, bool DrawVertices, bool ZeroTime)
          else
             std::cout<<" C state ";
          std::cout<<"("<<ActualFreq[i]<<" offset)"<<std::endl;
-         VertexPlot[i][IsCState]->AddDumpGates(runNumber,{buf},{-1});
+         std::vector<TA2Spill> spills = Get_A2_Spills(runNumber,{buf},{-1});
+         
+         VertexPlot[i][IsCState]->AddDumpGates(spills);
+         
+         std::vector<std::pair<double,double>> DarkTimes;
+         double last_tmin = -1;
+         double last_tmax = -1;
+
+         //Find all dark times between the above light times
+         std::vector<TA2Spill> AllSpills = Get_A2_Spills(runNumber,{"*"},{-1});
+         for (const TA2Spill& s: spills)
+         {
+            double tmin = s.GetStartTime();
+            double tmax = s.GetStopTime();
+            //std::cout<<"Window:\t"<<tmin<<"\t"<<tmax<<std::endl;
+            for (const TA2Spill& all: AllSpills)
+            {
+               
+               if (all.GetStartTime() > tmax && all.GetStartTime() > tmin)
+               {
+                  // Basic check for something wild going wrong
+                  assert ( last_tmax < tmax );
+                  // Check the dark period is shorter than 10 seconds
+                  assert (  all.GetStartTime() - tmax  < 10 );
+                  //std::cout<<"\tAdding "<<tmax<<"\t"<<all.GetStartTime() << std::endl;
+                  DarkTimes.push_back({ tmax, all.GetStartTime()});
+                  last_tmax = tmax;
+                  break;
+               }
+            }
+         }
+
+         for (const std::pair<double,double>& d: DarkTimes)
+         {
+            //std::cout <<"DEBUG: Dark time:\t"<< d.first <<"\t"<<d.second<<"\t " << d.second - d.first <<std::endl;
+            DarkVertexPlot[i][IsCState]->AddTimeGate( runNumber,  d.first, d.second);
+         }
+
+
       }
    }
 
@@ -59,6 +103,7 @@ void Plot_243_Light_Lineshape(int runNumber, bool DrawVertices, bool ZeroTime)
       for (int j=0; j<2; j++) //IsCState?
       {
          DataLoader.BookPlot(VertexPlot[i][j]);
+         DataLoader.BookPlot(DarkVertexPlot[i][j]);
       }
    }
    DataLoader.SetLVChannel("D243",2);
@@ -90,6 +135,13 @@ void Plot_243_Light_Lineshape(int runNumber, bool DrawVertices, bool ZeroTime)
             save_as += ".png";
             c1->SaveAs(save_as);
 
+            TCanvas* c2 = DarkVertexPlot[i][j]->DrawCanvas(TString("Dark") + title);
+            save_as = "R";
+            save_as += runNumber;
+            save_as += title;
+            save_as += ".png";
+            c2->SaveAs(save_as);
+
          }
       }
    }
@@ -105,32 +157,57 @@ void Plot_243_Light_Lineshape(int runNumber, bool DrawVertices, bool ZeroTime)
    //TH1D* hCState=new TH1D(title+"_CState",title+"_CState",9,0.,9.);
    std::vector<double> DStateCounts;
    std::vector<double> CStateCounts;
+
+   std::vector<double> DarkDStateCounts;
+   std::vector<double> DarkCStateCounts;
    for (int i=0; i<9; i++)
    {
       DStateCounts.push_back(VertexPlot[i][0]->GetNPassedCuts());
       CStateCounts.push_back(VertexPlot[i][1]->GetNPassedCuts());
+      
+      DarkDStateCounts.push_back(DarkVertexPlot[i][0]->GetNPassedCuts());
+      DarkCStateCounts.push_back(DarkVertexPlot[i][1]->GetNPassedCuts());
+      
       //hDState->Fill(i,VertexPlot[i][0]->GetNPassedCuts());
+
       //hCState->Fill(i,VertexPlot[i][1]->GetNPassedCuts());
    }
    TString canvasTitle="R";
    canvasTitle+=runNumber;
    TCanvas* c=new TCanvas(canvasTitle);
-   c->Divide(1, 2);
+   c->Divide(2, 2);
    c->cd(1);
    TGraph* DState=new TGraph(9,ActualFreq.data(),DStateCounts.data());
    DState->SetTitle(canvasTitle+" D_State");
    DState->Draw("AP*");
+
+   c->cd(2);
+   TGraph* DarkDState=new TGraph(9,ActualFreq.data(),DarkDStateCounts.data());
+   DarkDState->SetTitle(canvasTitle+" Dark D_State");
+   DarkDState->Draw("AP*");
+
    //c->cd(2);
    //hDState->Draw("HIST");
    std::cout<<"RunNumber: "<<runNumber<<std::endl;
-   std::cout <<"FrequencyOffset\tDStateCounts\tCStateCounts"<<std::endl;
+   std::cout <<"FrequencyOffset\tDStateCounts\tCStateCounts\tDarkDStateCounts\tDArkCStateCounts"<<std::endl;
    for (int i=0; i<9; i++)
-      std::cout<<ActualFreq[i]<<"\t"<<DStateCounts[i]<<"\t"<<CStateCounts[i]<<std::endl;
-   
+   {
+      std::cout<<ActualFreq[i];
+      std::cout<<"\t"<<DStateCounts[i]<<"\t"<<CStateCounts[i];
+      std::cout<<"\t"<<DarkDStateCounts[i]<<"\t"<<DarkCStateCounts[i];
+   }
+
+   c->cd(3);
    TGraph* CState=new TGraph(9,ActualFreq.data(),CStateCounts.data());
-   c->cd(2);
    CState->SetTitle(canvasTitle+" C_State");
    CState->Draw("AP*");
+   
+   c->cd(4);
+   TGraph* DarkCState=new TGraph(9,ActualFreq.data(),DarkCStateCounts.data());
+   DarkCState->SetTitle(canvasTitle+" Dark C_State");
+   DarkCState->Draw("AP*");
+   
+   
    //c->cd(4);
    //hCState->Draw("HIST");
    c->Update();
--- alpha2/macros/Plot_2018_243_Cooled_Lineshape.h
+++ alpha2/macros/Plot_2018_243_Cooled_Lineshape.h
@@ -1,6 +1,16 @@
 
 #include "TA2Plot.h"
 #include "TA2Plot_Filler.h"
+
+#include "TA2SpillGetters.h"
+#include "TA2Spill.h"
+#include "TGraph.h"
+#include "TCanvas.h"
+
+
+#include <vector>
+#include <utility> //std::pair
+
 int Plot_2018_243_Cooled_Lineshape(bool DrawVertices = false, bool zeroTime = true);
 void Plot_243_Light_Lineshape(int runNumber, bool DrawVertices, bool zeroTime = true);
 
