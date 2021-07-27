02a8b35f4b2c39e0f6258b90b3f42753fc085213
a-capra <andrea.capra85@gmail.com>
2021-07-26 20:36:20 -0700

    looking at 2018 pbars

---
+++ scripts/andrea/plotmixing2018.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MainEventTree.exe --rootfile $DATADIR/output/pbar3839.root --plot 0 --save 1 &> /dev/null &
+MainEventTree.exe --rootfile $DATADIR/output/pbar3840.root --plot 0 --save 1 &> /dev/null &
+MainEventTree.exe --rootfile $DATADIR/output/pbar3846.root --plot 0 --save 1 &> /dev/null &
+MainEventTree.exe --rootfile $DATADIR/output/pbar3847.root --plot 0 --save 1 &> /dev/null &
+MainEventTree.exe --rootfile $DATADIR/output/pbar3848.root --plot 0 --save 1 &> /dev/null &
+MainEventTree.exe --rootfile $DATADIR/output/pbar3837.root --plot 0 --save 1 &> /dev/null &
+MainEventTree.exe --rootfile $DATADIR/output/pbar3841.root --plot 0 --save 1 &> /dev/null &
+MainEventTree.exe --rootfile $DATADIR/output/pbar3842.root --plot 0 --save 1 &> /dev/null &
+MainEventTree.exe --rootfile $DATADIR/output/pbar3843.root --plot 0 --save 1 &> /dev/null &
+MainEventTree.exe --rootfile $DATADIR/output/pbar3844.root --plot 0 --save 1 &> /dev/null &
+MainEventTree.exe --rootfile $DATADIR/output/pbar3834.root --plot 0 --save 1 &> /dev/null &
+MainEventTree.exe --rootfile $DATADIR/output/pbar3838.root --plot 0 --save 1 &> /dev/null &
+MainEventTree.exe --rootfile $DATADIR/output/pbar3836.root --plot 0 --save 1 &> /dev/null &
