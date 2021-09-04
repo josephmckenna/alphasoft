dc170311cdbb6a45e465a1eea8164872d4cb7c74
Joseph McKenna <joseph.mckenna@cern.ch>
2021-09-04 13:22:15 +0200

    Add missing header (fix build on lxplus)

--- rootUtils/include/PlotGetters.h
+++ rootUtils/include/PlotGetters.h
@@ -9,6 +9,9 @@
 #include "TAPlot.h"
 #include "TA2Plot.h"
 #include "TAGPlot.h"
+
+#include <sstream>
+
 #ifdef BUILD_AG
 void Plot_Chrono(Int_t runNumber, Int_t Chronoboard, Int_t ChronoChannel, Double_t tmin=0., Double_t tmax=-1.);
 void Plot_Chrono(Int_t runNumber, Int_t Chronoboard, Int_t ChronoChannel, const char* description, Int_t repetition=1, Int_t offset=0);
