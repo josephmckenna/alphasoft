e381aa57791222d0454e53bab9832de015f46949
lukasgolino <lukas.golino@hotmail.co.uk>
2021-10-28 15:25:50 +0200

    Added Joe's comment changes, but they are untested as the class is currently not linked to the project. LibLinkDef.hh and CMakeList.txt still require updating.

--- ana/include/TChronoBoardCounter.h
+++ ana/include/TChronoBoardCounter.h
@@ -13,39 +13,40 @@ class TChronoBoardCounter: public TObject
    
     public:
     //default constr.
-    TChronoBoardCounter():fStartTime(-1), fStopTime(-1), fBoard(-1)
+    TChronoBoardCounter():fStartTime(-1), fStopTime(-1), fBoard(-1), fCounts({0})
     {
        //Should I do something with fCounts here?
+       //fCounts = {0};
     };
 
     //Copy constr.
-    TChronoBoardCounter(const TChronoBoardCounter& counter):fStartTime(counter.fStartTime), fStopTime(counter.fStopTime), fBoard(counter.fBoard), fCounts(counter.fBoard)
+    TChronoBoardCounter(const TChronoBoardCounter& counter):fStartTime(counter.fStartTime), fStopTime(counter.fStopTime), fBoard(counter.fBoard), fCounts(counter.fCounts)
     {
     };
 
     //Constr. for the board and dump times (can populate counts later)
-    TChronoBoardCounter(double startTime, double stopTime, int board):fStartTime(startTime), fStopTime(stopTime), fBoard(board)
+    TChronoBoardCounter(double startTime, double stopTime, int board):fStartTime(startTime), fStopTime(stopTime), fBoard(board), fCounts({0}
     {
     };
 
     //Constr. for just the board (can populate times later)
-    TChronoBoardCounter(int board):fBoard(board)
+    TChronoBoardCounter(int board):fStartTime(-1), fStopTime(-1), fBoard(board), fCounts({0})
     {
     };
 
     //=== Getters ===
-    double GetStartTime()           { return fStartTime; };
-    double GetStopTime()            { return fStopTime; };
-    double GetBoard()               { return fBoard; };
-    int* GetCounts()                { return &fCounts; };
-    int GetCount(int channel)       { return fCounts[channel]; };  //TODO, add a check that channel is in range. For now I will promise to not attempt to access unassigned memory. 
+    double GetStartTime() const                             { return fStartTime; };
+    double GetStopTime() const                              { return fStopTime; };
+    double GetBoard() const                                 { return fBoard; };
+    int* GetCounts() const                                  { return &fCounts; };
+    const int GetCount(int channel) const                   { return fCounts[channel]; };  //TODO, add a check that channel is in range. For now I will promise to not attempt to access unassigned memory. 
 
 
     //=== Setters ===
-    void SetStartTime(double startTime)       { fStartTime = startTime; };
-    void SetStopTime(double stopTime)       { fStopTime = stopTime; };
-    void SetBoard(double board)       { fBoard = board; };
-    void AddCountsToChannel(int channel, int counts) { fCounts[channel]+=counts; };
+    void SetStartTime(double startTime)                     { fStartTime = startTime; };
+    void SetStopTime(double stopTime)                       { fStopTime = stopTime; };
+    void SetBoard(double board)                             { fBoard = board; };
+    void AddCountsToChannel(int channel, int counts)        { fCounts[channel]+=counts; };
 
 
     //=== Operator overloads ===
