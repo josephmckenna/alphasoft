f6731095953427c6216c3ec1d4a78bacf3b1f4d1
agdaq <agdaq@alphagdaq>
2021-11-09 21:22:03 +0100

    Fix bug fetching the 0th chronobox channel (Fixes spill log)

--- rootUtils/src/TChronoChannelGetters.cxx
+++ rootUtils/src/TChronoChannelGetters.cxx
@@ -8,7 +8,7 @@ TChronoChannel Get_Chrono_Channel(Int_t runNumber, const char* ChannelName, Bool
    for (const std::pair<std::string,int>& board : TChronoChannel::CBMAP)
    {
       int chan = Get_Chrono_Channel_In_Board(runNumber, board.first, ChannelName, ExactMatch);
-      if (chan>0)
+      if (chan >= 0)
       {
          TChronoChannel c(board.first,chan);
          return c;
