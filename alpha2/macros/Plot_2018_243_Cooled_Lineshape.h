853770317a479b4dfbd55732df176c563498fe1d
Art Olin <olin@triumf.ca>
2022-01-22 17:25:57 -0800

    AO: header file is needed for macro

--- alpha2/macros/Plot_2018_243_Cooled_Lineshape.h
+++ alpha2/macros/Plot_2018_243_Cooled_Lineshape.h
@@ -5,6 +5,7 @@
 #include "TA2SpillGetters.h"
 #include "TA2Spill.h"
 #include "TGraph.h"
+#include "TGraphErrors.h"
 #include "TCanvas.h"
 
 
@@ -13,4 +14,6 @@
 
 int Plot_2018_243_Cooled_Lineshape(bool DrawVertices = false, bool zeroTime = true);
 std::vector<TA2Plot*> Plot_243_Light_And_Dark_Lineshape(int runNumber, bool DrawVertices, bool zeroTime = true);
-
+//double GetRep(double time, TTimeWindows* timeWindows, int& lastRep);
+double GetRep(double time, const std::vector<TA2Spill>& LaserSpills, int& lastRep, bool& IsLight);
+double GetRep1(double time, const std::vector<TA2Spill>& LaserSpills, int& lastRep, bool& IsLight);
