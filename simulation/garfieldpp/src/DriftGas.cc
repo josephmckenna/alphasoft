554470ae33433d37d03b0e3d311713c9667a889f
Lars Martin <lmartin@triumf.ca>
2022-02-03 12:07:25 -0800

    Added options for custom pressure and temperature, to allow us to investigate fluctuation effects.

--- simulation/garfieldpp/src/DriftGas.cc
+++ simulation/garfieldpp/src/DriftGas.cc
@@ -7,15 +7,15 @@
 #include "Garfield/MediumMagboltz.hh"
 
 
-int makeGas(double CO2frac = 10, int nEF = 20, double EFmax = 200000, int nBF = 6, double BFmax = 1.25, int nTh = 5, double thetaMax = 180)
+int makeGas(double CO2frac = 10, int nEF = 20, double EFmax = 200000, int nBF = 6, double BFmax = 1.25, int nTh = 5, double thetaMax = 180, double p = 725., double T = 293.15)
 {
     // Make a medium
     Garfield::MediumMagboltz* gas = new Garfield::MediumMagboltz();
     gas->SetComposition("ar", 100.-CO2frac, "co2", CO2frac);
-    gas->SetTemperature(293.15); // K
+    gas->SetTemperature(T); // K
     //gas->SetTemperature(303.15); // K
     //    gas->SetPressure(760.); // Torr @ TRIUMF
-    gas->SetPressure(725.); // Torr @ CERN, it may be 735 Torr = 980 hPa, altitude 432 m
+    gas->SetPressure(p); // Torr @ CERN, it may be 735 Torr = 980 hPa, altitude 432 m
 
     // Set electric field (EF) range covered by gas table.
     double EFmin =    100.; // V/cm
@@ -70,7 +70,7 @@ int makeGas(double CO2frac = 10, int nEF = 20, double EFmax = 200000, int nBF = 
 
     //gas->DisableDebugging();
     // Save the table.
-    TString filename = TString::Format("ar_%.0f_co2_%.0f_NTP_%dE%.0f_%dB%.2f.gas",100-CO2frac,CO2frac,nEF,EFmax,nBF,BFmax);
+    TString filename = TString::Format("ar_%.0f_co2_%.0f_%dE%.0f_%dB%.2f_%.0ftorr_%.2fK.gas",100-CO2frac,CO2frac,nEF,EFmax,nBF,BFmax,p,T);
     //    TString filename = TString::Format("ar_64_co2_26_cf4_10_NTP_%dE%.0f_%dB%.0f.gas",nEF,EFmax,nBF,BFmax);
     //    TString filename = TString::Format("ar_xx_co2_xx_air_0_NTP_%dE%.0f_%dB%.0f.gas",nEF,EFmax,nBF,BFmax);
     gas->WriteGasFile(filename.Data());
@@ -79,15 +79,15 @@ int makeGas(double CO2frac = 10, int nEF = 20, double EFmax = 200000, int nBF = 
 }
 
 int makeContaminatedGas(double N2frac = 1.0, int nEF = 25, double EFmax = 1000000,
-                        int nBF = 1, double BFmax = 0., int nTh = 0, double thetaMax = 0.)
+                        int nBF = 1, double BFmax = 0., int nTh = 0, double thetaMax = 0., double p = 725., double T = 293.15)
 {
    double CO2frac=30.;
     // Make a medium
     Garfield::MediumMagboltz* gas = new Garfield::MediumMagboltz();
     gas->SetComposition("ar", 100.-CO2frac, "co2", CO2frac-N2frac, "n2", N2frac);
-    gas->SetTemperature(293.15); // K
+    gas->SetTemperature(T); // K
     //    gas->SetPressure(760.); // Torr @ TRIUMF
-    gas->SetPressure(725.); // Torr @ CERN, it may be 735 Torr = 980 hPa, altitude 432 m
+    gas->SetPressure(p); // Torr @ CERN, it may be 735 Torr = 980 hPa, altitude 432 m
 
     // Set electric field (EF) range covered by gas table.
     double EFmin =    100.; // V/cm
@@ -142,7 +142,7 @@ int makeContaminatedGas(double N2frac = 1.0, int nEF = 25, double EFmax = 100000
 
     //gas->DisableDebugging();
     // Save the table.
-    TString filename = TString::Format("ar_%.0f_co2_%.0f_n2_%.0f_NTP_%dE%.0f_%dB%.2f.gas",100.-CO2frac,CO2frac-N2frac,N2frac,nEF,EFmax,nBF,BFmax);
+    TString filename = TString::Format("ar_%.0f_co2_%.0f_n2_%.0f_%dE%.0f_%dB%.2f_%.0ftorr_%.2fK.gas",100.-CO2frac,CO2frac-N2frac,N2frac,nEF,EFmax,nBF,BFmax,p,T);
     //    TString filename = TString::Format("ar_64_co2_26_cf4_10_NTP_%dE%.0f_%dB%.0f.gas",nEF,EFmax,nBF,BFmax);
     //    TString filename = TString::Format("ar_xx_co2_xx_air_0_NTP_%dE%.0f_%dB%.0f.gas",nEF,EFmax,nBF,BFmax);
     gas->WriteGasFile(filename.Data());
@@ -159,6 +159,8 @@ int main(int argc, char * argv[]){
     double BFmax = 1.2;
     int nTh = 5;
     double thetaMax = 180.;
+    double p = 725.;
+    double T = 293.15;
 
     for(int i = 1; i < argc; i++){
         switch(i){
@@ -169,6 +171,8 @@ int main(int argc, char * argv[]){
         case 5: BFmax = atof(argv[i]); break;
         case 6: nTh = atoi(argv[i]); break;
         case 7: thetaMax = atof(argv[i]); break;
+        case 8: p = atof(argv[i]); break;
+        case 9: T = atof(argv[i]); break;
         }
     }
     makeGas(CO2frac, nEF, EFmax, nBF, BFmax, nTh, thetaMax);
