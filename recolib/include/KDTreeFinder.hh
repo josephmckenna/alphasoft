9d3f8e0e2b2eff99743ce8ba2ea05b5592810a6b
Joseph McKenna <joseph.mckenna@cern.ch>
2022-03-02 09:54:41 +0100

    Add header missing from last commit

---
+++ recolib/include/KDTreeFinder.hh
@@ -0,0 +1,54 @@
+// Tracks finder class definition
+// for ALPHA-g TPC analysis
+// Author: A.Capra
+// Date: Sep. 2016
+
+#ifndef __AKDTREEFINDER__
+#define __AKDTREEFINDER__ 1
+#include "TSpacePoint.hh"
+#include "TracksFinder.hh"
+#include <vector>
+#include <list>
+#include <set>
+#include <bitset>
+#include "TKDTree.h"
+
+class KDTreeFinder: public TracksFinder
+{
+private:
+   const double fLastPointRadCut;
+   const double fPointsRadCut;
+   const double fPointsPhiCut;
+   const double fPointsZedCut;
+   const double fMaxIncreseAdapt;
+
+   std::vector<double> fX, fY, fZ;
+   TKDTreeID* fPoints;
+   // Bit masked cluster ID... limits us to 32 tracks / clusters
+   std::vector<std::bitset<64>> fCluster;
+   uint32_t fClusterID;
+
+public:
+   KDTreeFinder(std::vector<TSpacePoint*>*, const double MaxIncrease, const double LastPointRadCut );
+   ~KDTreeFinder()
+   {
+       delete fPoints;
+   };
+
+   inline double GetMaxIncreseAdapt() const {return fMaxIncreseAdapt;}
+   inline double GetLastPointRadCut() const { return fLastPointRadCut; }
+
+   virtual int RecTracks();
+
+   void PointCluster(const int index, double distcut, track_t& atrack);
+};
+
+#endif
+
+/* emacs
+ * Local Variables:
+ * tab-width: 8
+ * c-basic-offset: 3
+ * indent-tabs-mode: nil
+ * End:
+ */
